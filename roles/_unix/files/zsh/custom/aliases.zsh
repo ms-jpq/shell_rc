c79518f9ce665add28bf964e1ae94d5e92e58cb4
Fly me to the moon <git@bigly.dog>
2019-11-17 13:10:18 -0800

    added outdated casks

--- roles/_unix/files/zsh/custom/aliases.zsh
+++ roles/_unix/files/zsh/custom/aliases.zsh
@@ -4,29 +4,14 @@
 
 alias sudo='sudo -E '
 
-if type exa > /dev/null
-then
-  alias ls='exa --group-directories-first -ahF '
-  alias l='ls -1 '
-  alias ll='ls -l '
-  tree() {
-    exa --group-directories-first -ahF -T -L ${1-9999}
-  }
-else
-  alias ls='ls -hAF --color --group-directories-first'
-  alias l='ls -1'
-  alias ll='l -l'
-  tree() {
-    command tree -L ${1-9999}
-  }
-fi
+alias ls='exa --group-directories-first -ahF '
+alias l='ls -1 '
+alias ll='ls -l '
+tree() {
+  ls -T -L ${1-9999}
+}
 
-
-if type bat > /dev/null
-then
-  alias cat="bat --pager never --theme GitHub"
-fi
-
+alias cat="bat --pager never --theme GitHub"
 
 alias hist='history'
 
@@ -38,6 +23,9 @@ alias rsy='rsync -ah --no-o --no-g --partial --info=progress2'
 #################### ############### ####################
 #################### Anisible Region ####################
 #################### ############### ####################
+alias a='ansible'
+alias ap='ansible-playbook'
+alias ag='ansible-galaxy'
 
 
 #################### ########## ####################
--- roles/macos/files/zsh/custom/macos_aliases.zsh
+++ roles/macos/files/zsh/custom/macos_aliases.zsh
@@ -7,7 +7,9 @@ alias trash='trash -v'
 alias batt='pmset -g batt'
 alias dns='sudo killall -HUP mDNSResponder'
 
-alias brewup='brew update && brew upgrade && brew cleanup && brew doctor'
+alias brewup='brew update && brew upgrade && brew cleanup && brew doctor && brew cask outdated'
+
+alias mp='multipass'
 
 ma() {
   man -t $1 | open -fa Preview
