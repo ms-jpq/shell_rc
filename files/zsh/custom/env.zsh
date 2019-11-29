0c3b811cf25bfa47ebf40d5a879787a228d76ec7
Fly me to the moon <git@bigly.dog>
2019-11-29 11:31:53 -0800

    use micro for pager

--- files/zsh/custom/env.zsh
+++ files/zsh/custom/env.zsh
@@ -1,9 +1,12 @@
 export EDITOR=micro
-export PAGER=less
+export PAGER=micro
 
 export LESS="-FKQRi"
 
-export BAT_THEME=GitHub
+
 export BAT_PAGER=never
+export BAT_THEME=GitHub
+export BAT_STYLE=auto
+
 
 export FZF_DEFAULT_OPTS="--color light"
