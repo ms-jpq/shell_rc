254fb21f7c9e28a946ad7f195708ad937fd383a7
Fly me to the moon <git@bigly.dog>
2019-12-03 00:16:21 -0800

    added gotop

--- files/zsh/custom/aliases.zsh
+++ files/zsh/custom/aliases.zsh
@@ -17,6 +17,8 @@ alias f=' fzf'
 
 alias m='micro'
 
+alias gotop=' gotop -c lite'
+
 alias rsy='rsync -ah --no-o --no-g --partial --info=progress2'
 
 alias hist=' history'
--- files/zsh/custom_linux/basic.zsh
+++ files/zsh/custom_linux/basic.zsh
@@ -2,9 +2,9 @@
 #################### Linux Region ####################
 #################### ############ ####################
 
-alias ls=' ls --group-directories-first --color -hAF '
-alias l='ls -1 '
-alias ll='ls -l '
+alias ls=' ls --group-directories-first --color -hAF'
+alias l='ls -1'
+alias ll='ls -l'
 alias tree=' tree -L'
 
 
