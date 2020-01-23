8a99b0c6e062645ee17c924ef20151933bf89dae
Fly me to the moon <git@bigly.dog>
2020-01-22 18:16:28 -0800

    added broot

--- files/zsh/custom_linux/broot.zsh
+++
@@ -1,3 +0,0 @@
-# source "$HOME/Library/Preferences/org.dystroy.broot/launcher/bash/br"
-
-alias d='br'
--- files/zsh/custom_macos/broot_src.zsh
+++ files/zsh/custom_macos/broot_src.zsh
@@ -1,3 +1,1 @@
 source "$HOME/Library/Preferences/org.dystroy.broot/launcher/bash/br"
-
-alias d='br'
