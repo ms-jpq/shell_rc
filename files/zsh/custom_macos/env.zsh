f97a89d0e63d9ac91f49d97306600e4b215c7b35
Fly me to the moon <git@bigly.dog>
2020-01-27 17:14:15 +0800

    added ncurses

--- files/zsh/custom_macos/env.zsh
+++ files/zsh/custom_macos/env.zsh
@@ -6,8 +6,9 @@ export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
 export PATH="/usr/local/opt/curl/bin:$PATH"
 export PATH="/usr/local/opt/gnu-getopt/bin:$PATH"
 export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/openssl/bin:$PATH"
 export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/openssl/bin:$PATH"
+export PATH="/usr/local/opt/ncurses/bin:$PATH"
 
 
 export LC_ALL=zh_CN.UTF-8
