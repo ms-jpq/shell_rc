098c214413c1c35266c64587e79fb7963e94bd25
Fly me to the moon <git@bigly.dog>
2020-03-29 18:06:06 -0700

    added mkvenv

--- files/zsh/custom/python.zsh
+++ files/zsh/custom/python.zsh
@@ -4,6 +4,17 @@
 
 alias py='python3'
 
+mkvenv() {
+  local VENV_PATH=".venv"
+  if [[ -d "$VENV_PATH" ]]
+  then
+    echo "Virtualenv already initialized"
+  else
+    python3 -m venv "$VENV_PATH"
+  fi
+}
+
+
 pip3() {
   if [[ -z "$VIRTUAL_ENV" ]]
   then
