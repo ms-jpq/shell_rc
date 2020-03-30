038c734f2284a84c9f15eef5612da4f24c73a422
Fly me to the moon <git@bigly.dog>
2020-03-29 23:14:10 -0700

    use nodenv over nvm

--- files/zsh/custom/node.zsh
+++ files/zsh/custom/node.zsh
@@ -2,9 +2,8 @@
 #################### Node Region ####################
 #################### ########### ####################
 
-export NVM_DIR="$HOME/.nvm"
-. "$NVM_DIR/nvm.sh"
-. "$NVM_DIR/bash_completion"
+export PATH="$HOME/.nodenv/bin:$PATH"
+eval "$(nodenv init -)"
 
 
 alias np='PATH="$PWD/node_modules/.bin:$PATH" '
