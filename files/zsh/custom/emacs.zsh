beb6fac55b0e6d6684cada05650dcfcd10f83e20
Fly me to the moon <git@bigly.dog>
2020-04-07 02:17:23 -0700

    emacs no client

--- files/zsh/custom/emacs.zsh
+++ files/zsh/custom/emacs.zsh
@@ -3,15 +3,4 @@
 #################### ############ ####################
 
 alias e='emacs'
-alias ec='emacsclient -t'
-
 alias ep='touch .projectile'
-
-ee() {
-  if emacs_running
-  then
-    ec
-  else
-    e
-  fi
-}
