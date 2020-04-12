2e6e49e2ca3a516dff1e8cd35a8adcf11eb010ec
Fly me to the moon <git@bigly.dog>
2020-04-12 14:57:49 -0700

    Revert "removed lf"
    This reverts commit 0c859bbf217c9b6ef4eee47f839ea0a54d5fa3de.

---
+++ files/lf/unmap.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+import re
+
+
+cmds1 = """\
+up                       (default 'k' and '<up>')
+half-up                  (default '<c-u>')
+page-up                  (default '<c-b>' and '<pgup>')
+down                     (default 'j' and '<down>')
+half-down                (default '<c-d>')
+page-down                (default '<c-f>' and '<pgdn>')
+updir                    (default 'h' and '<left>')
+open                     (default 'l' and '<right>')
+quit                     (default 'q')
+top                      (default 'gg' and '<home>')
+bottom                   (default 'G' and '<end>')
+toggle                   (default '<space>')
+invert                   (default 'v')
+unselect                 (default 'u')
+copy                     (default 'y')
+cut                      (default 'd')
+paste                    (default 'p')
+clear                    (default 'c')
+redraw                   (default '<c-l>')
+reload                   (default '<c-r>')
+read                     (default ':')
+rename                   (default 'r')
+shell                    (default '$')
+shell-pipe               (default '%')
+shell-wait               (default '!')
+shell-async              (default '&')
+find                     (default 'f')
+find-back                (default 'F')
+find-next                (default ';')
+find-prev                (default ',')
+search                   (default '/')
+search-back              (default '?')
+search-next              (default 'n')
+search-prev              (default 'N')
+mark-save                (default 'm')
+mark-load                (default "'")
+mark-remove              (default `"`)
+"""
+
+cmds2 = """\
+cmd-escape               (default '<esc>')
+cmd-complete             (default '<tab>')
+cmd-enter                (default '<c-j>' and '<enter>')
+cmd-history-next         (default '<c-n>')
+cmd-history-prev         (default '<c-p>')
+cmd-delete               (default '<c-d>' and '<delete>')
+cmd-delete-back          (default '<bs>' and '<bs2>')
+cmd-left                 (default '<c-b>' and '<left>')
+cmd-right                (default '<c-f>' and '<right>')
+cmd-home                 (default '<c-a>' and '<home>')
+cmd-end                  (default '<c-e>' and '<end>')
+cmd-delete-home          (default '<c-u>')
+cmd-delete-end           (default '<c-k>')
+cmd-delete-unix-word     (default '<c-w>')
+cmd-yank                 (default '<c-y>')
+cmd-transpose            (default '<c-t>')
+cmd-interrupt            (default '<c-c>')
+cmd-word                 (default '<a-f>')
+cmd-word-back            (default '<a-b>')
+cmd-capitalize-word      (default '<a-c>')
+cmd-delete-word          (default '<a-d>')
+cmd-uppercase-word       (default '<a-u>')
+cmd-lowercase-word       (default '<a-l>')
+cmd-transpose-word       (default '<a-t>')
+"""
+
+cmds3 = """\
+map zh set hidden!
+map zr set reverse!
+map zn set info
+map zs set info size
+map zt set info time
+map za set info size:time
+map sn :set sortby natural; set info
+map ss :set sortby size; set info size
+map st :set sortby time; set info time
+map sa :set sortby atime; set info atime
+map sc :set sortby ctime; set info ctime
+map se :set sortby ext; set info
+map gh cd ~
+"""
+
+cmds4 = """\
+map e $$EDITOR $f
+map i $$PAGER $f
+map w $$SHELL
+"""
+
+
+def parse_unquotes():
+  c1_1 = "map \"'\""
+  c1_2 = 'map \'"\''
+  return [c1_1, c1_2]
+
+
+def escape_non_ascii(s: str):
+  ss = s.strip("'")
+  s1 = ss[0]
+  return ss if s1.isalnum() or s1 == "<" else f'"{ss}"'
+
+
+# Commands in LF are mapped differently
+def parse_quotes(lines, cmd):
+  m = re.findall("'[^\']+'", lines)
+  l = [f"{'c' if cmd else ''}map " + escape_non_ascii(s)
+       for s in m
+       if s]
+  return l
+
+
+def parse_normal_map(lines):
+  l = re.findall("map [^ ]+", lines)
+  return l
+
+
+def p_cmds12():
+  c_ = parse_unquotes()
+  c1 = parse_quotes(cmds1, False)
+  c2 = parse_quotes(cmds2, True)
+  c12 = [*c_, *c1, *c2]
+  return c12
+
+
+def p_cmds34():
+  c3 = parse_normal_map(cmds3)
+  c4 = parse_normal_map(cmds4)
+  return [*c3, *c4]
+
+
+c_all = [*p_cmds12(), *p_cmds34()]
+
+for c in c_all:
+  print(c)
