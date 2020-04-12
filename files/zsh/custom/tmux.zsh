6d8c6a798f1770a642214e6dbffcd9b56eac791f
Fly me to the moon <git@bigly.dog>
2020-04-12 15:54:39 -0700

    remove unset

--- files/zsh/custom/tmux.zsh
+++ files/zsh/custom/tmux.zsh
@@ -6,9 +6,5 @@ alias tl='tmux list-sessions'
 
 ta() {
   local session="$(tl -F '#{session_name}' | fzf -0 -1)"
-  if [[ -z "$session" ]]
-  then
-    unset session
-  fi
   tmux new-session -A -s "${session:-"MAIN"}"
 }
