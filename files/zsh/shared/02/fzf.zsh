7335f730b1701671d7bbf23d5e5ade01454082d0
Fly me to the moon <git@bigly.dog>
2020-04-30 15:35:41 -0700

    fzf default :D

--- files/zsh/shared/02/fzf.zsh
+++ files/zsh/shared/02/fzf.zsh
@@ -16,8 +16,6 @@ export FZF_DEFAULT_OPTS="--color $FZF_COLOUR \
                          --bind alt-a:select-all \
                          --bind alt-l:deselect-all"
 
-export FZF_DEFAULT_COMMAND='fd -HIL -t f'
-
 export FZF_PREVIEW="test -d {} \
                     && exa \
                     --color=always \
@@ -25,15 +23,28 @@ export FZF_PREVIEW="test -d {} \
                     -T -L 2 {} \
                     || bat --color always {}"
 
+
+
+export FZF_FD_PREFIX='fd -HIL'
+
+export FZF_DEFAULT_COMMAND="$FZF_FD_PREFIX -t f"
+export FZF_COMPLETION_OPTS="$(printf "--preview '%s'" "$FZF_PREVIEW")"
+
+export FZF_ALT_C_COMMAND="$FZF_FD_PREFIX -0 -t d -t l"
+export FZF_ALT_C_OPTS="$FZF_COMPLETION_OPTS --read0"
+
+export FZF_CTRL_T_COMMAND="$FZF_FD_PREFIX -0"
+export FZF_CTRL_T_OPTS="$FZF_COMPLETION_OPTS --read0"
+
+
 export FZF_TMUX_HEIGHT='100%'
 
-export FZF_COMPLETION_OPTS="$(printf "--preview '%s'" "$FZF_PREVIEW")"
 
 alias f='fzf'
 alias fp='fzf --preview $FZF_PREVIEW'
 
 
 d() {
-  local dest="$(FZF_DEFAULT_COMMAND='fd -HIL -t d -t l' fp -q "${*:-""}")"
+  local dest="$(FZF_DEFAULT_COMMAND="$FZF_ALT_C_COMMAND" fp --read0 -q "${*:-""}")"
   cd "$dest" || return 1
 }
