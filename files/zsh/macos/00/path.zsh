8bfa6ef3f1aecf942e0e17ff8204a7e6f51fff01
Fly me to the moon <git@bigly.dog>
2020-05-05 12:28:37 -0700

    whitespace

--- files/zsh/macos/00/path.zsh
+++ files/zsh/macos/00/path.zsh
@@ -16,4 +16,3 @@ export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
 export PATH="/usr/local/opt/gnu-which/libexec/gnubin:$PATH"
-
--- files/zsh/shared/02/tmux.zsh
+++ files/zsh/shared/02/tmux.zsh
@@ -8,4 +8,3 @@ ta() {
   local session="$(tl -F '#{session_name}' | fzf -0 -1)"
   tmux new-session -A -s "${session:-"MAIN"}"
 }
-
