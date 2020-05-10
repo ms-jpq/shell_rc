617e7641a6c5dc80fefcded04763b5231d3ac775
Fly me to the moon <git@bigly.dog>
2020-05-10 14:12:06 -0700

    bring back insta prmpt

---
+++ files/zsh/framework/00/p10k.zsh
@@ -0,0 +1,9 @@
+#################### #################### ####################
+#################### Instant Promp Region ####################
+#################### #################### ####################
+
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
+then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
