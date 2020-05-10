e3a39fada1e7efa3faf131a92221734529746d70
Fly me to the moon <git@bigly.dog>
2020-05-10 14:31:56 -0700

    completions

--- files/zsh/plugins/01/completions.zsh
+++ files/zsh/plugins/01/completions.zsh
@@ -2,4 +2,9 @@
 #################### Completions Region ####################
 #################### ################## ####################
 
+# INIT
+fpath=("$ZDOTDIR/zsh-completions/src" $fpath)
+# INIT
+
+
 autoload -U compinit && compinit
