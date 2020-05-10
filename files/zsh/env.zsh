38e132f48187124d7d4451b08d546f721ce9ab40
Fly me to the moon <git@bigly.dog>
2020-05-10 15:42:00 -0700

    fix env

--- files/zsh/env.zsh
+++ files/zsh/env.zsh
@@ -2,9 +2,11 @@
 #################### ENV Region ####################
 #################### ########## ####################
 
+# XDG #
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+# XDG #
+
 export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
 
 export PROMPT_EOL_MARK=""
-
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
