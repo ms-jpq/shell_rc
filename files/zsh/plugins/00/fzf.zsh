c98fc4df8036cbec33b31d0d7c8e9754cf746532
Fly me to the moon <git@bigly.dog>
2020-05-10 17:48:15 -0700

    fzf comp

--- files/zsh/plugins/00/fzf.zsh
+++ files/zsh/plugins/00/fzf.zsh
@@ -25,12 +25,12 @@ export FZF_PREVIEW="test -d {} \
 
 
 
-export FZF_FD_PREFIX='fd -HIL'
+export FZF_FD_PREFIX='fd -HL'
 
 export FZF_DEFAULT_COMMAND="$FZF_FD_PREFIX -t f -t l"
 export FZF_COMPLETION_OPTS="$(printf "--preview '%s'" "$FZF_PREVIEW")"
 
-export FZF_ALT_C_COMMAND="$FZF_FD_PREFIX -0 -t d -t l"
+export FZF_ALT_C_COMMAND="$FZF_FD_PREFIX -0 -t d"
 export FZF_ALT_C_OPTS="$FZF_COMPLETION_OPTS --read0"
 
 export FZF_CTRL_T_COMMAND="$FZF_FD_PREFIX -0"
@@ -38,12 +38,6 @@ export FZF_CTRL_T_OPTS="$FZF_COMPLETION_OPTS --read0"
 
 
 export FZF_TMUX_HEIGHT='100%'
-
-
-# INTI #
-source "$ZDOTDIR/fzf/shell/key-bindings.zsh"
-source "$ZDOTDIR/fzf/shell/completion.zsh"
-# INIT #
 
 
 alias f='fzf'
@@ -54,3 +48,23 @@ d() {
   local dest="$(FZF_DEFAULT_COMMAND="$FZF_ALT_C_COMMAND" fp --read0 -q "${*:-""}")"
   cd "$dest" || return 1
 }
+
+
+#################### ################ ####################
+#################### Overwrite Region ####################
+#################### ################ ####################
+
+_fzf_compgen_path() {
+  fd -HL -t d -t f -t l "$1"
+}
+
+
+_fzf_compgen_dir() {
+  fd -HL -t d "$1"
+}
+
+
+# INTI #
+source "$ZDOTDIR/fzf/shell/key-bindings.zsh"
+source "$ZDOTDIR/fzf/shell/completion.zsh"
+# INIT #
