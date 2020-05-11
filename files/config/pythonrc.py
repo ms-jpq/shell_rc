425534bc75e8b5bdda2211331640e90b3fadf916
Fly me to the moon <git@bigly.dog>
2020-05-11 14:26:28 -0700

    python history XDG

--- files/config/pythonrc.py
+++ files/config/pythonrc.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
-from pprint import pprint
-import readline
 
-readline.write_history_file = lambda *args: None
+import readline
+import os
+from pprint import pprint
+
+__history_file__ = os.path.join(os.environ["XDG_CACHE_HOME"], "python_hist")
+__readline_write_history__ = readline.write_history_file
+
+readline.write_history_file = lambda *args: __readline_write_history__(
+    __history_file__)
+if os.path.isfile(__history_file__) and os.access(__history_file__, os.R_OK):
+  readline.read_history_file(__history_file__)
