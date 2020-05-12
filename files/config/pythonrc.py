666bc186fb80efb49cdb68f6ce9f65aa1db1a21a
Fly me to the moon <git@bigly.dog>
2020-05-12 11:43:33 -0700

    python read write

--- files/config/pythonrc.py
+++ files/config/pythonrc.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
-import readline
+import atexit
 import os
+import readline
 from pprint import pprint
 
 __history_file__ = os.path.join(os.environ["XDG_CACHE_HOME"], "python_hist")
 __readline_write_history__ = readline.write_history_file
 
-readline.write_history_file = lambda *args: __readline_write_history__(
-    __history_file__)
+readline.write_history_file = lambda *args: None
+
 if os.path.isfile(__history_file__) and os.access(__history_file__, os.R_OK):
   readline.read_history_file(__history_file__)
+
+atexit.register(__readline_write_history__, __history_file__)
