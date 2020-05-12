75ad04736931999ed5cb2a30e8207c7bfc8bfbda
Fly me to the moon <git@bigly.dog>
2020-05-11 22:06:17 -0700

    setopt interactivecomments

--- files/zsh/shared/01/scripting.zsh
+++ files/zsh/shared/01/scripting.zsh
@@ -2,5 +2,6 @@
 #################### CD Region ####################
 #################### ######### ####################
 
-setopt multios      # auto tee
-setopt prompt_subst # auto expansion at commandline
+setopt interactivecomments # allow comments in prompt
+setopt multios             # auto tee
+setopt prompt_subst        # auto expansion at commandline
