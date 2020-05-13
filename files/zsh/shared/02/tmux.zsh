d4d8689648e8b25153c97b4091527f9dae30d580
Fly me to the moon <git@bigly.dog>
2020-05-12 20:44:39 -0700

    better tmux selector

--- files/zsh/shared/02/tmux.zsh
+++ files/zsh/shared/02/tmux.zsh
@@ -4,7 +4,18 @@
 
 alias tl='tmux list-sessions'
 
+
 ta() {
-  local session="$(tl -F '#{session_name}' | fzf -0 -1)"
-  tmux new-session -A -s "${session:-"MAIN"}"
+  if [[ $# -ne 0 ]]
+  then
+    tmux new-session -A -s "$*"
+  else
+    local session
+    session="$(tl -F '#{session_name}' | fzf -0 -1)"
+    if [[ $? -eq 130 ]]
+    then
+      return
+    fi
+    tmux new-session -A -s "${session:-"MAIN"}"
+  fi
 }
