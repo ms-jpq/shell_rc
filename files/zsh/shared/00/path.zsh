ec48b0c55785bee4e5fa15bf18393f95f9f20c44
Fly me to the moon <git@bigly.dog>
2020-05-13 16:48:18 -0700

    local bin

--- files/zsh/shared/00/path.zsh
+++ files/zsh/shared/00/path.zsh
@@ -6,5 +6,5 @@ if [[ "$SHLVL" -eq 1 ]]
 then
   export PATH="$XDG_CONFIG_HOME/zsh/bin:$PATH"
   export PATH="$XDG_CONFIG_HOME/scripts:$PATH"
-  export PATH="$XDG_DATA_HOME/bin:$PATH"
+  export PATH="$HOME/.local/bin:$PATH"
 fi
