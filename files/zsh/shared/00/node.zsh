e83f0eb2e7df7495367de75f348ffc3fb9becd5e
Fly me to the moon <git@bigly.dog>
2020-05-12 22:10:08 -0700

    added npm xdg

--- files/zsh/shared/00/node.zsh
+++ files/zsh/shared/00/node.zsh
@@ -10,5 +10,8 @@ fi
 eval "$(nodenv init - --no-rehash)"
 
 
+export npm_config_cache="$XDG_CACHE_HOME/npm"
+
+
 alias np='PATH="$PWD/node_modules/.bin:$PATH" '
 alias npx='export PATH="$PWD/node_modules/.bin:$PATH"'
