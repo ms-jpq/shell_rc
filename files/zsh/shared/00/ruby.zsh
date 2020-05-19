944aba24689158bc3009b84ff4e0c96308f0a4a1
Fly me to the moon <git@bigly.dog>
2020-05-18 18:44:54 -0700

    ruby

--- files/zsh/shared/00/ruby.zsh
+++ files/zsh/shared/00/ruby.zsh
@@ -7,7 +7,7 @@ if [[ "$SHLVL" -eq 1 ]]
 then
   export PATH="$RBENV_ROOT/bin:$PATH"
 fi
-eval "$(rbenv init - --no-rehash)"
+# eval "$(rbenv init - --no-rehash)"
 
 
 export GEM_HOME="$XDG_DATA_HOME/gem"
