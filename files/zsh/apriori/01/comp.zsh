4f4a7bd72e7f8b4a4b5daeb2379b9ef842848f9b
Fly me to the moon <git@bigly.dog>
2020-05-19 21:48:20 -0700

    added more glob

--- files/zsh/apriori/01/comp.zsh
+++ files/zsh/apriori/01/comp.zsh
@@ -9,7 +9,7 @@ setopt auto_param_slash  # add trailing / to folder names
 
 setopt noflowcontrol
 
-setopt complete_in_word  # move cursor to end
+setopt complete_in_word  # comp both ends of words
 setopt glob_complete     # do not aggressively expand glob
 
 setopt no_menu_complete  # do not autoselect the first completion entry
@@ -22,7 +22,7 @@ export WORDCHARS=''
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
 
 # Cache completions:
-zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion::complete:*' use-cache on
 zstyle ':completion::complete:*' cache-path "$XDG_CACHE_HOME/zsh_comp"
 
 
--- files/zsh/apriori/01/glob.zsh
+++ files/zsh/apriori/01/glob.zsh
@@ -3,5 +3,6 @@
 #################### ########### ####################
 
 setopt bad_pattern   # print error message
+setopt extended_glob # more glob?
 setopt no_case_glob  # case insensitive
 setopt no_case_match # case insensitive
