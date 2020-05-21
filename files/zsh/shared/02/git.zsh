b1237dd1310058cdadf33227d9d84cb9a75f0f6a
Fly me to the moon <git@bigly.dog>
2020-05-20 21:25:28 -0700

    outsource git

--- files/zsh/shared/02/git.zsh
+++ files/zsh/shared/02/git.zsh
@@ -1,17 +1,6 @@
 #################### ########## ####################
-#################### GIT Region ####################
+#################### Git Region ####################
 #################### ########## ####################
 
-if [[ "$SHLVL" -eq 1 ]]
-then
-  export PATH="$XDG_CONFIG_HOME/gitrc:$PATH"
-fi
+source "$XDG_CONFIG_HOME/gitrc/main.zsh"
 
-
-export GIT_PAGER='delta --paging=never | less'
-
-alias lg='lazygit'
-
-gitlines() {
-  git ls-files | rg "$@" | xargs wc -l
-}
