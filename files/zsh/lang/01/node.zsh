611faad89da1047470ff1643c4bbd40d24e009bf
Fly me to the moon <git@bigly.dog>
2020-06-05 15:09:56 -0700

    node use toggle

--- files/zsh/lang/01/node.zsh
+++ files/zsh/lang/01/node.zsh
@@ -6,6 +6,6 @@ export NODE_REPL_HISTORY="$XDG_CACHE_HOME/node_hist"
 export npm_config_cache="$XDG_CACHE_HOME/npm"
 
 
-alias np='PATH="$PWD/node_modules/.bin:$PATH" '
+alias np='paths toggle "$PWD/node_modules/.bin"'
 
 # Remember fx -- https://github.com/antonmedv/fx
