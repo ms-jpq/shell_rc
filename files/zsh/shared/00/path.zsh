11edb11d4f01803760c4b8cd3c55de0e4036ff15
Fly me to the moon <git@bigly.dog>
2020-06-05 13:44:27 -0700

    move zsh bin

--- files/zsh/shared/00/path.zsh
+++ files/zsh/shared/00/path.zsh
@@ -2,6 +2,6 @@
 #################### Path Region ####################
 #################### ########### ####################
 
-export PATH="$XDG_CONFIG_HOME/zsh/bin:$PATH"
+export PATH="$XDG_CONFIG_HOME/zsh/rc/bin:$PATH"
 export PATH="$XDG_CONFIG_HOME/scripts:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
