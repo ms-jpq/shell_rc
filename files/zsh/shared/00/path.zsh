f8bab4b53499edcce8b9723cfc13413baa7d91e8
Fly me to the moon <git@bigly.dog>
2020-06-05 14:52:27 -0700

    added path util

--- files/zsh/shared/00/path.zsh
+++ files/zsh/shared/00/path.zsh
@@ -5,3 +5,8 @@
 export PATH="$XDG_CONFIG_HOME/zsh/rc/bin:$PATH"
 export PATH="$XDG_CONFIG_HOME/scripts:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
+
+
+paths() {
+  export PATH="$(command paths "$1" "$2")"
+}
