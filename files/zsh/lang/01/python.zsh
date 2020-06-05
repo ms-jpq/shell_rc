934ee8287921668bbb09cea675b20b7dc82a02e1
Fly me to the moon <git@bigly.dog>
2020-06-05 16:41:00 -0700

    venv back

--- files/zsh/lang/01/python.zsh
+++ files/zsh/lang/01/python.zsh
@@ -23,3 +23,51 @@ alias python='python3'
 alias py='python3'
 alias pd='pydoc'
 alias srv='python3 -m http.server'
+
+
+#################### ########### ####################
+#################### Venv Region ####################
+#################### ########### ####################
+
+venv() {
+  local DEFAULT_VENV_PATH=".venv"
+  local ACTIVATE="$DEFAULT_VENV_PATH/bin/activate"
+  case "$1" in
+  init)
+    if [[ -d "$DEFAULT_VENV_PATH" ]]
+    then
+      echo "Virtualenv already initialized"
+    else
+      python3 -m venv "$DEFAULT_VENV_PATH"
+      echo "Initialized Virtualenv"
+      venv on
+    fi
+    ;;
+  on)
+    if [[ -f "$ACTIVATE" ]]
+    then
+      venv off
+      source "$ACTIVATE"
+      echo "Activated - $VIRTUAL_ENV"
+    else
+      echo "No Virtualenv found at - $ACTIVATE"
+    fi
+    ;;
+  off)
+    if [[ -n "$VIRTUAL_ENV" ]]
+    then
+      local VENV="$VIRTUAL_ENV"
+      if deactivate
+      then
+        echo "Deactivated - $VENV"
+      else
+        echo "Failed to deactivate - $VENV"
+      fi
+    fi
+    ;;
+  *)
+    echo "Invalid argument"
+    echo "venv - [init | on | off]"
+    ;;
+  esac
+}
