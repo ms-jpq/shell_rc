845f3d444d2337e5ebcff5fd1a5c6bebe5bf1484
Fly me to the moon <git@bigly.dog>
2020-06-06 12:21:32 -0700

    move to setup

---
+++ docker/setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eux
+set -o pipefail
+
+
+bash "$XDG_CONFIG_HOME/tmux/bin/tmux-init"
+touch "$XDG_CACHE_HOME/zz"
+
+
+pip3 install ranger-fm pynvim
+python3 "$XDG_CONFIG_HOME/ranger/bin/plug.py"
+VIM_INIT=1 nvim --headless
+
+
+mkdir "$XDG_CACHE_HOME/gitstatus"
+cd /_install
+wget https://github.com/romkatv/gitstatus/releases/download/v1.0.0/gitstatusd-linux-x86_64.tar.gz
+tar xzvf gitstatusd-linux-x86_64.tar.gz
+mv gitstatusd-linux-x86_64 "$XDG_CACHE_HOME/gitstatus/"
