3f69ce9e9fb0932c42f038d0e092583649b2f194
Fly me to the moon <git@bigly.dog>
2020-06-07 15:43:58 -0700

    headless update remote plugins

--- docker/setup.sh
+++ docker/setup.sh
@@ -11,6 +11,7 @@ touch "$XDG_CACHE_HOME/zz"
 pip3 install ranger-fm pynvim
 python3 "$XDG_CONFIG_HOME/ranger/bin/plug.py"
 VIM_INIT=1 nvim --headless
+nvim --headless +UpdateRemotePlugins +quit
 
 
 mkdir "$XDG_CACHE_HOME/gitstatus"
@@ -18,3 +19,4 @@ cd /_install
 wget https://github.com/romkatv/gitstatus/releases/download/v1.0.0/gitstatusd-linux-x86_64.tar.gz
 tar xzvf gitstatusd-linux-x86_64.tar.gz
 mv gitstatusd-linux-x86_64 "$XDG_CACHE_HOME/gitstatus/"
+
