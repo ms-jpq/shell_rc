4678a662d22e86e98d8c45bedf426ef951b3e695
Fly me to the moon <git@bigly.dog>
2020-06-07 23:52:29 -0700

    single quotes

--- docker/release.sh
+++ docker/release.sh
@@ -5,12 +5,13 @@ set -o pipefail
 
 cd "$(dirname "$0")/.."
 
-IMAGE="msjpq/cli"
+IMAGE='msjpq/cli'
 
-docker build -t "$IMAGE" . -f "docker/Dockerfile"
+docker build -t "$IMAGE" . -f 'docker/Dockerfile'
 
 
 if [[ $# -gt 1 ]]
 then
   docker push "$IMAGE"
 fi
+
