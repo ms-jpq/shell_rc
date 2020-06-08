594b545c64164e4d63c0ca9f9f805de322cdd2c1
Fly me to the moon <git@bigly.dog>
2020-06-08 00:11:14 -0700

    revert gitstatusd

--- docker/setup.sh
+++ docker/setup.sh
@@ -8,5 +8,13 @@ bash "$XDG_CONFIG_HOME/tmux/bin/tmux-init"
 python3 "$XDG_CONFIG_HOME/ranger/bin/ranger-init"
 bash "$XDG_CONFIG_HOME/nvim/bin/nvim-init"
 
+
 touch "$XDG_CACHE_HOME/zz"
+
+
+mkdir "$XDG_CACHE_HOME/gitstatus"
+cd /_install
+wget https://github.com/romkatv/gitstatus/releases/download/v1.0.0/gitstatusd-linux-x86_64.tar.gz
+tar xzvf gitstatusd-linux-x86_64.tar.gz
+mv gitstatusd-linux-x86_64 "$XDG_CACHE_HOME/gitstatus/"
 
