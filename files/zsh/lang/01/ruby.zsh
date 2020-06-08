3e93e50fb320165aa17a3a00462ceaedfcca5396
Fly me to the moon <git@bigly.dog>
2020-06-08 00:11:14 -0700

    revert gitstatusd

--- files/zsh/lang/01/ruby.zsh
+++ files/zsh/lang/01/ruby.zsh
@@ -2,10 +2,6 @@
 #################### Ruby Env Region ####################
 #################### ############### ####################
 
-export GEM_HOME="$XDG_DATA_HOME/gem"
-export GEM_SPEC_CACHE="$XDG_CACHE_HOME/gem"
-
-
 gem() {
   if rbenv local > /dev/null 2>&1
   then
