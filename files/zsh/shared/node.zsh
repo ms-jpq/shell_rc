86334e1af03a44cee249fee0a50ed8b06b422d98
Fly me to the moon <git@bigly.dog>
2020-06-11 11:54:27 -0700

    mv xdg

--- files/zsh/shared/node.zsh
+++ files/zsh/shared/node.zsh
@@ -2,10 +2,6 @@
 #################### Node Env Region ####################
 #################### ############### ####################
 
-export NODE_REPL_HISTORY="$XDG_CACHE_HOME/node_hist"
-export npm_config_cache="$XDG_CACHE_HOME/npm"
-
-
 np() {
   local LFS=$'\0'
   paths show 2>&1 | rg -F0 'node_modules' | while read -r p
--- files/zsh/shared/xdg.zsh
+++ files/zsh/shared/xdg.zsh
@@ -10,3 +10,6 @@ export NVIM_LOG_FILE="$XDG_CACHE_HOME/nvim.log"
 
 export GNUPGHOME="$XDG_DATA_HOME/gnupg"
 
+export NODE_REPL_HISTORY="$XDG_CACHE_HOME/node_hist"
+export npm_config_cache="$XDG_CACHE_HOME/npm"
+
