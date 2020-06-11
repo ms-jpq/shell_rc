775d5ac8771e043bbbd7ff6a398d4432cc565e50
Fly me to the moon <git@bigly.dog>
2020-06-11 08:14:37 -0700

    silent path

--- files/zsh/shared/ranger.zsh
+++
@@ -1,7 +0,0 @@
-#################### ############# ####################
-#################### Ranger Region ####################
-#################### ############# ####################
-
-alias ranger='ranger --cmd="set preview_files=true" --cmd="set use_preview_script=true"'
-alias ra='ranger'
-
