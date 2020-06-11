94bf4b0748c7655a1e963a020c256f02cb5834a3
Fly me to the moon <git@bigly.dog>
2020-06-11 13:38:58 -0700

    n

--- files/zsh/shared/z.zsh
+++ files/zsh/shared/z.zsh
@@ -12,7 +12,7 @@ z() {
   local B="$(sd '^[[\d|\.]|common:]+\s+' '' <<< "$A" | awk '!seen[$0]++')"
   if [[ -z "$B" ]]
   then
-    printf '%s' "no such file or directory: $*"
+    printf '%s\n' "no such file or directory: $*"
   else
     local C="$(fp -1 +s --tac <<< "$B")"
     cd "$C" || return 1
