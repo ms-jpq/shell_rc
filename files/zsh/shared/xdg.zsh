72a71a626eafd852036ab49975f34c1e901a20de
Fly me to the moon <git@bigly.dog>
2020-06-11 11:44:13 -0700

    mv

--- files/zsh/shared/xdg.zsh
+++ files/zsh/shared/xdg.zsh
@@ -4,9 +4,13 @@
 
 export LESSHISTFILE="$XDG_CACHE_HOME/less_hist"
 
-export NVIM_LOG_FILE="$XDG_CACHE_HOME/nvim.log"
+export _Z_DATA="$XDG_CACHE_HOME/zz"
 
 export WGETRC="$XDG_CONFIG_HOME/wgetrc"
+
+export NVIM_LOG_FILE="$XDG_CACHE_HOME/nvim.log"
+
+export ANSIBLE_CONFIG="$XDG_CONFIG_HOME/ansible.cfg"
 
 export GNUPGHOME="$XDG_DATA_HOME/gnupg"
 
