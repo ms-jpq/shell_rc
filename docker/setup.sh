62e5e58ff6b4034a02cc182984ae5607d27be8a4
Fly me to the moon <git@bigly.dog>
2020-06-11 11:12:28 -0700

    set upsh

--- docker/setup.sh
+++ docker/setup.sh
@@ -4,9 +4,9 @@ set -eux
 set -o pipefail
 
 
-bash "$XDG_CONFIG_HOME/tmux/bin/tmux-init"
-python3 "$XDG_CONFIG_HOME/ranger/bin/ranger-init"
-bash "$XDG_CONFIG_HOME/nvim/bin/nvim-init"
+"$XDG_CONFIG_HOME/tmux/bin/tmux-init"
+"$XDG_CONFIG_HOME/ranger/bin/ranger-init"
+"$XDG_CONFIG_HOME/nvim/bin/nvim-init"
 
 
 touch "$XDG_CACHE_HOME/zz"
