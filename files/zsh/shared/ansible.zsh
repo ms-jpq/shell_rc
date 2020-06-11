744cbd6fc3cd33310ed1df963cf7fd5e6ebf23b6
Fly me to the moon <git@bigly.dog>
2020-06-11 11:50:57 -0700

    mv

--- files/zsh/shared/ansible.zsh
+++ files/zsh/shared/ansible.zsh
@@ -2,6 +2,8 @@
 #################### Ansible Region ####################
 #################### ############## ####################
 
+export ANSIBLE_CONFIG="$XDG_CONFIG_HOME/ansible.cfg"
+
 alias a='ansible'
 alias ap='ansible-playbook'
 
