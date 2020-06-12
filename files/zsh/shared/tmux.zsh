8f8f900a1ae588c1514c661058313f30958374ee
Fly me to the moon <git@bigly.dog>
2020-06-12 11:22:30 -0700

    mv

--- files/zsh/shared/tmux.zsh
+++ files/zsh/shared/tmux.zsh
@@ -2,12 +2,6 @@
 #################### Tmux Region ####################
 #################### ########### ####################
 
-if [[ -n "$TMUX" ]]
-then
-  export PATH="$XDG_CONFIG_HOME/tmux/bin:$PATH"
-fi
-
-
 ta() {
   if [[ $# -ne 0 ]]
   then
