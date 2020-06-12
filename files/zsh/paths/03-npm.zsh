52287c8377e7728d4ea3573657f8c14d6fc62b63
Fly me to the moon <git@bigly.dog>
2020-06-12 11:34:39 -0700

    rm npm

--- files/zsh/paths/03-npm.zsh
+++
@@ -1,7 +0,0 @@
-#################### ########## ####################
-#################### NPM Region ####################
-#################### ########## ####################
-
-export NPM_GLOBAL_BIN="$XDG_DATA_HOME/npm_global/node_modules/.bin"
-export PATH="$NPM_GLOBAL_BIN:$PATH"
-
