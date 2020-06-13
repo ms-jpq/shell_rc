8ee875cac43334e48121d04518e343c31c376bdb
Fly me to the moon <git@bigly.dog>
2020-06-12 19:40:49 -0700

    added toggle

--- files/zsh/paths/04-venv.zsh
+++ files/zsh/paths/04-venv.zsh
@@ -4,16 +4,9 @@
 
 np() {
   local modules='node_modules/.bin'
-  local LFS=$'\0'
-  paths show 2>&1 | rg -F0 "$modules" | while read -r p
-  do
-    paths remove "$p"
-  done
-
-  local this="$modules"
-  if [[ -d "$this" ]]
+  if [[ -d "$modules" ]]
   then
-    paths add -r "$this"
+    paths toggle -r "$this"
   else
     printf '%s\n' 'RESET  -- npm PATH'
   fi
