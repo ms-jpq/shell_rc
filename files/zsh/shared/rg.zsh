a2258fc48ab88fcccabe0fd7dd5c9654ab5b36ad
Fly me to the moon <git@bigly.dog>
2020-06-23 07:32:22 -0700

    rg

--- files/zsh/shared/rg.zsh
+++ files/zsh/shared/rg.zsh
@@ -4,11 +4,3 @@
 
 export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/rg.conf"
 
-rgf() {
-  local OPTIONS='--color=always \
-                 --context=2 \
-                 --context-separator="$(hr "-" "$FZF_PREVIEW_COLUMNS")"'
-
-  rg "$@" --files-with-matches | fzf --preview="rg $OPTIONS $* {}"
-}
-
