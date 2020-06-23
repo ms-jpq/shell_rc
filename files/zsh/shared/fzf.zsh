7b361bef10e245c02eec79250cf9a33508456ca2
Fly me to the moon <git@bigly.dog>
2020-06-23 08:54:11 -0700

    ok

--- files/zsh/shared/fzf.zsh
+++ files/zsh/shared/fzf.zsh
@@ -24,15 +24,13 @@ export FZF_DEFAULT_OPTS="$FZF_COLOUR \
 
 export FZF_PREVIEW='preview {}'
 
-export FZF_FD_PREFIX='fd -HL'
-
-export FZF_DEFAULT_COMMAND="$FZF_FD_PREFIX -t f -t l"
+export FZF_DEFAULT_COMMAND='fd -t f -t l'
 export FZF_COMPLETION_OPTS="--preview='$FZF_PREVIEW'"
 
-export FZF_ALT_C_COMMAND="$FZF_FD_PREFIX -0 -I -t d"
+export FZF_ALT_C_COMMAND='fd -0 -I -t d'
 export FZF_ALT_C_OPTS="$FZF_COMPLETION_OPTS --read0"
 
-export FZF_CTRL_T_COMMAND="$FZF_FD_PREFIX -0"
+export FZF_CTRL_T_COMMAND='fd -0'
 export FZF_CTRL_T_OPTS="$FZF_COMPLETION_OPTS --read0"
 
 
@@ -44,7 +42,7 @@ alias fp="fzf --preview='$FZF_PREVIEW'"
 
 
 d() {
-  local dest="$(FZF_DEFAULT_COMMAND="$FZF_FD_PREFIX -0 -t d" fp --read0 -q "${*:-""}")"
+  local dest="$(FZF_DEFAULT_COMMAND='fd -0 -t d' fp --read0 -q "${*:-""}")"
   cd "$dest" || return 1
 }
 
@@ -54,12 +52,12 @@ d() {
 #################### ################ ####################
 
 _fzf_compgen_path() {
-  fd -HL -t d -t f -t l "$1"
+  fd -t d -t f -t l "$1"
 }
 
 
 _fzf_compgen_dir() {
-  fd -HL -t d "$1"
+  fd -t d "$1"
 }
 
 
