7b71cd0deb8d1f878072b029b640cb63aa1f0c92
Fly me to the moon <git@bigly.dog>
2020-06-26 21:49:03 -0700

    fmt

--- files/config/pythonrc.py
+++ files/config/pythonrc.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
-import atexit
-import os
 import readline
+from atexit import register
+from os import R_OK, access, environ
+from os.path import isfile, join
 from pprint import pprint
 
-__history_file__ = os.path.join(os.environ["XDG_CACHE_HOME"], "python_hist")
+__history_file__ = join(environ["XDG_CACHE_HOME"], "python_hist")
 __readline_write_history__ = readline.write_history_file
 
 readline.write_history_file = lambda *args: None
 
-if os.path.isfile(__history_file__) and os.access(__history_file__, os.R_OK):
-  readline.read_history_file(__history_file__)
+if isfile(__history_file__) and access(__history_file__, R_OK):
+    readline.read_history_file(__history_file__)
 
-atexit.register(__readline_write_history__, __history_file__)
+register(__readline_write_history__, __history_file__)
