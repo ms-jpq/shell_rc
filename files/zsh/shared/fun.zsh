497c77d438dfdf2d55d67d2bcc22e66bab9adb1d
Fly me to the moon <git@bigly.dog>
2020-06-27 13:46:23 -0700

    added pip

--- files/zsh/shared/fun.zsh
+++ files/zsh/shared/fun.zsh
@@ -6,14 +6,6 @@ alias shark='sudo termshark'
 
 alias fish='asciiquarium'
 
-alias ipinfo='curl https://ipinfo.io'
-
 alias foxd='dr browsh/browsh'
 
 alias icat='catimg'
-
-
-gay() {
-  local TEXT=${*:-"Fully Automated Luxury Gay Space Communism"}
-  figlet "$TEXT" | lolcat -a -d 1 -s 1000
-}
