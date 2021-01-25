048872c45c422c8199280475e421f95bd079ae66
Fly me to the moon <git@bigly.dog>
2021-01-24 21:55:59 -0800

    cls

--- files/zsh/paths/00-tmux.zsh
+++ files/zsh/paths/00-tmux.zsh
@@ -2,7 +2,11 @@
 #################### Tmux Region ####################
 #################### ########### ####################
 
+alias cls='clear'
+
 if [[ -n "$TMUX" ]]
 then
   pathprepend "$XDG_CONFIG_HOME/tmux/bin"
+
+  alias cls='clear && tmux clear-history'
 fi
