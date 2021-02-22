6ab8c437903247064978ebba8b7d82a259fb2a98
Fly me to the moon <git@bigly.dog>
2021-02-21 17:03:54 -0800

    k

--- docker/base/setup.sh
+++ docker/base/setup.sh
@@ -3,9 +3,10 @@
 set -eux
 set -o pipefail
 
+cd /_install || exit 1
+
 XDG_CACHE_HOME="$HOME/.cache"
 mkdir -p "$XDG_CACHE_HOME/gitstatus"
-cd /_install
 wget https://github.com/romkatv/gitstatus/releases/download/v1.0.0/gitstatusd-linux-x86_64.tar.gz
 tar xzvf gitstatusd-linux-x86_64.tar.gz
 mv gitstatusd-linux-x86_64 "$XDG_CACHE_HOME/gitstatus/"
