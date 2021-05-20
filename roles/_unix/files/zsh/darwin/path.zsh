05e39e12596a50835052d77dd5726d32b8a7888e
Fly me to the moon <git@bigly.dog>
2021-05-19 21:23:38 -0700

    ok

--- roles/_unix/files/zsh/darwin/path.zsh
+++ roles/_unix/files/zsh/darwin/path.zsh
@@ -1,23 +1,25 @@
+#!/usr/bin/env bash
+
 #################### ########### ####################
 #################### Path Region ####################
 #################### ########### ####################
 
 _paths=(
-  "/usr/local/opt/bc/bin"
-  "/usr/local/opt/coreutils/libexec/gnubin"
-  "/usr/local/opt/curl/bin"
-  "/usr/local/opt/findutils/libexec/gnubin"
-  "/usr/local/opt/gnu-getopt/bin"
-  "/usr/local/opt/gnu-sed/libexec/gnubin"
-  "/usr/local/opt/gnu-tar/libexec/gnubin"
-  "/usr/local/opt/gnu-which/libexec/gnubin"
-  "/usr/local/opt/grep/libexec/gnubin"
-  "/usr/local/opt/icu4c/bin"
-  "/usr/local/opt/icu4c/sbin"
-  "/usr/local/opt/lsof/bin"
-  "/usr/local/opt/ncurses/bin"
-  "/usr/local/opt/openssl/bin"
-  "/usr/local/sbin"
+  '/usr/local/opt/bc/bin'
+  '/usr/local/opt/coreutils/libexec/gnubin'
+  '/usr/local/opt/curl/bin'
+  '/usr/local/opt/findutils/libexec/gnubin'
+  '/usr/local/opt/gnu-getopt/bin'
+  '/usr/local/opt/gnu-sed/libexec/gnubin'
+  '/usr/local/opt/gnu-tar/libexec/gnubin'
+  '/usr/local/opt/gnu-which/libexec/gnubin'
+  '/usr/local/opt/grep/libexec/gnubin'
+  '/usr/local/opt/icu4c/bin'
+  '/usr/local/opt/icu4c/sbin'
+  '/usr/local/opt/lsof/bin'
+  '/usr/local/opt/ncurses/bin'
+  '/usr/local/opt/openssl/bin'
+  '/usr/local/sbin'
 )
 pathprepend "${_paths[@]}"
 unset _paths
--- roles/_unix/files/zsh/shared/git.zsh
+++ roles/_unix/files/zsh/shared/git.zsh
@@ -4,4 +4,15 @@
 
 export GIT_PAGER='delta'
 
-alias lg='LC_ALL=C lazygit'
+alias lg='lazygit'
+
+
+git-ssh() {
+  if [[ $# -eq 0 ]]
+  then
+    unset GIT_SSH_COMMAND
+  else
+    export GIT_SSH_COMMAND="$(command git-ssh "$@")"
+  fi
+}
+
