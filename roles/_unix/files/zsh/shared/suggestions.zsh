23cb2916396d96912e5f1fd3a5fad37df7898f02
Fly me to the moon <git@bigly.dog>
2021-05-19 22:46:30 -0700

    ok

--- roles/_unix/files/zsh/shared/suggestions.zsh
+++ roles/_unix/files/zsh/shared/suggestions.zsh
@@ -5,9 +5,9 @@
 #################### ################## ####################
 
 # INTI #
-export ZSH_AUTOSUGGEST_USE_ASYNC=true
-export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=4'
-export ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=(autosuggest-accept)
+ZSH_AUTOSUGGEST_USE_ASYNC=true
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=4'
+ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=(autosuggest-accept)
 source "$ZDOTDIR/../zsh-autosuggestions/zsh-autosuggestions.zsh"
 # INIT #
 
