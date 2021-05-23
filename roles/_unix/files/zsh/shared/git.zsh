6069564895d2979628d9fe374ef88dcbd71dcfdf
Fly me to the moon <git@bigly.dog>
2021-05-23 14:34:01 -0700

    ok

--- roles/_unix/files/zsh/shared/git.zsh
+++ roles/_unix/files/zsh/shared/git.zsh
@@ -8,11 +8,6 @@ alias lg='LC_ALL=C lazygit'
 
 
 git-ssh() {
-  if [[ $# -eq 0 ]]
-  then
-    unset GIT_SSH_COMMAND
-  else
-    export GIT_SSH_COMMAND="$(command git-ssh "$@")"
-  fi
+  source <(cmd git-ssh "$*")
 }
 
