28ab376294da4f0fd1dd4065a7a75c27ddb3050f
IIIIII <iiiiii@II.lan>
2021-12-30 07:20:40 -0800

    ok cool

--- docker/base/setup.sh
+++ docker/base/setup.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")" || exit 1
 
 
 XDG_CACHE_HOME="$HOME/.cache"
-mkdir -p "$XDG_CACHE_HOME/gitstatus"
-wget https://github.com/romkatv/gitstatus/releases/download/v1.0.0/gitstatusd-linux-x86_64.tar.gz
+mkdir -p -- "$XDG_CACHE_HOME/gitstatus"
+wget -- https://github.com/romkatv/gitstatus/releases/download/v1.0.0/gitstatusd-linux-x86_64.tar.gz
 tar xzvf gitstatusd-linux-x86_64.tar.gz
 mv gitstatusd-linux-x86_64 "$XDG_CACHE_HOME/gitstatus/"
