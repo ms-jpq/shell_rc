aecf2ac26c07ab8dbe814bee51dd552cdc20dcb5
IIIIII <github@bigly.dog>
2022-01-28 14:53:25 -0800

    ok

--- roles/_unix/files/zsh/shared/git.zsh
+++ roles/_unix/files/zsh/shared/git.zsh
@@ -4,11 +4,4 @@
 #################### Git Region ####################
 #################### ########## ####################
 
-export GIT_PAGER='delta'
-
 alias lg='lazygit'
-
-
-git-ssh() {
-  source <(cmd git-ssh "$@")
-}
