357d2b40716f083b7057bdee1ccc5b446693145a
IIIIII <github@bigly.dog>
2022-02-05 16:14:28 -0800

    ok

--- roles/_unix/files/zsh/paths/04-node.zsh
+++ roles/_unix/files/zsh/paths/04-node.zsh
@@ -6,14 +6,3 @@
 
 export NPM_GLOBAL_HOME="$XDG_DATA_HOME/npm_global"
 pathprepend "$NPM_GLOBAL_HOME/node_modules/.bin"
-
-
-np() {
-  local modules='node_modules/.bin'
-  if [[ -d "$modules" ]]
-  then
-    paths toggle -r "$modules"
-  else
-    printf '%s\n' 'RESET  -- npm PATH'
-  fi
-}
