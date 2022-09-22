eaf9546f3e36cd87517987636fbdb484f8e42746
IIIIII <git@bigly.dog>
2022-09-22 00:16:30 -0700

    try

--- roles/_unix/files/zsh/aposteriori/asdf.zsh
+++ roles/_unix/files/zsh/aposteriori/asdf.zsh
@@ -5,5 +5,7 @@
 #################### ########### ####################
 
 # INIT #
-ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdfrc" ASDF_DATA_DIR="$XDG_DATA_HOME/asdf" . "$ASDF_DATA_DIR/asdf.sh"
+export ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdfrc"
+export ASDF_DATA_DIR="$XDG_DATA_HOME/asdf"
+. "$ASDF_DATA_DIR/asdf.sh"
 # INIT #
