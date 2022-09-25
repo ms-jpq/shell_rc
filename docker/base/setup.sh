f224eeb27096a39eef7cb60028117570f4f8553d
IIIIII <git@bigly.dog>
2022-09-25 12:14:46 -0700

    p

--- docker/base/setup.sh
+++ docker/base/setup.sh
@@ -8,7 +8,7 @@ cd "$(dirname -- "$0")" || exit 1
 
 
 XDG_CACHE_HOME="$HOME/.cache"
-mkdir -p -- "$XDG_CACHE_HOME/gitstatus"
+mkdir --parent -- "$XDG_CACHE_HOME/gitstatus"
 wget -- https://github.com/romkatv/gitstatus/releases/latest/download/gitstatusd-linux-x86_64.tar.gz
 tar xzvf gitstatusd-linux-x86_64.tar.gz
 mv -- gitstatusd-linux-x86_64 "$XDG_CACHE_HOME/gitstatus/"
