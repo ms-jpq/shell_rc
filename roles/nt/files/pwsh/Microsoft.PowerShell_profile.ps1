8b0547d85b2d5767d109178814c24ca4b1dac68f
IIIIII <git@bigly.dog>
2022-10-01 18:18:49 -0700

    python links

--- roles/nt/files/pwsh/Microsoft.PowerShell_profile.ps1
+++ roles/nt/files/pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,16 @@
 Set-StrictMode -Version 'Latest'
 
-Set-PSReadLineOption -EditMode 'Emacs'
 
 function main {
-  $Env:Path = "$(Join-Path $HOME 'local' 'bin');$Env:Path"
+  Set-PSReadLineOption -EditMode 'Emacs'
+  Set-PSReadlineKeyHandler -Key 'Tab' -Function 'MenuComplete'
+
+  $Env:Path = "$(Join-Path $Env:LOCALAPPDATA 'bin');$Env:Path"
+  $Env:RIPGREP_CONFIG_PATH = Join-Path $Env:APPDATA 'rg.conf'
+
+  Invoke-Expression (&starship 'init' 'powershell')
 }
+
 
 Remove-Item -Path Function:main
 
-Invoke-Expression (&starship 'init' 'powershell')
