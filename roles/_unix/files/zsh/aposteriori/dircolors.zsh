4135ef51607e813a96a34bd3771dfd3f531c5796
IIIIII <git@bigly.dog>
2022-10-14 10:30:37 -0700

    dir colours

--- roles/_unix/files/zsh/aposteriori/dircolors.zsh
+++ roles/_unix/files/zsh/aposteriori/dircolors.zsh
@@ -4,5 +4,18 @@
 #################### LSCOLOR Region ####################
 #################### ############## ####################
 
-eval "$(dircolors --bourne-shell -- "$XDG_CONFIG_HOME/zsh/dircolors-solarized/dircolors.256dark")"
-# eval "$(dircolors --bourne-shell -- "$XDG_CONFIG_HOME/zsh/dircolors-solarized/dircolors.ansi-dark")"
+zsh_colours() {
+  local colour="$*"
+  local colours="$XDG_CACHE_HOME/$colour"
+
+  if [[ ! -f "$colours" ]]
+  then
+    dircolors --bourne-shell -- "$XDG_CONFIG_HOME/zsh/dircolors-solarized/$colour" > "$colours"
+  fi
+
+  source "$colours"
+}
+
+zsh_colours 'dircolors.256dark'
+# dircolors.ansi-dark
+unset -f zsh_colours
