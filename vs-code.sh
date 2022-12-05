44c0b42729e8a5fd51d1ab12f50099d27d4993c9
IIIIII <git@bigly.dog>
2022-12-05 08:32:07 -0800

    vsc

---
+++ vs-code.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -Eeu
+set -o pipefail
+shopt -s globstar nullglob
+
+
+cd -- "$(dirname -- "$0")" || exit 1
+
+DEST="$*"
+UNITS=()
+
+for unit in ./systemd/*
+do
+  UNITS+=("$(basename -- "$unit")")
+done
+
+rsync --recursive --links --perms --times --human-readable --info progress2 -- ./systemd/ "$DEST:/usr/local/lib/systemd/user/"
+
+ssh "$DEST" systemctl --user daemon-reload
+ssh "$DEST" systemctl --user enable -- "${UNITS[*]}"
+ssh "$DEST" systemctl --user start -- "${UNITS[*]}"
