a617f46a8cfba256ba1b40f56c00e28c55bb6a25
IIIIII <git@bigly.dog>
2022-12-09 09:12:44 -0800

    --

--- roles/_unix/files/home/.config/pipx-install.sh
+++ roles/_unix/files/home/.config/pipx-install.sh
@@ -7,7 +7,7 @@ shopt -s nullglob globstar
 cd -- "$(dirname -- "$0")" || exit 1
 
 
-readarray -d $'\n' -t REQUIREMENTS < "$XDG_CONFIG_HOME/requirements.txt"
+readarray -d $'\n' -t -- REQUIREMENTS < "$XDG_CONFIG_HOME/requirements.txt"
 
 python3 -m pip install --upgrade -- pipx
 
