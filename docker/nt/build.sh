603b758f03e9a50aab8ca1b0f76a01dd2dd4d709
IIIIII <git@bigly.dog>
2022-12-10 09:05:14 -0800

    rsa

--- docker/nt/build.sh
+++ docker/nt/build.sh
@@ -9,4 +9,7 @@ cd -- "$(dirname -- "$0")/../.." || exit 1
 
 TAG='windows-ansible'
 
+rm -f -- ./id_rsa*
+ssh-keygen -q -N '' -f ./id_rsa
+chmod 600 -- ./id_rsa*
 docker build --file ./docker/nt/Dockerfile --tag "$TAG" -- .
