b5ad672ceb422448b1e07fa3fb867450db5942ba
root <root@dev.t.home.arpa>
2022-12-12 21:11:38 +0000

    asdf

--- roles/posix/files/zsh/rc.d/aposteriori/asdf.zsh
+++ roles/posix/files/zsh/rc.d/aposteriori/asdf.zsh
@@ -4,6 +4,6 @@
 #################### ASDF Region ####################
 #################### ########### ####################
 
-ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdfrc"
-ASDF_DATA_DIR="$XDG_DATA_HOME/asdf"
+export ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdfrc"
+export ASDF_DATA_DIR="$XDG_DATA_HOME/asdf"
 source -- "$ASDF_DATA_DIR/asdf.sh"
