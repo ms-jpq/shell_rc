f24440485b9e14528bf12380ae8b2908b6747ce2
root <root@dev.t.home.arpa>
2022-12-12 20:50:31 +0000

    win

--- roles/posix/files/pwsh/Microsoft.PowerShell_profile.ps1
+++ roles/posix/files/pwsh/Microsoft.PowerShell_profile.ps1
@@ -2,4 +2,4 @@ Set-StrictMode -Version 'Latest'
 
 Set-PSReadLineOption -EditMode 'Emacs'
 Set-PSReadlineKeyHandler -Key 'Tab' -Function 'MenuComplete'
-oh-my-posh init pwsh --config (Join-Path -- "$LOCALAPPDATA" 'posh.yml') | Invoke-Expression
+oh-my-posh init pwsh --config (Join-Path -- ($IsWindows ? "$LOCALAPPDATA" : "$XDG_CONFIG_HOME") 'posh.yml') | Invoke-Expression
