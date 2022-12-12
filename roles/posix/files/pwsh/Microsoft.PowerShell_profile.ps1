d69c8e63ab957b6f012d0f0bc380a4439c50bc91
root <root@dev.t.home.arpa>
2022-12-12 21:15:52 +0000

    windows

--- roles/posix/files/pwsh/Microsoft.PowerShell_profile.ps1
+++ roles/posix/files/pwsh/Microsoft.PowerShell_profile.ps1
@@ -2,4 +2,15 @@ Set-StrictMode -Version 'Latest'
 
 Set-PSReadLineOption -EditMode 'Emacs'
 Set-PSReadlineKeyHandler -Key 'Tab' -Function 'MenuComplete'
-oh-my-posh init pwsh --config (Join-Path -- ($IsWindows ? "$LOCALAPPDATA" : "$XDG_CONFIG_HOME") 'posh.yml') | Invoke-Expression
+
+
+$Env:MSYSTEM = 'MSYS'
+$Env:Path = @(
+  Join-Path -- "$Env:APPDATA" 'bin'
+  "$Env:Path"
+  Join-Path -- "$Env:SystemDrive" 'msys64' 'ucrt64' 'bin'
+  Join-Path -- "$Env:SystemDrive" 'msys64' 'usr' 'bin'
+) | Join-String -Separator ';'
+
+
+oh-my-posh init pwsh --config (Join-Path -- ($IsWindows ? "$Env:LOCALAPPDATA" : "$XDG_CONFIG_HOME") 'posh.yml') | Invoke-Expression
