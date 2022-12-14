0dbca2547a977703f1e5a74246b36918139b707e
root <root@dev.t.home.arpa>
2022-12-14 06:50:01 +0000

    fix local

--- roles/posix/files/pwsh/Microsoft.PowerShell_profile.ps1
+++ roles/posix/files/pwsh/Microsoft.PowerShell_profile.ps1
@@ -4,13 +4,15 @@ Set-PSReadLineOption -EditMode 'Emacs'
 Set-PSReadlineKeyHandler -Key 'Tab' -Function 'MenuComplete'
 
 
-$Env:MSYSTEM = 'MSYS'
-$Env:Path = @(
-  Join-Path -- "$Env:APPDATA" 'bin'
-  "$Env:Path"
-  Join-Path -- "$Env:SystemDrive" 'msys64' 'ucrt64' 'bin'
-  Join-Path -- "$Env:SystemDrive" 'msys64' 'usr' 'bin'
-) | Join-String -Separator ';'
+if ($IsWindows) {
+  $Env:MSYSTEM = 'MSYS'
+  $Env:Path = @(
+    Join-Path -- "$Env:APPDATA" 'bin'
+    "$Env:Path"
+    Join-Path -- "$Env:SystemDrive" 'msys64' 'ucrt64' 'bin'
+    Join-Path -- "$Env:SystemDrive" 'msys64' 'usr' 'bin'
+  ) | Join-String -Separator ';'
+}
 
 
-oh-my-posh init pwsh --config (Join-Path -- ($IsWindows ? "$Env:LOCALAPPDATA" : "$XDG_CONFIG_HOME") 'posh.yml') | Invoke-Expression
+oh-my-posh init pwsh --config (Join-Path -- ($IsWindows ? "$Env:LOCALAPPDATA" : "$Env:XDG_CONFIG_HOME") 'posh.yml') | Invoke-Expression
