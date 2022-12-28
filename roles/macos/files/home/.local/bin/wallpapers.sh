d9a44252b3f0e0a8e66ce46867e16bc23d8253e0
IIIIII <git@bigly.dog>
2022-12-28 15:47:11 -0800

    wallpapers

---
+++ roles/macos/files/home/.local/bin/wallpapers.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -Eeu
+set -o pipefail
+shopt -s globstar nullglob
+
+
+ARGS=(
+  rsync
+  --recursive 
+  --links 
+  --perms 
+  --times 
+  --human-readable 
+  --info progress2 
+  --delete 
+  -- 
+  root@freenas.t.home.arpa:/media/wallpapers/
+  "$HOME/Library/Mobile Documents/com~apple~CloudDocs/桌面背景/"
+  )
+
+exec -- "${ARGS[@]}"
