6efc17b9e5c766eb0145e00619efbd4188a1c410
dog <git@bigly.dog>
2023-01-28 20:52:46 -0800

    pwsh

--- conn.ps1
+++ conn.ps1
@@ -5,83 +5,85 @@ $ErrorActionPreference = 'Stop'
 
 
 function basic {
-  # UTC BIOS
-  Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation' -Name 'RealTimeIsUniversal' -Type 'DWord' -Value 1
+    # UTC BIOS
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation' -Name 'RealTimeIsUniversal' -Type 'DWord' -Value 1
 
-  # Allow Scripting
-  Set-ExecutionPolicy -ExecutionPolicy 'Unrestricted'
+    # Allow Scripting
+    Set-ExecutionPolicy -ExecutionPolicy 'Unrestricted'
 
-  # Private Firewall
-  Set-NetConnectionProfile -NetworkCategory 'Private'
+    # Private Firewall
+    Set-NetConnectionProfile -NetworkCategory 'Private'
 
-  # Allow Ping
-  Set-NetFirewallRule -DisplayName 'File and Printer Sharing (Echo Request - ICMPv4-In)' -Enabled "$true"
-  Set-NetFirewallRule -DisplayName 'File and Printer Sharing (Echo Request - ICMPv6-In)' -Enabled "$true"
+    # Allow Ping
+    Set-NetFirewallRule -DisplayName 'File and Printer Sharing (Echo Request - ICMPv4-In)' -Enabled "$true"
+    Set-NetFirewallRule -DisplayName 'File and Printer Sharing (Echo Request - ICMPv6-In)' -Enabled "$true"
 
-  # NoPassword
-  Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa' -Name 'LimitBlankPasswordUse' -Type 'DWord' -Value 0
+    # NoPassword
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa' -Name 'LimitBlankPasswordUse' -Type 'DWord' -Value 0
 
-  # Disable Defrag
-  Disable-ScheduledTask -TaskName 'Microsoft\Windows\Defrag\ScheduledDefrag'
+    # Disable Defrag
+    Disable-ScheduledTask -TaskName 'Microsoft\Windows\Defrag\ScheduledDefrag'
 }
 
 
 function winrm {
-  Set-Service -Name 'WinRM' -StartupType 'Automatic' -Status 'Running'
+    Set-Service -Name 'WinRM' -StartupType 'Automatic' -Status 'Running'
 
-  Set-Item -Path 'WSMan:\localhost\Service\Auth\Basic' -Value $true
-  Set-Item -Path 'WSMan:\localhost\Service\AllowUnencrypted' -Value $true
+    Set-Item -Path 'WSMan:\localhost\Service\Auth\Basic' -Value $true
+    Set-Item -Path 'WSMan:\localhost\Service\AllowUnencrypted' -Value $true
 }
 
 
 function ssh {
-  Add-WindowsCapability -Online -Name 'OpenSSH.Server'
-  Set-Service -Name 'sshd' -StartupType 'Automatic' -Status 'Running'
+    Add-WindowsCapability -Online -Name 'OpenSSH.Server'
+    Set-Service -Name 'sshd' -StartupType 'Automatic' -Status 'Running'
 
-  $WIN_ADMIN = if (!(Test-Path variable:global:IsWindows) -Or $IsWindows) {
-    $C_USER = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $USER_OBJ = New-Object Security.Principal.WindowsPrincipal($C_USER)
-    $USER_OBJ.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-  } else {
-    0
-  }
+    $WIN_ADMIN = if (!(Test-Path variable:global:IsWindows) -Or $IsWindows) {
+        $C_USER = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+        $USER_OBJ = New-Object Security.Principal.WindowsPrincipal($C_USER)
+        $USER_OBJ.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    }
+    else {
+        0
+    }
 
-  $SSH_KEY_DEST = if ($WIN_ADMIN) {
-    Join-Path "$ENV:PROGRAMDATA" 'ssh' | Join-Path -ChildPath 'administrators_authorized_keys'
-  } else {
-    Join-Path '~' '.ssh' | Join-Path -ChildPath 'authorized_keys'
-  }
-  Write-Output "$SSH_KEY_DEST"
+    $SSH_KEY_DEST = if ($WIN_ADMIN) {
+        Join-Path "$ENV:PROGRAMDATA" 'ssh' | Join-Path -ChildPath 'administrators_authorized_keys'
+    }
+    else {
+        Join-Path '~' '.ssh' | Join-Path -ChildPath 'authorized_keys'
+    }
+    Write-Output "$SSH_KEY_DEST"
 
-  $SSH_KEY_SRC = New-TemporaryFile
-  $GITHUB_USER = Read-Host -Prompt 'Github User'
-  Invoke-WebRequest -OutFile $SSH_KEY_SRC -Uri "https://github.com/$GITHUB_USER.keys"
-  Move-Item -Force -Path $SSH_KEY_SRC -Destination $SSH_KEY_DEST
+    $SSH_KEY_SRC = New-TemporaryFile
+    $GITHUB_USER = Read-Host -Prompt 'Github User'
+    Invoke-WebRequest -OutFile $SSH_KEY_SRC -Uri "https://github.com/$GITHUB_USER.keys"
+    Move-Item -Force -Path $SSH_KEY_SRC -Destination $SSH_KEY_DEST
 
 
-  if ($WIN_ADMIN) {
-    $ACL = Get-Acl $SSH_KEY_DEST
-    $ACL.SetAccessRuleProtection($true, $false)
-    $ADMIN_RULE = New-Object system.security.accesscontrol.filesystemaccessrule('Administrators', 'FullControl', 'Allow')
-    $SYS_RULE = New-Object system.security.accesscontrol.filesystemaccessrule('SYSTEM', 'FullControl', 'Allow')
-    $ACL.SetAccessRule($ADMIN_RULE)
-    $ACL.SetAccessRule($SYS_RULE)
-    $ACL | Set-Acl
+    if ($WIN_ADMIN) {
+        $ACL = Get-Acl $SSH_KEY_DEST
+        $ACL.SetAccessRuleProtection($true, $false)
+        $ADMIN_RULE = New-Object system.security.accesscontrol.filesystemaccessrule('Administrators', 'FullControl', 'Allow')
+        $SYS_RULE = New-Object system.security.accesscontrol.filesystemaccessrule('SYSTEM', 'FullControl', 'Allow')
+        $ACL.SetAccessRule($ADMIN_RULE)
+        $ACL.SetAccessRule($SYS_RULE)
+        $ACL | Set-Acl
 
-    Write-Output '--> ACL SET'
-  }
+        Write-Output '--> ACL SET'
+    }
 }
 
 
 function rdp {
-  # No Sleep on AC
-  POWERCFG /SETACVALUEINDEX SCHEME_CURRENT SUB_NONE CONSOLELOCK 0
+    # No Sleep on AC
+    POWERCFG /SETACVALUEINDEX SCHEME_CURRENT SUB_NONE CONSOLELOCK 0
 
-  # RDP On
-  Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name 'fDenyTSConnections' -Type 'DWord' -Value 0
+    # RDP On
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name 'fDenyTSConnections' -Type 'DWord' -Value 0
 
-  # RDP Firewall
-  Enable-NetFirewallRule -Name 'RemoteDesktop*'
+    # RDP Firewall
+    Enable-NetFirewallRule -Name 'RemoteDesktop*'
 }
 
 
--- roles/nt/files/home/AppData/Roaming/WinInstall.ps1
+++ roles/nt/files/home/AppData/Roaming/WinInstall.ps1
@@ -4,12 +4,13 @@ Set-StrictMode -Version 'Latest'
 $ErrorActionPreference = 'Stop'
 
 foreach ($pkg in $args) {
-  winget list --id $pkg
-  if ($?) {
-    winget upgrade --id $pkg
-  } else {
-    winget install --accept-package-agreements --accept-source-agreements --id $pkg
-  }
+    winget list --id $pkg
+    if ($?) {
+        winget upgrade --id $pkg
+    }
+    else {
+        winget install --accept-package-agreements --accept-source-agreements --id $pkg
+    }
 }
 
 exit $LASTEXITCODE
