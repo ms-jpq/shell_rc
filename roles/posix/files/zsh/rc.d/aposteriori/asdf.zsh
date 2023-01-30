63a4ebb4ade28dffce975ebd0efbdcf90dece52f
dog <git@bigly.dog>
2023-01-30 14:41:43 -0800

    pretty

--- roles/posix/files/zsh/rc.d/aposteriori/asdf.zsh
+++ roles/posix/files/zsh/rc.d/aposteriori/asdf.zsh
@@ -4,7 +4,6 @@
 #################### ASDF Region ####################
 #################### ########### ####################
 
-export -- ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdfrc"
 export -- ASDF_DATA_DIR="$XDG_DATA_HOME/asdf"
 # shellcheck disable=SC1091
 source -- "$ASDF_DATA_DIR/asdf.sh"
