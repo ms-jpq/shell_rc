2988128578404a905fee80486f5becfc8ed92c6f
dog <git@bigly.dog>
2023-01-30 14:41:43 -0800

    pretty

--- roles/macos/files/home/.local/bin/wallpapers.sh
+++ roles/macos/files/home/.local/bin/wallpapers.sh
@@ -4,19 +4,18 @@ set -Eeu
 set -o pipefail
 shopt -s globstar nullglob
 
-
 ARGS=(
   rsync
-  --recursive 
-  --links 
-  --perms 
-  --times 
-  --human-readable 
-  --info progress2 
-  --delete 
-  -- 
+  --recursive
+  --links
+  --perms
+  --times
+  --human-readable
+  --info progress2
+  --delete
+  --
   root@freenas.t.home.arpa:/media/wallpapers/
   "$HOME/Library/Mobile Documents/com~apple~CloudDocs/桌面背景/"
-  )
+)
 
 exec -- "${ARGS[@]}"
