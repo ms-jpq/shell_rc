3b28909e9450a9bfca9f20fd43ceb2d6d117fde2
IIIIII <git@bigly.dog>
2023-01-30 14:21:17 -0800

    sep

--- roles/posix/files/pwsh/Microsoft.PowerShell_profile.ps1
+++ roles/posix/files/pwsh/Microsoft.PowerShell_profile.ps1
@@ -7,12 +7,12 @@ Set-PSReadLineKeyHandler -Key 'Tab' -Function 'MenuComplete'
 if ($IsWindows) {
     $Env:MSYSTEM = 'MSYS'
     $Env:Path = @(
-        Join-Path -- "$Env:APPDATA" 'bin'
-        "$Env:Path"
-        Join-Path -- "$Env:SystemDrive" 'msys64' 'ucrt64' 'bin'
-        Join-Path -- "$Env:SystemDrive" 'msys64' 'usr' 'bin'
-    ) | Join-String -Separator ';'
+        Join-Path -- $Env:APPDATA 'bin'
+        $Env:Path
+        Join-Path -- $Env:SystemDrive 'msys64' 'ucrt64' 'bin'
+        Join-Path -- $Env:SystemDrive 'msys64' 'usr' 'bin'
+    ) | Join-String -Separator ([IO.Path]::PathSeparator)
 }
 
 
-oh-my-posh init pwsh --config (Join-Path -- ($IsWindows ? "$Env:LOCALAPPDATA" : "$Env:XDG_CONFIG_HOME") 'posh.yml') | Invoke-Expression
+oh-my-posh init pwsh --config (Join-Path -- ($IsWindows ? $Env:LOCALAPPDATA : $Env:XDG_CONFIG_HOME) 'posh.yml') | Invoke-Expression
