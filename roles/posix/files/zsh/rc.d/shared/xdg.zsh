8f036c5ace0956cff063e77503def419717ec484
IIIIII <git@bigly.dog>
2023-05-04 13:08:59 -0700

    bash fake

--- roles/posix/files/zsh/rc.d/shared/xdg.zsh
+++ roles/posix/files/zsh/rc.d/shared/xdg.zsh
@@ -9,3 +9,4 @@ export -- WGETRC="$XDG_CONFIG_HOME/wgetrc"
 export -- GNUPGHOME="$XDG_DATA_HOME/gnupg"
 
 export -- NODE_REPL_HISTORY="$XDG_CACHE_HOME/node_hist"
+export -- NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npmrc"
