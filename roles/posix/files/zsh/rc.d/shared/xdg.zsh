b68b15ad97ba668e1f7d182ff38be3bf70bf9b51
IIIIII <git@bigly.dog>
2023-05-05 13:37:24 -0700

    dbg

--- roles/posix/files/zsh/rc.d/shared/xdg.zsh
+++ roles/posix/files/zsh/rc.d/shared/xdg.zsh
@@ -4,9 +4,10 @@
 #################### XDG Region ####################
 #################### ########## ####################
 
+export -- GNUPGHOME="$XDG_DATA_HOME/gnupg"
 export -- WGETRC="$XDG_CONFIG_HOME/wgetrc"
 
-export -- GNUPGHOME="$XDG_DATA_HOME/gnupg"
+export -- RUBY_DEBUG_HISTORY_FILE="$XDG_CACHE_HOME/rdbg_history"
 
 export -- NODE_REPL_HISTORY="$XDG_CACHE_HOME/node_hist"
 export -- NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npmrc"
