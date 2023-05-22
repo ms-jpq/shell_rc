d6449678f426fa775402a14121d4c57d6baf3e19
IIIIII <git@bigly.dog>
2023-05-21 21:43:17 -0700

    irbrc

--- roles/posix/files/zsh/rc.d/shared/xdg.zsh
+++ roles/posix/files/zsh/rc.d/shared/xdg.zsh
@@ -7,7 +7,7 @@
 export -- GNUPGHOME="$XDG_DATA_HOME/gnupg"
 export -- WGETRC="$XDG_CONFIG_HOME/wgetrc"
 
+export -- PYTHONSTARTUP="$XDG_CONFIG_HOME/pythonrc.py"
 export -- RUBY_DEBUG_HISTORY_FILE="$XDG_CACHE_HOME/rdbg_history"
-
 export -- NODE_REPL_HISTORY="$XDG_CACHE_HOME/node_hist"
 export -- NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npmrc"
