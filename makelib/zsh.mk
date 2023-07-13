b6b32ae145e439ce8ab40fb319bc90be225ca543
IIIIII <git@bigly.dog>
2023-07-13 08:32:36 -0700

    less make

--- makelib/zsh.mk
+++ makelib/zsh.mk
@@ -3,31 +3,18 @@ all: zshrc
 
 ZSH := $(shell find ./zsh)
 
-$(TMP)/dircolors: $(TMP)
-	if [[ -d '$@' ]]; then
-		cd -- '$@'
-		git pull --force
-	else
-		git clone -- 'https://github.com/seebi/dircolors-solarized' '$@'
-	fi
-	touch -- '$@'
-
-$(TMP)/dircolors.sh: $(TMP)/dircolors
-	dircolors --bourne-shell -- '$</dircolors.256dark' > '$@'
-	touch -- '$<' '$@'
+# $(TMP)/dircolors.sh: $(TMP)/dircolors
+# 	dircolors --bourne-shell -- '$</dircolors.256dark' > '$@'
+# 	touch -- '$<' '$@'
 
 define ZSH_TEMPLATE
 
 zshrc: $$(TMP)/$(1)/home.fs/.config/zsh
+$$(TMP)/$(1)/home.fs.tar: $$(TMP)/$(1)/home.fs/.config/zsh
 
-$$(TMP)/$(1)/home.fs/.config/zsh: ./libexec/zsh.sh $(TMP)/dircolors.sh $(ZSH)
+$$(TMP)/$(1)/home.fs/.config/zsh: ./libexec/zsh.sh $(ZSH)
 	'$$<' --os $(1) --out '$$@'
-	cat -- '$(TMP)/dircolors.sh' >> '$$@/.zshrc'
-	touch -- '$(TMP)/dircolors' '$(TMP)/dircolors.sh' '$$@'
 
 endef
 
 $(foreach os,$(OS),$(eval $(call ZSH_TEMPLATE,$(os))))
-
-zshrc:
-	touch -- '$(TMP)/dircolors' '$(TMP)/dircolors.sh' $^
