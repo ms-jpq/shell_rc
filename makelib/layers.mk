72fb6ce4fc0a837b1b26da8cbaf75897df5bd65d
IIIIII <git@bigly.dog>
2023-07-13 00:38:33 -0700

    xdg

--- makelib/layers.mk
+++ makelib/layers.mk
@@ -1,5 +1,7 @@
 .PHONY: tar
 
+RSYNC := rsync --mkpath --recursive --links --perms --times
+
 define FS_TEMPLATE
 
 $(1)_FS_REL := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/fs/~/.*')
@@ -8,11 +10,10 @@ $(1)_FS_ABS := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/fs/root/.
 $(1)_LINK_REL := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/link/~/.*')
 $(1)_LINK_ABS := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/link/root/.*')
 
-$$(TMP)/$(1)/home.link: $$($(1)_LINK_REL)
-	rsync -- './layers/posix/fs/~' './layers/$(1)/fs/~' '$$@'
-	rsync --mkpath --recursive --links --perms --times -- '$$@/'
+$$(TMP)/$(1)/home.fs: $$($(1)_FS_REL)
+	$$(RSYNC) -- './layers/posix/fs/~/' './layers/$(1)/fs/~/' '$$@'
 
-tar: $$(TMP)/$(1)/home.link
+tar: $$(TMP)/$(1)/home.fs
 
 endef
 
