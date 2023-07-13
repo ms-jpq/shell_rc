d8be1cbe1ecdf5e34c007c522333ac88e502cda7
IIIIII <git@bigly.dog>
2023-07-13 06:59:44 -0700

    rename

--- makelib/layers.mk
+++ makelib/layers.mk
@@ -1,24 +1,27 @@
 .PHONY: tar
 
+DIRS := root home
 RSYNC := rsync --mkpath --recursive --links --perms --times
 
 define FS_TEMPLATE
 
-$(1)_FS_REL := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/fs/~/.*')
-$(1)_FS_ABS := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/fs/root/.*')
+$(1)_FS_$(2) := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/fs/$(2)/.*')
+$(1)_LINK_$(2) := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/link/$(2)/.*')
 
-$(1)_LINK_REL := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/link/~/.*')
-$(1)_LINK_ABS := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/link/root/.*')
+$$(TMP)/$(1)/$(2).link: $$($(1)_LINK_$(2))
+	LAYERS=(./layers/{posix,$(1)}/link/$(2))
+	for layer in "$$$${LAYERS[@]}"; do
+		if [[ -d ""$$$$layer"" ]]; then
+			$$(RSYNC) "$$$$layer" '$$@/'
+		fi
+	done
 
-$$(TMP)/$(1)/home.fs: $$($(1)_FS_REL)
-	$$(RSYNC) -- './layers/posix/fs/~/' './layers/$(1)/fs/~/' '$$@'
+$$(TMP)/$(1)/$(2).link.tar: $$(TMP)/$(1)/$(2).link
+	mkdir -p -- '$$<'
+	tar -c -C '$$<' -f '$$@' .
 
-tar: $$(TMP)/$(1)/home.fs
+tar: $$(TMP)/$(1)/$(2).link.tar
 
 endef
 
-$(foreach os,$(OS),$(eval $(call FS_TEMPLATE,$(os))))
-
-
-echo:
-	@echo '$(darwin_FS_REL)'
+$(foreach dir,$(DIRS),$(foreach os,$(OS),$(eval $(call FS_TEMPLATE,$(os),$(dir)))))
