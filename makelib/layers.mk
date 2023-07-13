6d1a2d7b0268df724bc9559ca57210ac906eded9
IIIIII <git@bigly.dog>
2023-07-13 07:29:00 -0700

    layer all

--- makelib/layers.mk
+++ makelib/layers.mk
@@ -1,27 +1,28 @@
 .PHONY: tar
 
+KINDS = link fs
 DIRS := root home
 RSYNC := rsync --mkpath --recursive --links --perms --times
 
 define FS_TEMPLATE
 
-$(1)_FS_$(2) := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/fs/$(2)/.*')
-$(1)_LINK_$(2) := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/link/$(2)/.*')
+$(1)_$(2)_$(3) := $$(shell find ./layers -regex './layers/\(posix\|$(1)\)/$(2)/$(3)/.*')
 
-$$(TMP)/$(1)/$(2).link: $$($(1)_LINK_$(2))
-	LAYERS=(./layers/{posix,$(1)}/link/$(2))
+$$(TMP)/$(1)/$(3).$(2): $$($(1)_$(2)_$(3))
+	LAYERS=(./layers/{posix,$(1)}/$(2)/$(3))
 	for layer in "$$$${LAYERS[@]}"; do
 		if [[ -d ""$$$$layer"" ]]; then
 			$$(RSYNC) "$$$$layer" '$$@/'
 		fi
 	done
 
-$$(TMP)/$(1)/$(2).link.tar: $$(TMP)/$(1)/$(2).link
+$$(TMP)/$(1)/$(3).$(2).tar: $$(TMP)/$(1)/$(3).$(2)
 	mkdir -p -- '$$<'
 	tar -c -C '$$<' -f '$$@' .
 
-tar: $$(TMP)/$(1)/$(2).link.tar
+tar: $$(TMP)/$(1)/$(3).$(2).tar
 
 endef
 
-$(foreach dir,$(DIRS),$(foreach os,$(OS),$(eval $(call FS_TEMPLATE,$(os),$(dir)))))
+$(foreach kind,$(KINDS),$(foreach dir,$(DIRS),$(foreach os,$(OS),$(eval $(call FS_TEMPLATE,$(os),$(kind),$(dir))))))
+
