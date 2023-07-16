8f2cacfcea54e1f496bd5e9c9225d82a511e2402
IIIIII <git@bigly.dog>
2023-07-16 14:27:54 -0700

    ppa uri

--- layers/ubuntu/fs/home/.local/libexec/add-ppa.sh
+++ layers/ubuntu/fs/home/.local/libexec/add-ppa.sh
@@ -2,11 +2,29 @@
 
 set -o pipefail
 
+hash -- curl jq gpg
+
+# shellcheck disable=SC1091
+source -- /etc/os-release gpg
+
 PPA="$1"
 
 DIST="${PPA%%/*}"
 NAME="${PPA##*/}"
 
+FS_NAME="ppa_${DIST}_$NAME"
+GPG_FILE="/etc/apt/trusted.gpg.d/$FS_NAME.gpg"
+SOURCE_FILE="/etc/apt/sources.list.d/$FS_NAME.list"
+
+CURL=(curl --fail --location --no-progress-meter --)
+
 URI="https://api.launchpad.net/1.0/~$DIST/+archive/$NAME"
-JSON="$(curl --fail --location --no-progress-meter -- "$URI")"
+JSON="$("${CURL[@]}" "$URI")"
 FINGER_PRINT="$(jq --exit-status --raw-output '.signing_key_fingerprint' <<<"$JSON")"
+URI="https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x$FINGER_PRINT"
+"${CURL[@]}" "$URI" | gpg --batch --dearmor --yes --output "$GPG_FILE"
+
+# shellcheck disable=SC2154
+tee -- "$SOURCE_FILE" <<-EOF
+deb http://ppa.launchpad.net/neovim-ppa/unstable/ubuntu $VERSION_CODENAME main
+EOF
