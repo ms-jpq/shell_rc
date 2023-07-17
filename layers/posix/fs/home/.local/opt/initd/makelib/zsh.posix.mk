38c1ce6e88ba88a5523316aa481fe7b61411c54c
IIIIII <git@bigly.dog>
2023-07-17 01:23:39 -0700

    dir colours

--- layers/posix/fs/home/.local/opt/initd/makelib/zsh.posix.mk
+++ layers/posix/fs/home/.local/opt/initd/makelib/zsh.posix.mk
@@ -6,7 +6,15 @@ zsh.posix: $(CACHE)/zsh/dircolors.sh $(HOME)/.local/state/zsh/zz
 
 $(CACHE)/zsh/dircolors.sh: $(OPT)/dircolors-solarized pkg.posix
 	mkdir -p -- '$(CACHE)/zsh'
-	dircolors --bourne-shell -- '$</dircolors.256dark' > '$@'
+	case "$$OSTYPE" in
+	darwin*)
+		DC="$(brew --prefix)/opt/coreutils/libexec/gnubin/dircolors"
+		;;
+	*)
+		DC='dircolors'
+		;;
+	esac
+	"$DC" --bourne-shell -- '$</dircolors.256dark' > '$@'
 
 $(HOME)/.local/state/zsh/zz:
 	mkdir -p -- '$(HOME)/.local/state/zsh'
