a937dd7297d7122912c8e9c04101e02bd1075622
IIIIII <git@bigly.dog>
2023-07-17 02:26:58 -0700

    no os

--- layers/posix/fs/home/.local/opt/initd/makelib/tmux.posix.mk
+++ layers/posix/fs/home/.local/opt/initd/makelib/tmux.posix.mk
@@ -1,6 +1,10 @@
 .PHONY: tmux
 
-# tmux: $(SHARE)/tmux
+ifneq (aarch64-linux, $(MACHTYPE)-$(OS))
+
+tmux: $(SHARE)/tmux
+
+endif
 
 $(SHARE)/tmux: $(CONFIG)/tmux/requirements.txt | pkg.posix
 	/usr/bin/python3 -m venv -- '$@'
