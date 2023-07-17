d354a417f74fbce1a2c5d0b7d7780a9da5104764
IIIIII <git@bigly.dog>
2023-07-17 06:31:35 -0700

    no windows for now

--- makelib/build.mk
+++ makelib/build.mk
@@ -1,6 +1,15 @@
 .PHONY: build build.ubuntu
 
+ifeq (x86_64-nt, $(MACHTYPE)-$(OS))
+build: build.nt
+else
 build: build.ubuntu
+endif
 
 build.ubuntu: ./docker/ubuntu.Dockerfile
+	@echo $(MACHTYPE)-$(OS)
 	docker buildx build --progress plain --tag 'shell' --file '$<' -- .
+
+build.nt: ./docker/nt.Dockerfile
+	@echo $(MACHTYPE)-$(OS)
+	docker build --progress plain --tag 'shell' --file '$<' -- .
