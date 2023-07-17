297d7352e046cd10d1207c71c10de8a12d0ac4a8
IIIIII <git@bigly.dog>
2023-07-17 00:37:57 -0700

    ok

--- layers/ubuntu/fs/home/.local/opt/initd/makelib/pkg.ubuntu.mk
+++ layers/ubuntu/fs/home/.local/opt/initd/makelib/pkg.ubuntu.mk
@@ -1,11 +1,11 @@
 .PHONY: apt-deps
 	
-APT_INSTALL := DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --yes
+APT_INSTALL := DEBIAN_FRONTEND=noninteractive sudo -- apt-get install --no-install-recommends --yes
 APT_DEPS := /etc/ssl/certs/ca-certificates.crt /usr/bin/curl /usr/bin/gpg /usr/bin/jq /usr/bin/git /usr/bin/unzip
 
 apt-deps:
 	APT=(ca-certificates curl gnupg jq git unzip)
-	apt-get update
+	sudo -- apt-get update
 	$(APT_INSTALL) -- "$${APT[@]}"
 
 $(APT_DEPS): apt-deps
@@ -20,4 +20,4 @@ pkg.posix: /etc/apt/sources.list.d/ubuntu_partner.list /etc/apt/sources.list.d/p
 	EOF
 
 /etc/apt/sources.list.d/ppa_neovim-ppa_unstable.list: $(APT_DEPS)
-	./libexec/add-ppa.sh neovim-ppa/unstable
+	sudo -- ./libexec/add-ppa.sh neovim-ppa/unstable
--- layers/ubuntu/fs/home/.local/opt/initd/makelib/zsh.ubuntu.mk
+++ layers/ubuntu/fs/home/.local/opt/initd/makelib/zsh.ubuntu.mk
@@ -5,13 +5,13 @@ zsh: zsh.ubuntu /usr/local/bin/bat /usr/local/bin/fd $(BIN)/oh-my-posh
 /usr/bin/zsh /bin/batcat /bin/fdfind: pkg.posix
 
 /usr/local/bin/bat: /bin/batcat
-	ln -sf -- '$<' '$@'
+	sudo -- ln -sf -- '$<' '$@'
 
 /usr/local/bin/fd: /bin/fdfind
-	ln -sf -- '$<' '$@'
+	sudo -- ln -sf -- '$<' '$@'
 
 $(BIN)/oh-my-posh: $(BIN)/posh-linux-$(GOARCH)
-	ln -sf -- '$<' '$@'
+	sudo -- ln -sf -- '$<' '$@'
 
 zsh.ubuntu: /usr/bin/zsh
 	if [[ "$$SHELL" != '$<' ]]; then
