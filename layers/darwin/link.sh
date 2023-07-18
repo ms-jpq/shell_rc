ceb7f7b4b13c99d026dbb38a0f32f4d36100a4da
IIIIII <iiiiii@I6.lan>
2023-07-17 20:12:50 -0700

    build

--- layers/darwin/link.sh
+++ layers/darwin/link.sh
@@ -3,3 +3,11 @@
 set -o pipefail
 
 cd -- ~
+
+mkdir -p -- "$HOME/.local"
+
+ln -sf -- "$HOME/Applications" "$HOME/.local/opt"
+ln -sf -- "$HOME/Library/Application Support" "$HOME/.local/share"
+ln -sf -- "$HOME/Library/Caches" "$HOME/.cache"
+ln -sf -- "$HOME/Library/Caches" "$HOME/.local/state"
+ln -sf -- "$HOME/Library/Preferences" "$HOME/.config"
--- layers/nt/link.sh
+++ layers/nt/link.sh
@@ -3,3 +3,11 @@
 set -o pipefail
 
 cd -- ~
+
+mkdir -p -- "$HOME/.local"
+
+ln -sf -- "$HOME/AppData/Local" "$HOME/.local/share"
+ln -sf -- "$HOME/AppData/LocalHigh" "$HOME/.local/opt"
+ln -sf -- "$HOME/AppData/LocalLow" "$HOME/.cache"
+ln -sf -- "$HOME/AppData/LocalLow" "$HOME/.local/state"
+ln -sf -- "$HOME/AppData/Roaming" "$HOME/.config"
