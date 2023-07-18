183c5f743bb7113c9809a7cfaae68ae4c89962b7
IIIIII <iiiiii@I6.lan>
2023-07-17 22:56:28 -0700

    makemake

--- layers/ubuntu/home/.local/opt/initd/makelib/pkg.ubuntu.mk
+++ layers/ubuntu/home/.local/opt/initd/makelib/pkg.ubuntu.mk
@@ -1,14 +1,13 @@
-.PHONY: apt-deps
-	
 APT_INSTALL := DEBIAN_FRONTEND=noninteractive sudo --preserve-env -- apt-get install --no-install-recommends --yes
-APT_DEPS := /etc/ssl/certs/ca-certificates.crt /usr/bin/curl /usr/bin/gpg /usr/bin/jq /usr/bin/git /usr/bin/unzip
+APT_DEPS := /usr/bin/curl /usr/bin/gpg /usr/bin/jq /usr/bin/git /usr/bin/unzip
 
-apt-deps:
+/etc/ssl/certs/ca-certificates.crt:
 	APT=(ca-certificates curl gnupg jq git unzip)
 	sudo -- apt-get update
 	$(APT_INSTALL) -- "$${APT[@]}"
+	touch -- '$@'
 
-$(APT_DEPS): apt-deps
+$(APT_DEPS): /etc/ssl/certs/ca-certificates.crt
 
 pkg.posix: /etc/apt/sources.list.d/ubuntu_partner.list /etc/apt/sources.list.d/ppa_neovim-ppa_unstable.list
 
