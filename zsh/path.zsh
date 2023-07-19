07cad375d4544bee9b79ab6c93bb0223bebe8b2b
IIIIII <github@reeee.ca>
2023-07-18 22:39:02 -0700

    compq

---
+++ zsh/path.zsh
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S -- bash
+
+fpath=("$ZDOTDIR/fn" "${fpath[@]}")
+
+autoload -Uz -- compinit
+
+# shellcheck disable=SC1036,SC2120
+_comp_init() {
+  # shellcheck disable=SC2154
+  local -- dump="$XDG_CACHE_HOME/zsh/zcompdump" f=()
+  # shellcheck disable=SC2034
+  f=("$dump"(Nm-6))
+  if (($#f)); then
+    f=(-C)
+  fi
+  compinit -i "${f[@]}" -d "$dump"
+}
+
+_comp_init
+unset -f _comp_init
