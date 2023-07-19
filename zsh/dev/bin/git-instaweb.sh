8a0b10ee2b7fc74dff75c97992538b9fa5128b5e
IIIIII <iiiiii@I6.lan>
2023-07-19 03:55:03 -0700

    insta web

---
+++ zsh/dev/bin/git-instaweb.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+export -- GIT_EXEC_PATH GIT_DIR
+
+GIT_EXEC_PATH="$(git --exec-path)"
+GIT_DIR="$(git --no-optional-locks rev-parse --path-format=absolute --git-common-dir)"
+WEB_DIR="$GIT_DIR/gitweb"
+export -- GITWEB_CONFIG="$WEB_DIR/gitweb_config.perl"
+TMP="$WEB_DIR/tmp"
+CGI_BIN="$WEB_DIR/cgi-bin"
+
+INFO="$(git --info-path)"
+WEB="${INFO%/*}/gitweb"
+CGI="$WEB/gitweb.cgi"
+STATIC="$WEB/static"
+
+SRV="$(realpath -- "${0%/*}/../libexec/git-instaweb.py")"
+ROOT="$(git --no-optional-locks rev-parse --show-toplevel)"
+NAME="${ROOT##*/}"
+
+link() {
+  local -- src="$1" dst="$2"
+
+  if ! [[ -L "$dst" ]]; then
+    ln -sf -- "$src" "$dst"
+  fi
+}
+
+mkdir -p -- "$CGI_BIN" "$TMP"
+link "$STATIC" "$CGI_BIN/static"
+link "$CGI" "$CGI_BIN/gitweb.cgi"
+
+read -r -d '' -- PERL <<-EOF || true
+our \$projectroot = "$ROOT";
+our \$git_temp = "$TMP";
+our \$projects_list = \$projectroot;
+
+# ADDN
+our \$prevent_xss = !0;
+our \$omit_owner = !0;
+our \$omit_age_column = !0;
+our \$fallback_encoding = "utf-16";
+our \$site_name = "$NAME";
+
+\$feature{'remote_heads'}{'default'} = [1];
+EOF
+
+printf -- '%s\n' "$PERL" >"$GITWEB_CONFIG"
+
+cd -- "$WEB_DIR"
+exec -- "$SRV"
---
+++ zsh/dev/libexec/git-instaweb.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env -S -- PYTHONSAFEPATH= python3
+
+from contextlib import suppress
+from http import HTTPStatus
+from http.server import CGIHTTPRequestHandler, ThreadingHTTPServer
+from pathlib import PurePosixPath
+from posixpath import normpath, sep
+from socket import IPPROTO_IPV6, IPV6_V6ONLY, AddressFamily, getfqdn
+from socketserver import TCPServer
+from sys import stderr
+from typing import Any
+from urllib.parse import urlsplit
+from webbrowser import open
+
+_POSIX_ROOT = PurePosixPath(sep)
+
+_CGI_BIN = _POSIX_ROOT
+_CGI_SCRIPT = _POSIX_ROOT / "cgi-bin" / "gitweb.cgi"
+
+
+def _path(Handler: CGIHTTPRequestHandler) -> PurePosixPath:
+    return PurePosixPath(urlsplit(Handler.path).path)
+
+
+def _redirect(handler: CGIHTTPRequestHandler) -> bool:
+    if _path(handler) == _POSIX_ROOT:
+        handler.send_response_only(HTTPStatus.SEE_OTHER)
+        handler.send_header("Location", normpath(_CGI_SCRIPT))
+        handler.end_headers()
+        return True
+    else:
+        return False
+
+
+def _rewrite(handler: CGIHTTPRequestHandler) -> None:
+    if _path(handler) == _CGI_BIN:
+        handler.path = normpath(_POSIX_ROOT)
+
+
+class _Handler(CGIHTTPRequestHandler):
+    def is_cgi(self) -> bool:
+        return _path(self).is_relative_to(_CGI_SCRIPT) and super().is_cgi()
+
+    def do_HEAD(self) -> None:
+        if not _redirect(self):
+            _rewrite(self)
+            super().do_HEAD()
+
+    def do_GET(self) -> None:
+        if not _redirect(self):
+            _rewrite(self)
+            super().do_GET()
+
+    def do_POST(self) -> None:
+        if not _redirect(self):
+            _rewrite(self)
+            super().do_POST()
+
+    def log_message(self, format: str, *args: Any) -> None:
+        ...
+
+
+class _Server(ThreadingHTTPServer):
+    def server_bind(self) -> None:
+        if self.socket.family == AddressFamily.AF_INET6:
+            self.socket.setsockopt(IPPROTO_IPV6, IPV6_V6ONLY, 0)
+        TCPServer.server_bind(self)
+        _, server_port, *_ = self.socket.getsockname()
+        self.server_name = getfqdn()
+        self.server_port = server_port
+
+
+with suppress(KeyboardInterrupt):
+    srv = _Server(("", 0), _Handler)
+    addr = f"http://{srv.server_name}:{srv.server_port}"
+    open(addr)
+    print(addr, file=stderr)
+    srv.serve_forever()
