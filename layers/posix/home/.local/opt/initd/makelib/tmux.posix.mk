3da064f3b9d8fe2148d90facee03f50b4d542046
IIIIII <iiiiii@I6.lan>
2023-07-19 11:30:53 -0700

    less

--- layers/posix/home/.local/opt/initd/makelib/tmux.posix.mk
+++ layers/posix/home/.local/opt/initd/makelib/tmux.posix.mk
@@ -4,7 +4,7 @@ ifneq (aarch64-linux, $(MACHTYPE)-$(OS))
 tmux: $(SHARE)/tmux
 endif
 
-$(SHARE)/tmux: $(CONFIG)/tmux/requirements.txt
-	/usr/bin/python3 -m venv -- '$@'
+$(SHARE)/tmux:
+	/usr/bin/python3 -m venv --upgrade -- '$@'
 	'$@/bin/pip' install --require-virtualenv --upgrade --requirement '$<'
 	touch -- '$@'
