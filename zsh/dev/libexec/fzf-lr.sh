8c563a9daff7f9a00bb380d5061a5de4b29a7bb2
IIIIII <iiiiii@I6.lan>
2023-07-19 07:01:51 -0700

    ok

--- zsh/dev/libexec/fzf-lr.sh
+++ zsh/dev/libexec/fzf-lr.sh
@@ -13,7 +13,7 @@ ARGV=(
   --preview-window=right:70%:wrap
   --read0
   # --print0
-  "--bind=return:accept+execute:$EXECUTE_HEAD{+f}"
+  "--bind=return:become:$EXECUTE_HEAD{+f}"
   "--preview=$PREVIEW_HEAD{+f}"
 )
 
