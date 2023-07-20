32d608a999ce5e7381a5cd546345c67a8795b97e
IIIIII <iiiiii@I6.lan>
2023-07-19 21:35:29 -0700

    :d

--- layers/nt/winget.ps1
+++ layers/nt/winget.ps1
@@ -10,8 +10,19 @@ if (!$has_pkg) {
     Add-AppxPackage -Path 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx'
 
     $releases = Invoke-RestMethod -Uri 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
-    $latest = $releases.assets | Where-Object { $_.browser_download_url.EndsWith('msixbundle') } | Select-Object -First 1
+    $bundle = $releases.assets | Where-Object { $_.browser_download_url.EndsWith('msixbundle') } | Select-Object -First 1
+    $license = $releases.assets | Where-Object { $_.browser_download_url.EndsWith('License1.xml') } | Select-Object -First 1
 
-    Write-Host -- $latest.browser_download_url
-    Add-AppxPackage -Path $latest.browser_download_url
+    Write-Host -- $bundle
+    Write-Host -- $license
+    
+    $t_bundle = $bundle.browser_download_url | Split-Path -Leaf
+    $t_license = $license.browser_download_url | Split-Path -Leaf
+
+    Invoke-WebRequest -Uri $bundle.browser_download_url -OutFile $t_bundle
+    Invoke-WebRequest -Uri $license.browser_download_url -OutFile $t_license
+
+    Add-AppxProvisionedPackage -Online -PackagePath $t_bundle -LicensePath $t_license
+
+    winget.exe list --accept-source-agreements
 }
