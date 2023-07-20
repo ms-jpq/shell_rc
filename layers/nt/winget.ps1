8636c8d3ab33f2ea06fcc502117ee656db5b5650
IIIIII <iiiiii@I6.lan>
2023-07-19 21:01:24 -0700

    :d

--- layers/nt/winget.ps1
+++ layers/nt/winget.ps1
@@ -7,7 +7,7 @@ $has_pkg = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
 
 if (!$has_pkg) {
     Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
-    Add-AppxPackage -Path 'https://github.com/microsoft/microsoft-ui-xaml/releases/latest/download/Microsoft.UI.Xaml.2.8.x64.appx'
+    Add-AppxPackage -Path 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx'
 
     $releases = Invoke-RestMethod -Uri 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
     $latest = $releases.assets | Where-Object { $_.browser_download_url.EndsWith('msixbundle') } | Select-Object -First 1
