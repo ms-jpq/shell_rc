e578e2eb199ca1275979799d963d4158322f423c
IIIIII <github@bigly.dog>
2023-07-28 08:59:01 -0700

    man2html

--- zsh/iso/fn/woman.sh
+++ zsh/iso/fn/woman.sh
@@ -3,7 +3,7 @@
 woman() {
   local -- page tmp
   tmp="$(mktemp -d)/$*.html"
-  page="$(man -P tee -- "$*")"
+  page="$(LC_ALL=C.UTF-8 man -P tee -- "$*")"
   man2html -compress -title "MAN - $*" <<<"$page" >"$tmp"
   open "$tmp"
 }
