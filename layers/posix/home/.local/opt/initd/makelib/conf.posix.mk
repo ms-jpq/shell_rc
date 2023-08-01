36624f3f85101cac8a27604dce8f4efb74267cb2
IIIIII <github@bigly.dog>
2023-08-01 09:33:34 -0700

    v9

--- layers/posix/home/.local/opt/initd/makelib/conf.posix.mk
+++ layers/posix/home/.local/opt/initd/makelib/conf.posix.mk
@@ -5,7 +5,7 @@ conf.posix: $(CONFIG)/git/config conf.posix.ssh
 
 $(CONFIG)/git/config: | /usr/bin/git
 	touch -- '$@'
-	'$|' config --global include.path user_config
+	git config --global include.path user_config
 
 conf.posix.ssh:
 	./libexec/line-in-file.sh '$(HOME)/.ssh/config' 'Include ~/.config/ssh/*.conf'
