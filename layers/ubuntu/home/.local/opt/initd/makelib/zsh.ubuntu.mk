ed20744df5cf86ae2a5435aa37bc217d85457db3
IIIIII <github@bigly.dog>
2023-08-01 07:02:06 -0700

    v3

--- layers/ubuntu/home/.local/opt/initd/makelib/zsh.ubuntu.mk
+++ layers/ubuntu/home/.local/opt/initd/makelib/zsh.ubuntu.mk
@@ -5,13 +5,13 @@ zsh: zsh.ubuntu /usr/local/bin/bat /usr/local/bin/fd $(BIN)/oh-my-posh
 /usr/bin/zsh /bin/batcat /bin/fdfind: pkg.posix
 
 /usr/local/bin/bat: /bin/batcat
-	sudo -- ln -sf -- '$<' '$@'
+	sudo -- ln -v -sf -- '$<' '$@'
 
 /usr/local/bin/fd: /bin/fdfind
-	sudo -- ln -sf -- '$<' '$@'
+	sudo -- ln -v -sf -- '$<' '$@'
 
 $(BIN)/oh-my-posh: $(BIN)/posh-linux-$(GOARCH)
-	ln -sf -- '$<' '$@'
+	ln -v -sf -- '$<' '$@'
 
 zsh.ubuntu: /usr/bin/zsh
 	if [[ "$$SHELL" != '$<' ]]; then
