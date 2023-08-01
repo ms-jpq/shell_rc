cdba72611ffc1cbcbe592bf216d610bb96c42a6e
IIIIII <github@bigly.dog>
2023-08-01 10:55:16 -0700

    v13

--- layers/ubuntu/home/.local/opt/initd/makelib/pkg.ubuntu.mk
+++ layers/ubuntu/home/.local/opt/initd/makelib/pkg.ubuntu.mk
@@ -9,7 +9,7 @@ APT_DEPS := /etc/ssl/certs/ca-certificates.crt /usr/bin/curl /usr/bin/gpg /usr/b
 
 $(APT_DEPS): | /usr/bin/unzip
 
-$(SHARE)/tmux $(NVIM_MVP): | /usr/share/doc/python3-venv
+$(SHARE)/tmux $(NVIM_MVP) $(OPT)/pipx: | /usr/share/doc/python3-venv
 
 pkg.posix: /etc/apt/sources.list.d/ubuntu_partner.list /etc/apt/sources.list.d/ppa_neovim-ppa_unstable.list
 
