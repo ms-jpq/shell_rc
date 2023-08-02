92b85b19c0d5a537692f39090ec83cab8a8ad82f
IIIIII <github@bigly.dog>
2023-08-02 12:57:06 -0700

    try 2 part

--- layers/nt/winget.ps1
+++ layers/nt/winget.ps1
@@ -7,8 +7,17 @@ Set-PSDebug -Trace 1
 $has_pkg = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
 
 if (!$has_pkg) {
-    Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
-    Add-AppxPackage -Path 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx'
+    $vc_libs = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+    $xaml_ui = 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx'
+
+    $t_vc_libs = $vc_libs | Split-Path -Leaf
+    $t_xaml_ui = $xaml_ui | Split-Path -Leaf
+
+    Invoke-WebRequest -Uri $vc_libs -OutFile $t_vc_libs
+    Invoke-WebRequest -Uri $xaml_ui -OutFile $t_xaml_ui
+
+    Add-AppxPackage -Path $t_vc_libs
+    Add-AppxPackage -Path $t_xaml_ui
 
     $releases = Invoke-RestMethod -Uri 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
     $bundle = $releases.assets | Where-Object { $_.browser_download_url.EndsWith('msixbundle') } | Select-Object -First 1
