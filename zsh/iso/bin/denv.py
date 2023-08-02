032d002520a25045a9b595e41cd2e387c5ee61e7
IIIIII <github@bigly.dog>
2023-08-02 00:29:11 -0700

    sh

--- zsh/iso/bin/denv.py
+++ zsh/iso/bin/denv.py
@@ -55,19 +55,22 @@ def _parse(text: str) -> Iterator[Tuple[str, Optional[str]]]:
 
 def _subst(val: str, env: Mapping[str, str]) -> str:
     def cont() -> Iterator[str]:
-        lex = shlex(val, posix=True)
-        lex.whitespace = ""
-        acc: MutableSequence[str] = []
+        if val.startswith("'") and val.endswith("'"):
+            yield val[1:-1]
+        else:
+            lex = shlex(val, posix=True)
+            lex.whitespace = ""
+            acc: MutableSequence[str] = []
 
-        for token in lex:
-            if token.isspace():
-                yield Template("".join(acc)).substitute(env)
-                acc.clear()
-                yield token
-            else:
-                acc.append(token)
+            for token in lex:
+                if token.isspace():
+                    yield Template("".join(acc)).substitute(env)
+                    acc.clear()
+                    yield token
+                else:
+                    acc.append(token)
 
-        yield Template("".join(acc)).substitute(env)
+            yield Template("".join(acc)).substitute(env)
 
     try:
         parsed = "".join(cont())
