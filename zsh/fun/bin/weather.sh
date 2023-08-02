37c0cb6162db788049b335020f87bccdb41ba9cd
IIIIII <github@bigly.dog>
2023-08-01 23:17:28 -0700

    weather

---
+++ zsh/fun/bin/weather.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+URIS=(
+  'https://wttr.in?m&format=4'
+  'https://wttr.in?mT'
+  'https://v2.wttr.in?mT'
+)
+
+URI="${URIS[$((RANDOM % ${#URIS[@]}))]}"
+
+CURL=(
+  curl
+  --fail
+  --location
+  --no-progress-meter
+)
+
+if [[ -v LC_ALL ]]; then
+  CURL+=(--header "Accept-Language: ${LC_ALL%_*}")
+fi
+
+CURL+=(-- "$URI")
+
+exec -- "${CURL[@]}"
