d5fc07e1cbb3ed8f95883652f28a9a9a72cfa962
IIIIII <github@bigly.dog>
2023-08-03 09:07:23 -0700

    windows

--- layers/ubuntu/home/.local/opt/initd/makelib/pkg.ubuntu.mk
+++ layers/ubuntu/home/.local/opt/initd/makelib/pkg.ubuntu.mk
@@ -1,3 +1,5 @@
+all: pipx
+
 APT_INSTALL := DEBIAN_FRONTEND=noninteractive sudo --preserve-env -- apt-get install --no-install-recommends --yes
 APT_DEPS := /etc/ssl/certs/ca-certificates.crt /usr/bin/curl /usr/bin/gpg /usr/bin/jq /usr/bin/git /usr/share/doc/python3-venv
 
