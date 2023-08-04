b3320ea4d51cf13b17383b935ec3e924d32ab7b5
IIIIII <github@bigly.dog>
2023-08-04 00:28:53 -0700

    autocd

--- zsh/iso/fn/bash.sh
+++ zsh/iso/fn/bash.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env -S -- bash
 
 # shellcheck disable=SC2154
-HISTFILE="$XDG_STATE_HOME/shell_history/bash" command -- bash -O dotglob -O nullglob -O extglob -O globstar "$@"
+HISTFILE="$XDG_STATE_HOME/shell_history/bash" command -- bash -O dotglob -O nullglob -O extglob -O globstar -O autocd "$@"
