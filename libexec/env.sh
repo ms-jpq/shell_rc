6a3119006bd45ed8fcb5a4cbd4ed70c6dcd1e23a
IIIIII <github@bigly.dog>
2023-08-05 23:09:02 -0700

    restore

--- libexec/env.sh
+++ libexec/env.sh
@@ -6,18 +6,22 @@ case "$OSTYPE" in
 linux*)
   # shellcheck disable=SC1091
   source -- /etc/os-release
+  HOMEDRIVE='/'
   NPROC="$(nproc)"
   MEMINFO_KB="$(awk '/MemTotal/ { print $2 }' </proc/meminfo)"
   MEMINFO=$((MEMINFO_KB * 1024))
-  MAKE="$(command -v -- gmake)"
+  MAKE="$(command -v -- gmake || true)"
+  MAKE="${MAKE:-"gmake"}"
   ;;
 darwin*)
+  HOMEDRIVE='/'
   ID="$(sw_vers -productName)"
   VERSION_ID="$(sw_vers -productVersion)"
   VERSION_CODENAME="$VERSION_ID"
   NPROC="$(sysctl -n hw.physicalcpu)"
   MEMINFO="$(sysctl -n hw.memsize)"
-  MAKE="$(command -v -- gmake)"
+  MAKE="$(command -v -- gmake || true)"
+  MAKE="${MAKE:-"gmake"}"
   ;;
 msys)
   if command -v -- pwsh >/dev/null; then
@@ -36,14 +40,14 @@ msys)
   }
 
   # shellcheck disable=2154
-  HOME="$HOMEDRIVE$HOMEPATH"
+  HOME="$HOMEPATH"
   ID="$(wmic os get Caption | trim)"
   VERSION_ID="$(wmic os get Version | trim)"
   VERSION_CODENAME="$VERSION_ID"
   NPROC="$("$PSH" -command '(Get-WmiObject -Class Win32_Processor).NumberOfCores')"
   MEMINFO="$(wmic ComputerSystem get TotalPhysicalMemory | trim)"
   # shellcheck disable=SC2154
-  MAKE="$SystemDrive/msys2/usr/bin/make"
+  MAKE="$SYSTEMDRIVE/msys64/usr/bin/make"
   ;;
 *)
   exit 1
@@ -52,6 +56,7 @@ esac
 
 tee <<-EOF
 ENV_HOME=$(printf -- '%q' "$HOME")
+ENV_HOMEDRIVE=$(printf -- '%q' "$HOMEDRIVE")
 ENV_HOSTNAME=$(printf -- '%q' "$HOSTNAME")
 ENV_HOSTTYPE=$(printf -- '%q' "$HOSTTYPE")
 ENV_ID=$(printf -- '%q' "$ID")
@@ -63,3 +68,5 @@ ENV_OSTYPE=$(printf -- '%q' "$OSTYPE")
 ENV_VERSION_CODENAME=$(printf -- '%q' "$VERSION_CODENAME")
 ENV_VERSION_ID=$(printf -- '%q' "$VERSION_ID")
 EOF
+
+env >&2
--- makelib/layers.mk
+++ makelib/layers.mk
@@ -2,9 +2,7 @@
 all: layers
 
 DIRS := root home
-$(warning $(RSYNC))
 RSYNC ?= rsync
-$(warning $(RSYNC))
 RSY := $(RSYNC) --recursive --links --perms --keep-dirlinks
 
 define FS_TEMPLATE
