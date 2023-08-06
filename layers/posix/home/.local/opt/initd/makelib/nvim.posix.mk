5ac281a5aef955ad68fd51c08362d5777e1289a3
IIIIII <github@bigly.dog>
2023-08-06 10:59:33 -0700

    homeie

--- layers/posix/home/.local/opt/initd/makelib/nvim.posix.mk
+++ layers/posix/home/.local/opt/initd/makelib/nvim.posix.mk
@@ -5,4 +5,4 @@ NVIM_MVP := $(CONFIG)/nvim/pack/modules/start/chadtree
 nvim: $(NVIM_MVP)
 
 $(NVIM_MVP): | $(CONFIG)/nvim
-	'$(GMAKE)' --directory '$(call UNIX_2_NT,$(CONFIG)/nvim)' -- mvp 'HOME=$(HOME)'
+	'$(GMAKE)' --directory '$(call UNIX_2_NT,$(CONFIG)/nvim)' -- mvp 'HOME=$(HOME)' 'USERPROFILE=$(HOME)'
