9e1209738c285dc955c2e5091c3062142cdf4193
IIIIII <github@bigly.dog>
2023-08-08 20:49:29 -0700

    better bash

--- zsh/apriori/history.bash
+++ zsh/apriori/history.bash
@@ -2,3 +2,6 @@
 
 # shellcheck disable=SC2154
 HISTFILE="$XDG_STATE_HOME/shell_history/bash"
+HISTCONTROL=ignoredups
+
+shopt -s -- histappend
--- zsh/apriori/scripting.zsh
+++ zsh/apriori/scripting.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S -- bash
 
-setopt interactivecomments # allow comments in prompt
-setopt multios             # auto tee
-setopt prompt_subst        # auto expansion at commandline
+setopt -- interactivecomments # allow comments in prompt
+setopt -- multios             # auto tee
+setopt -- prompt_subst        # auto expansion at commandline
