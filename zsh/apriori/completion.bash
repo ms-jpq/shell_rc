fff79f4c67b770fe5c0ba55c0b4986ae990c921f
IIIIII <github@bigly.dog>
2023-08-08 22:22:33 -0700

    comments

--- zsh/apriori/completion.bash
+++ zsh/apriori/completion.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S -- bash
 
-bind -- 'TAB:menu-complete'
-bind -- 'set completion-ignore-case on'
-bind -- 'set mark-symlinked-directories on'
-bind -- 'set show-all-if-ambiguous on'
+bind -- 'TAB:menu-complete'                 # Tab complete
+bind -- 'set completion-ignore-case on'     # Case insensitive
+bind -- 'set mark-symlinked-directories on' # Mark symlinked directories with '/'
+bind -- 'set show-all-if-ambiguous on'      # Show all if ambiguous
