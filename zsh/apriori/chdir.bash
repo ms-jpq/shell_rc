51ff9c8a579f8890726f21fb8b2e51971c100a9d
IIIIII <github@bigly.dog>
2023-08-08 22:22:33 -0700

    comments

--- zsh/apriori/chdir.bash
+++ zsh/apriori/chdir.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S -- bash
 
-shopt -s -- autocd
-shopt -s -- cdspell
-shopt -s -- dirspell
-shopt -s -- cdable_vars
+shopt -s -- autocd      # cd to a directory without typing cd
+shopt -s -- cdspell     # correct spelling errors in cd
+shopt -s -- dirspell    # correct spelling errors in dir names
+shopt -s -- cdable_vars # cd to a variable
