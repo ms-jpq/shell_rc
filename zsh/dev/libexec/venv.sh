91a8d777fd63588840f75296f9503ec42232d184
IIIIII <github@bigly.dog>
2023-08-10 11:16:53 -0700

    venv ok

--- zsh/dev/libexec/venv.sh
+++ zsh/dev/libexec/venv.sh
@@ -2,92 +2,98 @@
 
 set -o pipefail
 
-VIRTUAL_ENV="${VIRTUAL_ENV:-}"
+VIRTUAL_ENV="${VIRTUAL_ENV:-""}"
 
 export -- DEFAULT_VENV_PATH="${DEFAULT_VENV_PATH:-"${2:-"$PWD/.venv"}"}"
 
-PY_EXE="$DEFAULT_VENV_PATH/bin/python3"
+case "$OSTYPE" in
+msys)
+  PY_EXE="$DEFAULT_VENV_PATH/Scripts/python.exe"
+  ;;
+*)
+  PY_EXE="$DEFAULT_VENV_PATH/bin/python3"
+  ;;
+esac
+
 ACTIVATE="$DEFAULT_VENV_PATH/bin/activate"
 
 OP="$1"
-shift
+shift -- 1
 
 read -r -d '' -- PYPROJ <<-'PYTHON' || true
+from collections.abc import Iterator
 from itertools import chain
+from os import execl
+from os.path import normcase
 from pathlib import Path
-from subprocess import check_call
-from sys import executable
-from typing import Union
-
+from shlex import quote
+from sys import executable, stderr
 from tomllib import load
 
-pyproject = Path("pyproject.toml")
 
+def _argv() -> Iterator[str | Path]:
+    cwd = Path.cwd()
+    pyproject = cwd / "pyproject.toml"
 
-def pip(*argv: Union[str, Path]) -> None:
-    check_call(
-        (executable, "-m", "pip", "install", "--upgrade", "--require-virtualenv", *argv)
-    )
+    for path in cwd.glob("requirements*.txt"):
+        yield from ("--requirement", path)
+    yield "--"
 
-
-if pyproject.is_file():
-    with pyproject.open("rb") as fd:
-        toml = load(fd)
-
-    if project := toml.get("project"):
-        deps = tuple(
-            chain(
-                project.get("dependencies", ()),
-                chain.from_iterable(project.get("optional-dependencies", {}).values()),
-            )
+    if pyproject.is_file():
+        with pyproject.open("rb") as fd:
+            toml = load(fd)
+        project = toml.get("project", {})
+        yield from project.get("dependencies", ())
+        yield from chain.from_iterable(
+            project.get("optional-dependencies", {}).values()
         )
-        if deps:
-            pip("--", *deps)
 
-for path in Path.cwd().glob("requirements*.txt"):
-    pip("--requirement", path)
+
+argv = tuple(_argv())
+print(*map(lambda s: quote(normcase(s)), argv), file=stderr)
+
+execl(
+    executable,
+    executable,
+    "-m",
+    "pip",
+    "install",
+    "--upgrade",
+    "--require-virtualenv",
+    *argv,
+)
 PYTHON
 
 case "$OP" in
-init)
-  if [[ -d "$DEFAULT_VENV_PATH" ]]; then
-    printf >&2 -- '%s\n' "Virtualenv already initialized"
-  else
-    python3 -m venv -- "$DEFAULT_VENV_PATH" >&2
-    "$PY_EXE" <<<"$PYPROJ" >&2
-    printf -- '%s\n' "Initialized Virtualenv" >&2
-  fi
-  "$0" on
-  ;;
 on)
   if [[ -f "$ACTIVATE" ]]; then
     "$0" off
     printf -- '\n'
   else
-    "$0" init
+    if ! [[ -d "$DEFAULT_VENV_PATH" ]]; then
+      python3 -m venv -- "$DEFAULT_VENV_PATH" >&2
+      "$PY_EXE" <<<"$PYPROJ" >&2
+    fi
   fi
   printf -- '%q ' 'source' '--' "$ACTIVATE"
-  printf -- '%s\n' "Activated - $DEFAULT_VENV_PATH" >&2
+  printf -- '%s\n' "[x] - $DEFAULT_VENV_PATH" >&2
   ;;
 off)
-  if [[ -v VIRTUAL_ENV ]]; then
+  if [[ -n "$VIRTUAL_ENV" ]]; then
     printf -- '%q ' 'deactivate'
-  else
-    true
+    printf -- '%s\n' "[ ] - $VIRTUAL_ENV" >&2
   fi
-  printf -- '%s\n' "Deactivating - $VIRTUAL_ENV" >&2
   ;;
 rm)
   if [[ -d "$DEFAULT_VENV_PATH" ]]; then
-    if [[ "$VIRTUAL_ENV" = "$DEFAULT_VENV_PATH" ]]; then
+    if [[ "{VIRTUAL_ENV" == "$DEFAULT_VENV_PATH" ]]; then
       "$0" off
     fi
     printf -- '%q ' 'rm' '-rf' '--' "$DEFAULT_VENV_PATH"
   fi
-  printf -- '%s\n' "Removed - $DEFAULT_VENV_PATH" >&2
+  printf -- '%s\n' "[-] - $DEFAULT_VENV_PATH" >&2
   ;;
 *)
-  printf -- '%s\n' "Invalid argument" >&2
-  printf -- '%s\n' "venv - [on | off | rm]" >&2
+  printf -- '%s\n' "${0##*/} - [on | off | rm]" >&2
   ;;
 esac
