7b503b30601028acdfd23bb75016111a4d9eb4ce
IIIIII <github@bigly.dog>
2023-08-11 20:18:20 -0700

    winname

--- layers/posix/home/.local/opt/initd/libexec/flock.sh
+++ layers/posix/home/.local/opt/initd/libexec/flock.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
 
 set -o pipefail
-set -x
+
+LOCK="$1"
+shift -- 1
 
 case "$OSTYPE" in
 msys)
-  exec -- "${0%/*}/flock.ps1" "$@"
+  FLOCK="$(base64 <<<"$LOCK")"
+  exec -- "${0%/*}/flock.ps1" "$FLOCK" "$@"
   ;;
 *)
-  LOCK="$1"
-  shift -- 1
   if command -v -- flock >/dev/null; then
     exec -- flock "$LOCK" "$@"
   else
