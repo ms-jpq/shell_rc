61e63637bbb523fbf07cbd4f8ff24cebf027c92a
IIIIII <github@bigly.dog>
2023-08-12 22:32:54 -0700

    np

--- layers/posix/home/.local/opt/initd/libexec/nproc.sh
+++ layers/posix/home/.local/opt/initd/libexec/nproc.sh
@@ -6,17 +6,7 @@ case "$OSTYPE" in
 darwin*)
   sysctl -n hw.physicalcpu
   ;;
-linux*)
-  nproc
-  ;;
-msys)
-  if command -v -- nproc >/dev/null; then
-    nproc
-  else
-    "${0%/*}/pwsh.sh" 'Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -ExpandProperty NumberOfProcessors'
-  fi
-  ;;
 *)
-  exit 1
+  nproc
   ;;
 esac
