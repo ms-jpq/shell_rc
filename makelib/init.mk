07004d524d24a7e5f90582a7a933fa275265cb43
IIIIII <github@bigly.dog>
2023-08-12 22:14:22 -0700

    lint!

--- makelib/init.mk
+++ makelib/init.mk
@@ -30,7 +30,7 @@ export -- PYDEPS
 ./node_modules/.bin:
 	npm install --upgrade --no-package-lock
 
-./var/bin/shellcheck: | ./var/bin
+$(VAR)/bin/shellcheck: | $(VAR)/bin
 	VERSION='v0.9.0'
 	case "$$OSTYPE" in
 	darwin*)
@@ -41,10 +41,10 @@ export -- PYDEPS
 		;;
 	esac
 	URI="https://github.com/koalaman/shellcheck/releases/latest/download/shellcheck-$$VERSION.$$FILE.tar.xz"
-	$(CURL) -- "$$URI" | tar --extract --xz --file - --directory './var/bin' --strip-components 1 "shellcheck-$$VERSION/shellcheck"
+	$(CURL) -- "$$URI" | tar --extract --xz --file - --directory '$(VAR)/bin' --strip-components 1 "shellcheck-$$VERSION/shellcheck"
 	chmod +x '$@'
 
-./var/bin/hadolint: | ./var/bin
+$(VAR)/bin/hadolint: | $(VAR)/bin
 	case "$$OSTYPE" in
 	darwin*)
 		FILE='hadolint-Darwin-x86_64'
@@ -57,7 +57,7 @@ export -- PYDEPS
 	$(CURL) --output '$@' -- "$$URI"
 	chmod +x '$@'
 
-./var/bin/shfmt: | ./var/bin
+$(VAR)/bin/shfmt: | $(VAR)/bin
 	VERSION='v3.7.0'
 	case "$$OSTYPE" in
 	darwin*)
