db63d66a3c462a0d8be602416d63d9c3e8fa9314
IIIIII <github@bigly.dog>
2023-08-13 18:12:55 -0700

    fmt5

--- zsh/iso/bin/systemd-fmt.sh
+++ zsh/iso/bin/systemd-fmt.sh
@@ -2,36 +2,7 @@
 
 set -o pipefail
 
-if [[ "${SYSTEMD_FMT_MODE:-""}" == 'stream' ]]; then
-  declare -A -- SEEN=()
-  TMP="$(mktemp)"
-
-  while read -r -d '' -- FILE; do
-    if [[ -L "$FILE" ]]; then
-      FILE="$(realpath -- "$FILE")"
-    fi
-    if [[ -v SEEN["$FILE"] ]]; then
-      continue
-    fi
-    SEEN["$FILE"]=1
-
-    printf -- '%q\n' "$FILE" >&2
-    SYSTEMD_FMT_MODE='' "$0" <"$FILE" >"$TMP"
-    mv -f -- "$TMP" "$FILE"
-  done
-elif (($#)); then
-  for FILE in "$@"; do
-    if [[ -d "$FILE" ]]; then
-      for F in "$FILE"/**/{*.link,*.netdev,*.network,*.socket,*.service,*/repart.d/*.conf,*/systemd/**/*.conf}; do
-        printf -- '%s\0' "$F"
-      done
-    else
-      printf -- '%s\0' "$FILE"
-    fi
-  done | SYSTEMD_FMT_MODE='stream' "$0"
-elif [[ -t 0 ]]; then
-  exec -- "$0" .
-else
+fmt() {
   readarray -t -d $'\n' -- LINES
 
   MAX=0
@@ -91,8 +62,41 @@ else
     elif [[ "$LINE" =~ ^[[:space:]]*([^[:space:]]*)[[:space:]]*$ ]]; then
       printf -- '%s\n' "${BASH_REMATCH[1]}"
     else
-      printf -- '%s\n' "unexpected --> $LINE" >&2
+      printf -- '%s\n' "??? --> $LINE" >&2
       exit 1
     fi
   done
+}
+
+if [[ "${SYSTEMD_FMT_MODE:-""}" == 'stream' ]]; then
+  declare -A -- SEEN=()
+  TMP="$(mktemp)"
+
+  while read -r -d '' -- FILE; do
+    if [[ -L "$FILE" ]]; then
+      FILE="$(realpath -- "$FILE")"
+    fi
+    if [[ -v SEEN["$FILE"] ]]; then
+      continue
+    fi
+    SEEN["$FILE"]=1
+
+    printf -- '%q\n' "$FILE" >&2
+    SYSTEMD_FMT_MODE='' fmt <"$FILE" >"$TMP"
+    mv -f -- "$TMP" "$FILE"
+  done
+elif (($#)); then
+  for FILE in "$@"; do
+    if [[ -d "$FILE" ]]; then
+      for F in "$FILE"/**/{*.link,*.netdev,*.network,*.socket,*.service,*/repart.d/*.conf,*/systemd/**/*.conf}; do
+        printf -- '%s\0' "$F"
+      done
+    else
+      printf -- '%s\0' "$FILE"
+    fi
+  done | SYSTEMD_FMT_MODE='stream' "$0"
+elif [[ -t 0 ]]; then
+  exec -- "$0" .
+else
+  fmt
 fi
