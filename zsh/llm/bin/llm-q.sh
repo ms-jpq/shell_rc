f08af6929ea79201573ef7b599bab65a4633c9e5
IIIIII <github@bigly.dog>
2023-08-15 09:35:29 -0700

    llm

---
+++ zsh/llm/bin/llm-q.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+OPTS='m:,t:,r:'
+LONG_OPTS='model:,temperature:,role:'
+GO="$(getopt --options="$OPTS" --longoptions="$LONG_OPTS" --name="$0" -- "$@")"
+eval -- set -- "$GO"
+
+ARGV=("$@")
+
+GPTHIST="${GPTHIST:-"$(mktemp)"}"
+GPTTMP="${GPTTMP:-"$(mktemp)"}"
+
+export -- GPTHIST GPTTMP
+printf -- '%q\n' "$GPTHIST"
+
+MODEL='gpt-3.5-turbo'
+ROLE='system'
+
+while (($#)); do
+  case "$1" in
+  -m | --model)
+    MODEL="$2"
+    shift -- 2
+    ;;
+  -t | --temperature)
+    TEMP="$2"
+    shift -- 2
+    ;;
+  -r | --role)
+    ROLE="$2"
+    shift -- 2
+    ;;
+  --)
+    shift -- 1
+    break
+    ;;
+  *)
+    exit 1
+    ;;
+  esac
+done
+
+read -r -d '' -- MSG
+jq --exit-status --raw-input --arg role "$ROLE" '{ role: $role, content: . }' <<<"$MSG" >>"$GPTHIST"
+
+# shellcheck disable=SC2016
+JQ=(
+  jq
+  --exit-status
+  --slurp
+  --arg model "$MODEL"
+  --arg temp "${TEMP:-"$(jot -r -p 1 1 0 2)"}"
+  '{ model: $model, temperature: ($temp | tonumber), messages: . }'
+)
+LLMQ=(
+  "${0%%-*}"
+  --write-out '%{http_code}'
+  --output "$GPTTMP"
+  --data @-
+  -- 'https://api.openai.com/v1/chat/completions'
+)
+
+JSON="$("${JQ[@]}" <"$GPTHIST")"
+CODE="$(RECURSION=1 "${LLMQ[@]}" <<<"$JSON")"
+
+if ((CODE != 200)); then
+  jq <"$GPTTMP" || cat -- "$GPTTMP"
+else
+  jq --exit-status --raw-output '.choices[].message.content' <"$GPTTMP" | glow
+fi
+
+exec -- "$0" "${ARGV[@]}"
---
+++ zsh/llm/bin/llm.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+if ! (($#)); then
+  printf -- '%q\n' "$0"*
+  exit
+fi
+
+# shellcheck disable=SC2154
+TOKEN_F="$XDG_STATE_HOME/gpt/token"
+edit() {
+  mkdir -v -p -- "${TOKEN_F%/*}"
+  # shellcheck disable=SC2154
+  exec -- $EDITOR "$TOKEN_F"
+
+}
+
+if ! [[ -v RECURSION ]]; then
+  PROGRAM="${1:-""}"
+  case "$PROGRAM" in
+  r | request) ;;
+  e | edit)
+    edit
+    ;;
+  *)
+    shift -- 1
+    exec -- "$0-$PROGRAM" "$@"
+    ;;
+  esac
+fi
+
+if ! [[ -f "$TOKEN_F" ]]; then
+  edit
+fi
+
+TOKEN="$(<"$TOKEN_F")"
+
+CURL=(
+  curl
+  --header 'Content-Type: application/json'
+  --oauth2-bearer "$TOKEN"
+  --no-progress-meter
+  "$@"
+)
+
+if [[ -t 1 ]]; then
+  "${CURL[@]}" | jq --exit-status --sort-keys
+else
+  exec -- "${CURL[@]}"
+fi
