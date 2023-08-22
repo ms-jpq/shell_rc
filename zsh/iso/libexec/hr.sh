2b2147116da80f085b264faf67841102423ec422
IIIIII <github@bigly.dog>
2023-08-22 12:57:35 -0700

    hr

--- zsh/iso/libexec/hr.sh
+++ zsh/iso/libexec/hr.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env -S -- bash -Eeuo pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
 
 WORD="${1-"#"}"
-COLS="${2:-"$(tput -- cols)"}"
+COLS="${2:-"$(tput -- cols 2>/dev/null || :)"}"
+COLS="${COLS:-80}"
 
 LEN="${#WORD}"
 REPS=$((COLS / LEN + 1))
