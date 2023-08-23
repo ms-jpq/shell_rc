1e1d628529ef56007a8e6cfecf5156cdfdcf5f5b
IIIIII <github@bigly.dog>
2023-08-22 19:59:44 -0700

    gitls

---
+++ zsh/dev/bin/git-ls-r.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+parse() {
+  LINE="$(</dev/stdin)"
+  POINTER="${LINE%% *}"
+}
+
+case "${SCRIPT_MODE:-""}" in
+preview)
+  parse
+  git show "$POINTER" "$@" | delta
+  ;;
+execute)
+  parse
+  printf -- '%q\n' "$POINTER"
+  ;;
+*)
+  ARGV=(
+    git log
+    --relative
+    --walk-reflogs
+    --remove-empty
+    --color --pretty='format:%Cgreen%gD%Creset %Cblue%ad%Creset %s'
+    -z
+    --all-match
+    "$@"
+  )
+  "${ARGV[@]}" | "${0%/*}/../libexec/fzf-lr.sh" "$0" "$@"
+  ;;
+esac
--- zsh/dev/bin/git-rg-r.sh
+++ zsh/dev/bin/git-rg-r.sh
@@ -2,36 +2,9 @@
 
 set -o pipefail
 
-parse() {
-  LINE="$(</dev/stdin)"
-  POINTER="${LINE%% *}"
-}
-
 SEARCH=()
 for RE in "$@"; do
   SEARCH+=(-G "$RE")
 done
 
-case "${SCRIPT_MODE:-""}" in
-preview)
-  parse
-  git show "$POINTER" "${SEARCH[@]}" | delta
-  ;;
-execute)
-  parse
-  printf -- '%q\n' "$POINTER"
-  ;;
-*)
-  ARGV=(
-    git log
-    --relative
-    --walk-reflogs
-    --remove-empty
-    --color --pretty='format:%Cgreen%gD%Creset %Cblue%ad%Creset %s'
-    -z
-    --all-match
-    "${SEARCH[@]}"
-  )
-  "${ARGV[@]}" | "${0%/*}/../libexec/fzf-lr.sh" "$0" "$@"
-  ;;
-esac
+exec -- "${0%/*}/git-ls-r" --all-match "${SEARCH[@]}"
