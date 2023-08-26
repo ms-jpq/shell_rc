77cbc4ad40072c0023407bacef7612f6c3ca5341
IIIIII <github@bigly.dog>
2023-08-26 15:15:06 -0700

    ssed

--- zsh/dev/bin/jjq.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
-
-set -o pipefail
-
-read -r -d '' -- SCRIPT
-
-jq "$SCRIPT" "$@" || true
-
-exec -- "$0" "$@"
