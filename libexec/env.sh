10bba2e8138b96b7f84c29a215121ff5816375ce
IIIIII <github@bigly.dog>
2023-08-27 18:38:03 -0700

    bytes

--- libexec/env.sh
+++ libexec/env.sh
@@ -6,15 +6,15 @@ case "$OSTYPE" in
 linux*)
   # shellcheck disable=SC1091
   source -- /etc/os-release
-  MEMINFO_KB="$(awk '/MemTotal/ { print $2 }' </proc/meminfo)"
-  MEMINFO=$((MEMINFO_KB * 1024))
+  MEMBYTES_KB="$(awk '/MemTotal/ { print $2 }' </proc/meminfo)"
+  MEMBYTES=$((MEMBYTES_KB * 1024))
   MAKE='gmake'
   ;;
 darwin*)
   ID="$(sw_vers -productName)"
   VERSION_ID="$(sw_vers -productVersion)"
   VERSION_CODENAME="$VERSION_ID"
-  MEMINFO="$(sysctl -n hw.memsize)"
+  MEMBYTES="$(sysctl -n hw.memsize)"
   MAKE='gmake'
   ;;
 msys)
@@ -32,7 +32,7 @@ msys)
   ID="$(wmic os get Caption | trim)"
   VERSION_ID="$(wmic os get Version | trim)"
   VERSION_CODENAME="$VERSION_ID"
-  MEMINFO="$(wmic ComputerSystem get TotalPhysicalMemory | trim)"
+  MEMBYTES="$(wmic ComputerSystem get TotalPhysicalMemory | trim)"
   # shellcheck disable=SC2154
   MAKE="$SYSTEMDRIVE/msys64/usr/bin/make"
   ;;
@@ -47,7 +47,7 @@ ENV_HOSTNAME=$(printf -- '%q' "$HOSTNAME")
 ENV_HOSTTYPE=$(printf -- '%q' "$HOSTTYPE")
 ENV_ID=$(printf -- '%q' "$ID")
 ENV_MAKE=$(printf -- '%q' "$MAKE")
-ENV_MEMINFO=$(printf -- '%q' "$MEMINFO")
+ENV_MEMBYTES=$(printf -- '%q' "$MEMBYTES")
 ENV_OSTYPE=$(printf -- '%q' "$OSTYPE")
 ENV_VERSION_CODENAME=$(printf -- '%q' "$VERSION_CODENAME")
 ENV_VERSION_ID=$(printf -- '%q' "$VERSION_ID")
