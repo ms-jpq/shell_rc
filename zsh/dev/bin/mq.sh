5d1e1021a9d3f048c635fd4828e50bba02bbbe49
IIIIII <github@bigly.dog>
2023-08-29 22:13:41 -0700

    mqmq

--- zsh/dev/bin/mq.sh
+++ zsh/dev/bin/mq.sh
@@ -6,19 +6,28 @@ A0="$1"
 ARGV=("$A0")
 shift -- 1
 
-read -r -d '' -- SCRIPT
-
 case "$A0" in
 m4)
   ARGV+=(--prefix-builtins)
   ;;
+perl)
+  ARGV+=(-CASD -w)
+  ;;
 psql)
   ARGV+=(--no-password --single-transaction --expanded)
+  ;;
+node)
+  ARGV+=(--input-type=module)
+  ;;
+jshell)
+  ARGV+=(-)
   ;;
 *) ;;
 esac
 
 ARGV+=("$@")
+
+read -r -d '' -- SCRIPT
 printf -- '\n>> '
 printf -- '%q ' "${ARGV[@]}"
 "${ARGV[@]}" <<<"$SCRIPT" || true
