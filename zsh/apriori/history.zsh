14386851efa973d5391a7109ba59792d5161e5a5
IIIIII <github@bigly.dog>
2023-09-01 13:12:50 -0700

    k

--- zsh/apriori/history.zsh
+++ zsh/apriori/history.zsh
@@ -1,7 +1,8 @@
 #!/usr/bin/env -S -- bash
 
+# setopt -- extended_history     # Record timestamp of command in HISTFILE
+# setopt -- share_history          # Share command history data
 setopt -- append_history         # Add commands to HISTFILE in order of execution
-setopt -- extended_history       # Record timestamp of command in HISTFILE
 setopt -- hist_expire_dups_first # Delete duplicates first when HISTFILE size exceeds HISTSIZE
 setopt -- hist_fcntl_lock        # Modern history locking
 setopt -- hist_ignore_dups       # Ignore duplicated commands history list
@@ -9,7 +10,6 @@ setopt -- hist_no_functions      # Ignore functions in history
 setopt -- hist_reduce_blanks     # Remove unneeded blanks
 setopt -- hist_verify            # Show command with history expansion to user before running it
 setopt -- inc_append_history     # Add commands to HISTFILE immediately
-setopt -- share_history          # Share command history data
 
 # shellcheck disable=SC2034
 SAVEHIST=10000
