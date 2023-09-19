b14a46b317d0004977f5b9fa6c9423c0975004d8
IIIIII <github@bigly.dog>
2023-09-18 19:08:22 -0700

    links

--- zsh/iso/fn/rsy.sh
+++ zsh/iso/fn/rsy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env -S -- bash
 
-rsync --mkpath --recursive --links --keep-dirlinks --perms --times --human-readable --info progress2 -- "$@"
+rsync --mkpath --recursive --keep-dirlinks --links --keep-dirlinks --perms --times --human-readable --info progress2 -- "$@"
