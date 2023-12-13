47602022d940ac2de7cb61b89f6a348599938ba9
IIIIII <github@bigly.dog>
2023-12-13 20:23:32 +0800

    avahidaemon conf

---
+++ layers/ubuntu/root.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+link() {
+  local -- src="$1" dst="$2"
+
+  if ! [[ -L "$dst" ]]; then
+    ln -v -sf -- "$src" "$dst"
+  fi
+}
+
+link /usr/share/doc/avahi-daemon/examples/ssh.service /etc/avahi/services/ssh.service
