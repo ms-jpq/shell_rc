eedae211b78fc6549b607df46143993892feb66d
IIIIII <github@bigly.dog>
2023-12-31 14:11:39 -0400

    scanner

---
+++ zsh/iso/nvim.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env -S -- bash
+
+# shellcheck disable=SC2154
+printf -v EDITOR -- '%q' "$XDG_CONFIG_HOME/zsh/libexec/edit.sh"
+export -- EDITOR
+printf -v MANPAGER -- '%q ' nvim +Man! --
+export -- MANPAGER
+
+path=("$XDG_CONFIG_HOME/nvim/bin" "${path[@]}")
