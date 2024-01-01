ccaf7651960dabb3b30245e40888aaf8593c66e0
IIIIII <github@bigly.dog>
2024-01-01 00:24:35 -0400

    yes more

--- zsh/tmux/fn/tmux-edit.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S -- bash
-
-tmux-edit() {
-  local -- tmp
-  tmp="$(mktemp)"
-  tmux-dump >"$tmp"
-  # shellcheck disable=SC2154
-  "$EDITOR" "$tmp"
-}
--- zsh/tmux/fn/tmux-ls.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env -S -- bash
-
-tmux list-panes -a -F '#{session_name} #{window_index} #{pane_index} #{pane_id} #{?window_active,#{?pane_active,<-,},}'
