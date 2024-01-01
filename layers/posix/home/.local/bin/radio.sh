dfe6499384f7291955b305232d4472196eadb62f
IIIIII <github@bigly.dog>
2024-01-01 14:08:27 -0400

    add radio.sh

---
+++ layers/posix/home/.local/bin/radio.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+OPT="$HOME/.local/opt"
+PYRADIO="$OPT/pyradio"
+VENV="$PYRADIO/venv"
+PY="$VENV/bin/python"
+
+if ! [[ -d "$PYRADIO" ]]; then
+  REPO='coderholic/pyradio'
+  TMP="$(mktemp -d)"
+  LATEST="$("$OPT/initd/libexec/gh-latest.sh" "$TMP" "$REPO")"
+  URI="https://github.com/coderholic/pyradio/archive/refs/tags/$LATEST.tar.gz"
+  curl --fail --no-progress-meter -- "$URI" | tar -x -z -C "$TMP"
+  mv -f -- "$TMP/pyradio-"* "$PYRADIO"
+fi
+
+if ! [[ -d "$VENV" ]]; then
+  python3 -m venv --upgrade -- "$VENV"
+  "$PY" -m pip install --require-virtualenv --upgrade --requirement "$PYRADIO/requirements_pipx.txt"
+fi
+
+if (($#)); then
+  ARGV=("$@")
+else
+  ARGV=(--theme black_on_white)
+fi
+
+exec -- env --chdir "$PYRADIO" -- TERM=xterm-256color "$PY" -m pyradio.main "${ARGV[@]}"
