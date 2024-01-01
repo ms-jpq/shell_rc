bbd554b326fc1e8999f0b601552c2d96344d4e05
IIIIII <github@bigly.dog>
2024-01-01 10:31:56 -0400

    try tz

--- layers/posix/home/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ layers/posix/home/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -17,6 +17,14 @@ if ($IsWindows) {
     $Env:Path = @(Join-Path -Path $appdata 'bin' $Env:Path) | Join-String -Separator ([IO.Path]::PathSeparator)
 }
 
+if ($Env:TZ -eq $null) {
+    $tz = ""
+    [TimeZoneInfo]::TryConvertWindowsIdToIanaId((Get-TimeZone).Id, [ref] $tz) | Out-Null
+    if (! $tz -eq $null) {
+        $Env:TZ = $tz
+    }
+    Remove-Variable -Name tz
+}
 
 oh-my-posh init pwsh --config (Join-Path -Path $appdata 'posh' 'config.yml') | Invoke-Expression
 
