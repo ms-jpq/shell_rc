cf5a36989a7b97251ea2f1696ad9ba78f438855d
IIIIII <github@bigly.dog>
2024-01-01 03:23:31 -0400

    readtz

--- zsh/apriori/locale-tz.sh
+++ zsh/apriori/locale-tz.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env -S -- bash
 
-export -- TZ
+export -- LC_ALL TZ
 
-if ! [[ -v LC_ALL ]]; then
-  export -- LC_ALL='zh_CN.UTF-8'
+if [[ -z "${LC_ALL:-""}" ]]; then
+  LC_ALL='zh_CN.UTF-8'
+fi
+
+if [[ -z "${TZ:-""}" ]]; then
+  case "$OSTYPE" in
+  darwin*)
+    TZ="$(readlink -- /etc/localtime)"
+    TZ="${TZ#'/var/db/timezone/zoneinfo/'}"
+    ;;
+  linux*)
+    TZ="$(readlink -- /etc/localtime)"
+    TZ="${TZ#'/usr/share/zoneinfo/'}"
+    ;;
+  msys)
+    :
+    ;;
+  *) ;;
+  esac
 fi
