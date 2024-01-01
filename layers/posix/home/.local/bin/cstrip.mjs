e95a902acacdaceaf6860999c48486721709f02b
IIIIII <github@bigly.dog>
2024-01-01 01:58:06 -0400

    strip

--- layers/posix/home/.local/bin/cstrip.mjs
+++ layers/posix/home/.local/bin/cstrip.mjs
@@ -1,13 +1,17 @@
 #!/usr/bin/env -S -- node
 
+import { EOL } from "node:os";
 import { stdin, stdout } from "node:process";
+import { createInterface } from "node:readline/promises";
+import { pipeline } from "node:stream/promises";
 import { stripVTControlCharacters } from "node:util";
 
-const acc = [];
-for await (const bytes of stdin) {
-  acc.push(bytes);
-}
-const buf = Buffer.concat(acc);
-const raw = buf.toString();
-const stripped = stripVTControlCharacters(raw);
-stdout.write(stripped);
+await pipeline(async function* () {
+  for await (const line of createInterface({
+    input: stdin,
+    crlfDelay: Infinity,
+  })) {
+    yield stripVTControlCharacters(line);
+    yield EOL;
+  }
+}, stdout);
