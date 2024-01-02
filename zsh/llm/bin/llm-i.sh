b553f51b6f6da545a30dc2234c29b9fc08c67eb3
IIIIII <github@bigly.dog>
2024-01-02 11:12:26 -0400

    ok2

--- zsh/llm/bin/llm-i.sh
+++ zsh/llm/bin/llm-i.sh
@@ -5,10 +5,14 @@ set -o pipefail
 PROMPT="$*"
 JSON="$(jq --exit-status --raw-input '{ prompt: . }' <<<"$PROMPT")"
 RESP="$(RECURSION=1 "${0%-*}" --data @- -- 'https://api.openai.com/v1/images/generations' <<<"$JSON")"
-US="$(jq --exit-status --raw-output '.data[].url' <<<"$RESP")"
-readarray -t -- URIS <<<"$US"
+if jq --exit-status '.error' <<<"$RESP" >/dev/null; then
+  jq <<<"$RESP" >&2
+else
+  US="$(jq --exit-status --raw-output '.data[].url' <<<"$RESP")"
+  readarray -t -- URIS <<<"$US"
 
-for URI in "${URIS[@]}"; do
-  printf -- '%s\n' "$URI"
-  open -- "$URI" || true
-done
+  for URI in "${URIS[@]}"; do
+    printf -- '%s\n' "$URI"
+    open -- "$URI" || true
+  done
+fi
