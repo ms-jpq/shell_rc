288daf025d9e2f2193079dbd82c14d94dfa0dbc8
IIIIII <github@bigly.dog>
2024-01-03 11:40:04 -0400

    more hooks

---
+++ layers/posix/home/.config/tmux/libexec/saved.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+if ! [[ -v TMUX_SAVED ]]; then
+  TMUX_SAVED=1 exec -- flock --conflict-exit-code 0 --nonblock "$0" "$0" "$@"
+fi
+
+while true; do
+  sleep -- 1
+  "${0%/*}/save.sh" "$@"
+done
