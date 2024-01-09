e543fcf7f5ea5bed781b6a38e51b2fa9df805287
IIIIII <github@bigly.dog>
2024-01-08 23:59:51 -0400

    ok2

--- layers/ubuntu/home.sh
+++ layers/ubuntu/home.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
 
 set -o pipefail
+
+link() {
+  local -- src="$1" dst="$2"
+
+  if ! [[ -L "$dst" ]]; then
+    mkdir -v -p -- "${dst%/*}"
+    ln -v -sf -- "$src" "$dst"
+  fi
+}
+
+link ~/.config/gnupg ~/.gnupg
