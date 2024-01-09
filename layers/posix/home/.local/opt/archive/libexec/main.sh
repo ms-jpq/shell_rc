b87e7411466785b53a14b157ed0a66d4f666aa8a
IIIIII <github@bigly.dog>
2024-01-09 09:38:18 -0400

    addcheckout

---
+++ layers/posix/home/.local/opt/archive/libexec/main.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+cd -- ~
+
+TMP="$(mktemp -d)"
+chmod -v g-rwx,o-rwx "$TMP"
+gpg -v --armor --export-secret-keys --export-options export-backup --output "$TMP/gnupg.asc"
+cp -v -a -r -- "$HOME/.ssh" "$TMP/ssh"
+cp -v -a -r -- "$HOME/.local/share/ssh" "$TMP/ssh_hosts"
+cp -v -a -r -- "$HOME/.local/share/it2" "$TMP/iterm2"
+open -- "$TMP"
