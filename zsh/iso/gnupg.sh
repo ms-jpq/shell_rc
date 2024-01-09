0f6cf58f3136cf0b66197bad1646827af08a3ebc
IIIIII <github@bigly.dog>
2024-01-08 23:59:51 -0400

    ok2

--- zsh/iso/gnupg.sh
+++ zsh/iso/gnupg.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env -S -- bash
 
 # shellcheck disable=SC2154
-export -- GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
-export -- SSH_AUTH_SOCK="$GNUPGHOME/S.gpg-agent.ssh"
+if [[ -v XDG_RUNTIME_DIR ]]; then
+  export -- SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
+else
+  export -- GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
+  export -- SSH_AUTH_SOCK="$GNUPGHOME/S.gpg-agent.ssh"
+fi
