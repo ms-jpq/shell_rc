18e6943c9316821d122c52327a698b3868e41dcc
IIIIII <github@bigly.dog>
2024-01-09 08:43:09 -0400

    ssh compression

---
+++ zsh/darwin/gnupg.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env -S -- bash
+
+# shellcheck disable=SC2154
+export -- GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
--- zsh/iso/gnupg.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S -- bash
-
-# shellcheck disable=SC2154
-if [[ -v XDG_RUNTIME_DIR ]]; then
-  export -- SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
-else
-  export -- GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
-  export -- SSH_AUTH_SOCK="$GNUPGHOME/S.gpg-agent.ssh"
-fi
