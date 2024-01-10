47f7ee5e358d6041387fc9f873fbece1bce8e56a
IIIIII <github@bigly.dog>
2024-01-10 00:29:04 -0400

    agent2

--- layers/darwin/home/.config/gnupg/libexec/agent.sh
+++ layers/darwin/home/.config/gnupg/libexec/agent.sh
@@ -3,15 +3,10 @@
 set -Eeu
 set -o pipefail
 
-BIN='/opt/homebrew/bin'
-PATH="$BIN:$PATH"
-
-export -- GNUPGHOME="$HOME/.config/gnupg"
-
 ARGV=(
   gpg-agent
   --log-file "$HOME/.local/state/gnupg/agent.log"
   --daemon "$HOME/.config/gnupg/libexec/sleep.sh"
 )
 
-exec -- "${ARGV[@]}" </dev/null
+PATH="/opt/homebrew/bin:$PATH" GNUPGHOME="$HOME/.config/gnupg" exec -- "${ARGV[@]}"
