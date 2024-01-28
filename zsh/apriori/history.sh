5f553f2f9e8c693ebbfc15d16f4c042a61b4c6bf
IIIIII <github@bigly.dog>
2024-01-28 13:02:52 -0800

    split zsh & bash history?

--- zsh/apriori/history.sh
+++ zsh/apriori/history.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env -S -- bash
 
-HISTSIZE=50000
-# shellcheck disable=SC2154
-HISTFILE="$XDG_STATE_HOME/shell_history/sh"
+HISTSIZE=10000
--- zsh/apriori/history.zsh
+++ zsh/apriori/history.zsh
@@ -1,14 +1,17 @@
 #!/usr/bin/env -S -- bash
 
+setopt -- extended_history       # Save timestamps of command
 setopt -- share_history          # Share command history data, turns on `extended_history`
-setopt -- append_history         # Add commands to HISTFILE in order of execution
 setopt -- hist_expire_dups_first # Delete duplicates first when HISTFILE size exceeds HISTSIZE
 setopt -- hist_fcntl_lock        # Modern history locking
 setopt -- hist_ignore_dups       # Ignore duplicated commands history list
 setopt -- hist_no_functions      # Ignore functions in history
+setopt -- hist_no_store          # Ignore history calls from history
 setopt -- hist_reduce_blanks     # Remove unneeded blanks
 setopt -- hist_verify            # Show command with history expansion to user before running it
-setopt -- inc_append_history     # Add commands to HISTFILE immediately
 
 # shellcheck disable=SC2034
-SAVEHIST=10000
+SAVEHIST="$HISTSIZE"
+
+# shellcheck disable=SC2154
+HISTFILE="$XDG_STATE_HOME/shell_history/zsh"
