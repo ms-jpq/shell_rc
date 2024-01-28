ebd667ed59add91d4b66e8424d91b2d2650bb01a
IIIIII <github@bigly.dog>
2024-01-28 13:02:52 -0800

    split zsh & bash history?

--- zsh/apriori/history.bash
+++ zsh/apriori/history.bash
@@ -1,8 +1,12 @@
 #!/usr/bin/env -S -- bash
 
-HISTCONTROL=ignoredups
-
 shopt -s -- cmdhist    # Save multiline commands as one entry
 shopt -s -- histappend # Append instead of replace history
 shopt -s -- histverify # Verify history edits
 shopt -s -- lithist    # Do not replace $'\n' with ';'
+
+HISTCONTROL=ignoredups
+HISTTIMEFORMAT='%F %T '
+
+# shellcheck disable=SC2154
+HISTFILE='/dev/null'
