df110873d30114dcd448e0f9bfc4655bd8b41165
IIIIII <github@bigly.dog>
2024-01-30 18:14:16 -0800

    k

--- zsh/darwin/fn/networkctl.sh
+++ zsh/darwin/fn/networkctl.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env -S -- bash
 
-networksetup -listallhardwareports
-scutil --nwi
+if (($#)); then
+  networksetup "$@"
+else
+  networksetup -listallhardwareports
+  # shellcheck disable=SC2154
+  "$XDG_CONFIG_HOME/zsh/libexec/hr.sh"
+  networksetup -listnetworkserviceorder
+  "$XDG_CONFIG_HOME/zsh/libexec/hr.sh"
+  scutil --nwi
+fi
