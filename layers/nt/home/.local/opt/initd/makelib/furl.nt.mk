a95a0ac1deb6ed91f9b970cf2d667a2bf01b886f
IIIIII <github@bigly.dog>
2024-02-05 15:54:18 -0800

    k

--- layers/nt/home/.local/opt/initd/makelib/furl.nt.mk
+++ layers/nt/home/.local/opt/initd/makelib/furl.nt.mk
@@ -11,6 +11,12 @@ curl: $(BIN)/$(notdir $1)
 endef
 
 
+ifeq ($(HOSTTYPE), aarch64)
+S5_TYPE := $(GOARCH)
+else
+S5_TYPE := 64bit
+endif
+
 V_S5CMD   =  $(patsubst v%,%,$(shell $(GH_LATEST) peak/s5cmd))
 V_WATCHEX := $(patsubst v%,%,$(shell $(GH_LATEST) watchexec/watchexec))
 V_XSV     =  $(shell $(GH_LATEST) BurntSushi/xsv)
@@ -21,7 +27,7 @@ define CURL_ARCHIVES
 gitui.exe                                                   https://github.com/extrawurst/gitui/releases/latest/download/gitui-win.tar.gz
 gping.exe                                                   https://github.com/orf/gping/releases/latest/download/gping-Windows-x86_64.zip
 htmlq.exe                                                   https://github.com/mgdm/htmlq/releases/latest/download/htmlq-x86_64-windows.zip
-s5cmd.exe                                                   https://github.com/ms-jpq/sad/releases/latest/download/$(HOSTTYPE)_Windows-64bit.zip
+s5cmd.exe                                                   https://github.com/peak/s5cmd/releases/latest/download/s5cmd_$(V_S5CMD)_Windows-$(S5_TYPE).zip
 sad.exe                                                     https://github.com/ms-jpq/sad/releases/latest/download/x86_64-pc-windows-msvc.zip
 watchexec-$(V_WATCHEX)-x86_64-pc-windows-msvc/watchexec.exe https://github.com/watchexec/watchexec/releases/latest/download/watchexec-$(V_WATCHEX)-x86_64-pc-windows-msvc.zip
 xsv.exe                                                     https://github.com/BurntSushi/xsv/releases/latest/download/xsv-$(V_XSV)-x86_64-pc-windows-msvc.zip
