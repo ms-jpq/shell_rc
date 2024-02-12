6c4b88ede32759f8d218b418292dbc861bbb96c1
IIIIII <github@bigly.dog>
2024-02-12 14:30:46 -0800

    ok

--- zsh/aposteriori.sh
+++ zsh/aposteriori.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env -S -- bash
 
+# shellcheck disable=SC2154
+for sh in "$XDG_DATA_HOME/zsh"/{sh,aliases}/*.sh; do
+  # shellcheck disable=SC1090
+  source -- "$sh"
+done
+
 # shellcheck disable=SC1091
 if [[ -v BASH ]]; then
   sh='bash'
