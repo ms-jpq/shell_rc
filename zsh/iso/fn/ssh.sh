61d6b24b9bc1be32cd9437c3b599b84705f724c3
IIIIII <github@bigly.dog>
2024-02-12 19:51:06 -0800

    k

--- zsh/iso/fn/ssh.sh
+++ zsh/iso/fn/ssh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S -- bash
 
-if (($# > 1)); then
-  command -- ssh "$@"
-else
-  autossh -M 0 "$@"
-fi
+# if (($# > 1)); then
+command -- ssh "$@"
+# else
+#   autossh -M 0 "$@"
+# fi
