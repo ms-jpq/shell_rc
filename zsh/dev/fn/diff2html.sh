14aaf4c939c0983dbe63edbc76ba25ee20523dce
IIIIII <github@bigly.dog>
2024-02-15 17:21:16 -0800

    ok

--- zsh/dev/fn/diff2html.sh
+++ zsh/dev/fn/diff2html.sh
@@ -2,8 +2,14 @@
 
 diff2html() {
   local -- argv=("$@")
+  if [[ -z "$*" ]]; then
+    argv+=(--style side)
+  fi
   if ! [[ -t 0 ]]; then
     argv+=(--input stdin)
+  fi
+  if ! [[ -t 1 ]]; then
+    argv+=(--output stdout)
   fi
   npm exec --yes -- diff2html-cli "${argv[@]}"
 }
