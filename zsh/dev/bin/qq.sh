a352a5467747d77de0043d0ee17b79f21065b20a
IIIIII <github@bigly.dog>
2024-03-04 18:07:34 -0800

    qq.sh

---
+++ zsh/dev/bin/qq.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+LIBEXEC="${0%/*}/../libexec"
+
+case "$1" in
+jq)
+  SH="$LIBEXEC/q-jq.sh"
+  ;;
+http)
+  SH="$LIBEXEC/q-http.sh"
+  shift -- 1
+  ;;
+graphql)
+  SH="$LIBEXEC/q-graphql.sh"
+  shift -- 1
+  ;;
+*)
+  SH="$LIBEXEC/q-m4.sh"
+  ;;
+esac
+
+exec -a "$SH" -- "$SH" "$@"
