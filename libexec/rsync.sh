b193781b0b283c35fa2ecacb862f7cce71cb04cb
IIIIII <github@bigly.dog>
2024-04-02 00:00:50 -0700

    k2

--- libexec/rsync.sh
+++ libexec/rsync.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
 
 set -o pipefail
-set -x
 
 SRC="$2"
 DST="$3"
 REMOTE="${DST%%:*}"
-SINK="${DST#*:}"
+SINK="$(sed -E -e 's#:\\#:\\\\#' <<<"${DST#*:}")"
 
 if [[ "$REMOTE" == 'localhost' ]]; then
   RSH=()
 else
   # shellcheck disable=SC2206
   RSH=($1 "$REMOTE")
+  SINK="\"$SINK\""
 fi
 
 tar -c -C "$SRC" -- . | "${RSH[@]}" tar -x -p -C "$SINK"
