f53129d80ce4fced1604ba13c57f71e16f65be0e
IIIIII <github@bigly.dog>
2024-04-13 06:31:01 -0700

    add http echo

---
+++ zsh/dev/bin/http-echo.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+TXT="${1:-"$TXT"}"
+PORT="${2:-8888}"
+ADDR="${3:-"127.0.0.1"}"
+
+COMMENT="${COMMENT:-"^#"}"
+if [[ -t 0 ]]; then
+  export -- TXT
+  printf -- '%q ' curl -- "http://$ADDR":"$PORT"
+  printf -- '\n'
+  printf -- '%s%q\n' "body comment: " "$COMMENT"
+  exec -- socat TCP-LISTEN:"$PORT,bind=$ADDR",reuseaddr,fork EXEC:"$0" 2>&1
+fi
+
+TX='>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+RX='<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+EF='.....................................................................'
+
+{
+  printf -- '%s\n' "$RX"
+
+  BYTES=0
+  while read -r LINE; do
+    LINE="${LINE%$'\r'}"
+    printf -- '%s\n' "$LINE"
+    if [[ -z "$LINE" ]]; then
+      break
+    fi
+
+    LHS="${LINE%%:*}"
+    case "${LHS,,}" in
+    content-length)
+      BYTES="${LINE##*: }"
+      ;;
+    *) ;;
+    esac
+  done
+  head -c "$BYTES"
+  printf -- '\n%s\n' "$TX"
+} >&2
+
+read -r -d '' -- AWK <<-'AWK' || true
+BEGIN { CMT="^#" }
+/^$/ { CMT=COMMENT }
+$0 !~ CMT { print $0 }
+AWK
+
+awk -v "COMMENT=$COMMENT" "$AWK" <"$TXT" | tee -- /dev/stderr
+printf -- '%s\n' "$EF" >&2
