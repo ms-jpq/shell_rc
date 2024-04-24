aa987a7dc33895a03c292c12ab950704cef723d8
IIIIII <github@bigly.dog>
2024-04-24 17:28:06 -0400

    norc

--- zsh/dev/bin/yarn.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
-
-set -o pipefail
-
-# shellcheck disable=SC2154
-exec -- "${0%/*}/nx" yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config" "$@"
