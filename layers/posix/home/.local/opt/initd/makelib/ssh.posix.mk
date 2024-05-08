0a429b04cec1ebb439780c3bb9e0306f586e518b
IIIIII <github@bigly.dog>
2024-05-08 17:44:14 -0400

    ssh

--- layers/posix/home/.local/opt/initd/makelib/ssh.posix.mk
+++ layers/posix/home/.local/opt/initd/makelib/ssh.posix.mk
@@ -1,7 +1,7 @@
 .PHONY: ssh
 all: ssh
 
-ssh: $(HP)/ssh/authorized_keys
-$(HP)/ssh/authorized_keys:
+ssh: $(HP)/.ssh/authorized_keys
+$(HP)/.ssh/authorized_keys:
 	URI='https://github.com/ms-jpq.keys'
 	$(CURL) --output '$@' -- "$$URI"
