790d9ecaf09f2fb3d331ffd2a74ec78920826171
IIIIII <github@bigly.dog>
2024-05-18 20:16:19 -0400

    pr

--- zsh/dev/bin/git-pr.sh
+++ zsh/dev/bin/git-pr.sh
@@ -6,5 +6,6 @@ PR="$1"
 
 REMOTE="$(git remote)"
 git config --local --replace-all -- "remote.$REMOTE.fetch" "+refs/pull/*/head:refs/remotes/$REMOTE/pr/*"
+git config --local --add -- "remote.$REMOTE.fetch" '+refs/heads/*:refs/remotes/origin/*'
 git fetch
 git switch --track -- "$REMOTE/pr/$PR"
