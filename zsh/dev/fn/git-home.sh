ce69702ba662d4ddfd08a7c0f7ac0a48f18ecd76
hao <hao@stainlessapi.com>
2024-06-04 19:47:02 -0400

    top

--- zsh/dev/fn/git-home.sh
+++ zsh/dev/fn/git-home.sh
@@ -2,6 +2,6 @@
 
 git-home() {
   local -- top
-  top="$(git --no-optional-locks rev-parse --show-toplevel)"
+  top="$(git --no-optional-locks rev-parse --path-format=absolute --show-toplevel)"
   cd -- "$top" || return 1
 }
--- zsh/dev/fn/git-unlock.sh
+++ zsh/dev/fn/git-unlock.sh
@@ -2,6 +2,6 @@
 
 git-unlock() {
   local -- dir
-  dir="$(git --no-optional-locks rev-parse --git-dir)"
+  dir="$(git --no-optional-locks rev-parse --path-format=absolute --git-dir)"
   rm -v -f -- "$dir/index.lock"
 }
