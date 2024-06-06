469e9b9b0816ffa64a22082537d1982426023c62
IIIIII <github@bigly.dog>
2024-06-06 08:25:45 -0400

    k

--- layers/posix/home/.config/npm/init-module.js
+++ layers/posix/home/.config/npm/init-module.js
@@ -1,6 +1,6 @@
-const json = {
-  $schema: "https://json.schemastore.org/package.json",
-  dependencies: {},
-  devDependencies: {},
-  type: "module",
-};
+const { readFileSync } = require("node:fs");
+const { join } = require("node:path");
+
+const pkg = join(__dirname, "package.json");
+const data = readFileSync(pkg).toString();
+console.info(data);
