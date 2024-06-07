590672aff5ebf84e92eec0b2c5407dd435023e00
IIIIII <github@bigly.dog>
2024-06-07 08:22:38 -0400

    tsconfig

--- layers/posix/home/.config/npm/init-module.cjs
+++ layers/posix/home/.config/npm/init-module.cjs
@@ -1,3 +1,10 @@
-"use strict";
+const conf = "tsconfig.json";
+const { cwd } = require("node:process");
+const { join } = require("node:path");
+const {
+  copyFileSync,
+  constants: { COPYFILE_FICLONE },
+} = require("node:fs");
 
 module.exports = require("./package.json");
+copyFileSync(join(__dirname, conf), join(cwd(), conf), COPYFILE_FICLONE);
