d7e5f6562fc885436210a789107c05d0a3daf1f3
dogisgreat <github@bigly.dog>
2024-09-25 23:12:47 -0400

    swift

--- layers/ubuntu/home/.local/libexec/swift-dl.sh
+++ layers/ubuntu/home/.local/libexec/swift-dl.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
 
 set -o pipefail
+
+# shellcheck disable=SC1091
+source -- /etc/os-release
+
+SWIFT_VERSION='6.0.1'
+# shellcheck disable=SC2154
+URI="https://download.swift.org/swift-$SWIFT_VERSION-release/$ID${VERSION_ID//./}/swift-$SWIFT_VERSION-RELEASE/swift-$SWIFT_VERSION-RELEASE-$ID$VERSION_ID.tar.gz"
+
+SWIFTS_HOME=~/".local/opt/swift"
+SWIFT_HOME="$SWIFTS_HOME/$SWIFT_VERSION"
+rm -v -fr -- "$SWIFT_HOME"
+mkdir -v -p -- "$SWIFT_HOME"
+curl --location --fail-with-body --no-progress-meter -- "$URI" | tar -v --extract --gzip --directory "$SWIFT_HOME" --strip-components 1
+ln -sf -- "$SWIFT_HOME" "$SWIFTS_HOME/current"
