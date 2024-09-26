69cf0002e86efad71a2b388ed5a68fa0a89efbd4
hao <hao@stainlessapi.com>
2024-09-26 10:35:06 -0400

    swift

--- layers/ubuntu/home/.local/libexec/swift-dl.sh
+++ layers/ubuntu/home/.local/libexec/swift-dl.sh
@@ -17,13 +17,11 @@ TOOLCHAIN="https://download.swift.org/$SWIFT_V/$ID${VERSION_ID//./}/$SWIFT_VERSI
 STATIC_SDK="https://download.swift.org/$SWIFT_V/static-sdk/$SWIFT_VERSION/${SWIFT_VERSION}_static-linux-0.0.1.artifactbundle.tar.gz"
 printf -- '%s\n' "$TOOLCHAIN" "$STATIC_SDK" >&2
 
-CURL=(curl --no-progress-meter --location --fail-with-body --url "$TOOLCHAIN" --next --location --fail-with-body --url "$STATIC_SDK")
-TAR=(tar -v --extract --ignore-zeros --gzip --directory "$TMP" --strip-components 1)
-
 rm -v -fr -- "$TMP"
 mkdir -v -p -- "$TMP"
-"${CURL[@]}" | "${TAR[@]}"
+curl --no-progress-meter --location --fail-with-body -- "$TOOLCHAIN" | tar -v --extract --ignore-zeros --gzip --directory "$TMP" --strip-components 1
 
+"$SWIFT_HOME/usr/bin/swift" sdk install -- "$STATIC_SDK"
 rm -v -fr -- "$SWIFT_HOME"
 mv -v -f -- "$TMP" "$SWIFT_HOME"
 ln -snf -- "$SWIFT_HOME" "$SWIFTS_HOME/current"
