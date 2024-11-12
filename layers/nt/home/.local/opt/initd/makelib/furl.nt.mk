ec1154ede4edf8ed67f39d4bd7765a1925b06410
dogisgreat <github@bigly.dog>
2024-11-11 23:36:31 -0500

    what

--- layers/nt/home/.local/opt/initd/makelib/furl.nt.mk
+++ layers/nt/home/.local/opt/initd/makelib/furl.nt.mk
@@ -21,18 +21,14 @@ endif
 
 V_S5CMD   =  $(patsubst v%,%,$(shell $(GH_LATEST) peak/s5cmd))
 V_WATCHEX := $(patsubst v%,%,$(shell $(GH_LATEST) watchexec/watchexec))
-V_XSV     =  $(shell $(GH_LATEST) BurntSushi/xsv)
 
 
 define CURL_ARCHIVES
 
 diffnav.exe                                                 https://github.com/dlvhdr/diffnav/releases/latest/download/diffnav_Windows_$(DIFFN_TYPE).zip
-gitui.exe                                                   https://github.com/extrawurst/gitui/releases/latest/download/gitui-win.tar.gz
 gping.exe                                                   https://github.com/orf/gping/releases/latest/download/gping-Windows-x86_64.zip
 s5cmd.exe                                                   https://github.com/peak/s5cmd/releases/latest/download/s5cmd_$(V_S5CMD)_Windows-$(S5_TYPE).zip
-sad.exe                                                     https://github.com/ms-jpq/sad/releases/latest/download/x86_64-pc-windows-msvc.zip
 watchexec-$(V_WATCHEX)-x86_64-pc-windows-msvc/watchexec.exe https://github.com/watchexec/watchexec/releases/latest/download/watchexec-$(V_WATCHEX)-x86_64-pc-windows-msvc.zip
-xsv.exe                                                     https://github.com/BurntSushi/xsv/releases/latest/download/xsv-$(V_XSV)-x86_64-pc-windows-msvc.zip
 
 endef
 
