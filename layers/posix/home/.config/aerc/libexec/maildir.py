b3a574827bd512a2f093a9d6e510e3f66b14539e
dogisgreat <github@bigly.dog>
2024-12-30 11:38:22 -0800

    x

--- layers/posix/home/.config/aerc/libexec/maildir.py
+++ layers/posix/home/.config/aerc/libexec/maildir.py
@@ -5,7 +5,9 @@ from contextlib import suppress
 from email.errors import HeaderParseError
 from email.header import decode_header
 from email.utils import getaddresses
+from itertools import chain
 from mailbox import Maildir, MaildirMessage
+from os.path import pathsep
 from pathlib import Path
 from sys import exit
 from typing import Iterator
@@ -21,11 +23,12 @@ def _parse_args() -> Namespace:
     return parser.parse_args()
 
 
-def _maildirs(root: Path) -> Iterator[tuple[str, Maildir]]:
+def _maildirs(root: Path) -> Iterator[tuple[str, Path, Maildir]]:
     for mailboxes in root.glob("*/"):
         for globbed in mailboxes.rglob(".mbsyncstate"):
             if globbed.is_file():
-                yield mailboxes.name, Maildir(globbed.parent)
+                path = globbed.parent
+                yield mailboxes.name, path, Maildir(path)
 
 
 def _decode(name: str) -> Iterator[str]:
@@ -41,18 +44,30 @@ def _decode(name: str) -> Iterator[str]:
 
 
 def _parse(mail: MaildirMessage) -> Iterator[tuple[str, str]]:
-    for hdr in ("from", "cc", "bcc"):
+    for hdr in ("from", "to", "cc", "bcc"):
         for label, addr in getaddresses(mail.get_all(hdr, [])):
             for name in _decode(label):
                 yield addr, name
 
 
+def _iter_keys(root: Path) -> Iterator[tuple[str, Maildir, Iterator[Path]]]:
+    for mailbox, dir, maildir in _maildirs(root):
+        paths = (dir.joinpath(subdir).iterdir() for subdir in ("cur", "new"))
+        yield mailbox, maildir, chain.from_iterable(paths)
+
+
 def main() -> None:
     args = _parse_args()
-    for mailbox, maildir in _maildirs(Path(args.maildirs)):
-        for message in maildir:
-            for addr, name in _parse(message):
-                print([mailbox, addr])
+    for mailbox, maildir, paths in _iter_keys(Path(args.maildirs)):
+        for path in paths:
+            key, sep, _ = path.name.partition(pathsep)
+            if sep != pathsep:
+                continue
+
+            with suppress(KeyError):
+                message = maildir[key]
+                for addr, name in _parse(message):
+                    print(addr, name)
 
 
 try:
