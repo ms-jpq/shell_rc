f8402883056b799dab3f7a8e74465dcb2d6b3815
dogisgreat <github@bigly.dog>
2025-01-01 20:39:13 -0800

    ok

--- layers/posix/home/.local/opt/maildir/launched.sh
+++ layers/posix/home/.local/opt/maildir/launched.sh
@@ -7,7 +7,7 @@ LABEL="mnotify.$CHANNEL"
 shift -- 1
 
 {
-  if find "$@" -type f | grep -E -- .; then
+  if find "$@" -type f -print0 | grep --null-data -E -- . > /dev/null; then
     ~/.local/libexec/notify.cjs "📩 ↘ $CHANNEL" '' '-' ping
   fi
 } 2>&1 | logger -t "$LABEL"
