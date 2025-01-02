20514414bf7b7d11ac29cdd2beb8dc1b9860489e
dogisgreat <github@bigly.dog>
2025-01-02 06:33:38 -0800

    ok

--- layers/posix/home/.local/opt/maildir/launched.sh
+++ layers/posix/home/.local/opt/maildir/launched.sh
@@ -14,10 +14,11 @@ SOCK=(/tmp/kitty.*.sock)
     for MAIL in "$DIR"/*; do
       FROM="$(formail -c -x from: < "$MAIL")"
       SUBJECT="$(formail -c -x subject: < "$MAIL")"
-      ~/.local/libexec/notify.cjs "📩 ↘ $FROM" '' "$SUBJECT" ping
 
       if ((${#SOCK[@]})); then
         /Applications/kitty.app/Contents/MacOS/kitten @ --to "unix:${SOCK[*]}" -- kitten notify --icon info -- "📩 ↘ $FROM" "$SUBJECT"
+      else
+        ~/.local/libexec/notify.cjs "📩 ↘ $FROM" '' "$SUBJECT" ping
       fi
 
     done
