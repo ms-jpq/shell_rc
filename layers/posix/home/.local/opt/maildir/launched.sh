430779da64f05a82f80e46e4bc90717ef69c8762
dogisgreat <github@bigly.dog>
2025-01-01 20:58:18 -0800

    procmail

--- layers/posix/home/.local/opt/maildir/launched.sh
+++ layers/posix/home/.local/opt/maildir/launched.sh
@@ -1,13 +1,18 @@
-#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob
 
 set -o pipefail
+shopt -u failglob
 
 CHANNEL="$1"
 LABEL="mnotify.$CHANNEL"
 shift -- 1
 
 {
-  if find "$@" -type f -print0 | grep --null-data -E -- . > /dev/null; then
-    ~/.local/libexec/notify.cjs "📩 ↘ $CHANNEL" '' '-' ping
-  fi
+  for DIR in "$@"; do
+    for MAIL in "$DIR"/*; do
+      FROM="$(formail -X from: < "$MAIL" | sed -E -e 's/^(F|f)rom: //')"
+      SUBJECT="$(formail -X subject: < "$MAIL" | sed -E -e 's/^(S|s)ubject: //')"
+      ~/.local/libexec/notify.cjs "📩 ↘ $FROM" '' "$SUBJECT" ping
+    done
+  done
 } 2>&1 | logger -t "$LABEL"
