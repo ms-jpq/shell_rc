784b68ec97dad64251c251e965cb06dcf99c2314
dogisgreat <github@bigly.dog>
2025-01-02 06:28:41 -0800

    launchd

--- layers/posix/home/.local/opt/maildir/launched.sh
+++ layers/posix/home/.local/opt/maildir/launched.sh
@@ -9,17 +9,17 @@ shift -- 1
 
 SOCK=(/tmp/kitty.*.sock)
 
-if ! ((${#SOCK[@]})); then
-  exit
-fi
-
 {
   for DIR in "$@"; do
     for MAIL in "$DIR"/*; do
       FROM="$(formail -c -x from: < "$MAIL")"
       SUBJECT="$(formail -c -x subject: < "$MAIL")"
-      kitten @ --to "unix:${SOCK[*]}" -- kitten notify --icon info -- "📩 ↘ $FROM" "$SUBJECT"
-      # ~/.local/libexec/notify.cjs "📩 ↘ $FROM" '' "$SUBJECT" ping
+      ~/.local/libexec/notify.cjs "📩 ↘ $FROM" '' "$SUBJECT" ping
+
+      if ((${#SOCK[@]})); then
+        /Applications/kitty.app/Contents/MacOS/kitten @ --to "unix:${SOCK[*]}" -- kitten notify --icon info -- "📩 ↘ $FROM" "$SUBJECT"
+      fi
+
     done
   done
 } 2>&1 | logger -t "$LABEL"
