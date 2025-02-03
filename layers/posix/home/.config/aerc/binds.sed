03a5fa02cd99429f0486d3eb68a4182d2be79038
dogisgreat <github@bigly.dog>
2025-02-03 06:31:19 -0800

    aerc binds

---
+++ layers/posix/home/.config/aerc/binds.sed
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S -- sed -E -f
+
+/\[messages\]/ {
+  a\<C-r> = :connect<Enter>
+}
+
+/d = :choose/d
+/D = :delete/ {
+  i\D = :move Trash<Enter>
+  d
+}
--- layers/posix/home/.local/opt/initd/makelib/mail.posix.mk
+++ layers/posix/home/.local/opt/initd/makelib/mail.posix.mk
@@ -3,3 +3,7 @@
 mail: $(CONFIG)/isyncrc
 $(CONFIG)/isyncrc: | $(CONFIG)/isyncrc.example
 	cp -v -f -- '$|' '$@'
+
+mail: $(CONFIG)/aerc/binds.conf
+$(CONFIG)/aerc/binds.conf: $(CONFIG)/aerc/binds.sed | /opt/homebrew/opt/aerc/share/aerc/binds.conf
+	sed -E -f '$^' -- '$|' > '$@'
