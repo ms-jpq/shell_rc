d7e0f2e10c1f55cf1dbb642debc085e7adce04b6
dogisgreat <github@bigly.dog>
2025-02-03 08:10:36 -0800

    ok

--- layers/posix/home/.local/opt/initd/makelib/mail.posix.mk
+++ layers/posix/home/.local/opt/initd/makelib/mail.posix.mk
@@ -3,7 +3,3 @@
 mail: $(CONFIG)/isyncrc
 $(CONFIG)/isyncrc: | $(CONFIG)/isyncrc.example
 	cp -v -f -- '$|' '$@'
-
-mail: $(CONFIG)/aerc/binds.conf
-$(CONFIG)/aerc/binds.conf: $(CONFIG)/aerc/binds.sed | /opt/homebrew/opt/aerc/share/aerc/binds.conf
-	sed -E -f '$^' -- '$|' > '$@'
