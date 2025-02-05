26278c4d63eabceef3a5bee4287ccc8b9a5e72b3
dogisgreat <github@bigly.dog>
2025-02-05 13:16:19 -0800

    obs

---
+++ zsh/dev/bin/obs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+case "$OSTYPE" in
+darwin*)
+  ARGV=(open -- "obsidian://$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/$*")
+  ;;
+linux*)
+  ARGV=(xdg-open "obsidian://$*")
+  ;;
+msys)
+  ARGV=(start "obsidian://$*")
+  ;;
+*)
+  set -x
+  exit 1
+  ;;
+esac
+
+exec -- "${ARGV[@]}"
