c5b03c435a6cf62813209f24f68176ac39a61bdd
dogisgreat <hao@stainlessapi.com>
2025-05-06 14:51:24 -0400

    do not run not much

--- layers/posix/home/.local/opt/isync/launched.sh
+++ layers/posix/home/.local/opt/isync/launched.sh
@@ -10,7 +10,7 @@ BIN='/opt/homebrew/bin'
 
 {
   SASL_PATH="$OPT/cyrus-sasl/lib/sasl2:$OPT/cyrus-sasl-xoauth2/lib/sasl2" "$BIN/mbsync" -- "$CHANNEL"
-  "$BIN/notmuch" --config ~/.config/notmuch/"$CHANNEL"/config new
+  # "$BIN/notmuch" --config ~/.config/notmuch/"$CHANNEL"/config new
 
   find ~/.local/state/isync/"$LABEL".queue -mindepth 1 -delete
 } 2>&1 | logger -t "$LABEL"
