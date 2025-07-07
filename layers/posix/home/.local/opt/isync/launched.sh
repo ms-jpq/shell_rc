673a11ceaa04ccdfe82015d44d0a6bd15f0f8b03
dogisgreat <github@bigly.dog>
2025-07-07 09:36:22 -0400

    ok

--- layers/posix/home/.local/opt/isync/launched.sh
+++ layers/posix/home/.local/opt/isync/launched.sh
@@ -8,8 +8,17 @@ LABEL="mbsync.$CHANNEL"
 OPT='/opt/homebrew/opt'
 BIN='/opt/homebrew/bin'
 
+EXEC=(
+  timeout
+  --preserve-status
+  --kill-after 1s
+  600s
+  "$BIN/mbsync"
+  -- "$CHANNEL"
+)
+
 {
-  SASL_PATH="$OPT/cyrus-sasl/lib/sasl2:$OPT/cyrus-sasl-xoauth2/lib/sasl2" "$BIN/mbsync" -- "$CHANNEL"
+  SASL_PATH="$OPT/cyrus-sasl/lib/sasl2:$OPT/cyrus-sasl-xoauth2/lib/sasl2" "${EXEC[@]}"
   # "$BIN/notmuch" --config ~/.config/notmuch/"$CHANNEL"/config new
 
   find ~/.local/state/isync/"$LABEL".queue -mindepth 1 -delete
