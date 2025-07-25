3c2aec71f3a0ca17563a71da8809eabe26b2ec43
dogisgreat <github@bigly.dog>
2025-07-25 00:38:21 -0400

    add hx

--- zsh/dev/path.sh
+++ zsh/dev/path.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env -S -- bash
 
 path=(
+  # shellcheck disable=2154
+  "$XDG_CONFIG_HOME/helix/bin"
   "$HOME/.local/opt/qemu/bin"
   "$HOME/.local/opt/ai/bin"
   "${path[@]}"
--- zsh/iso/helix.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env -S -- bash
-
-# shellcheck disable=SC2154
-PATH="$XDG_CONFIG_HOME/helix/bin:$PATH"
