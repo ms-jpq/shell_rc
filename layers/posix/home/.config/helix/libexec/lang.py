4bd069b13650d79ca60dd7e8969e9366929dedfc
dogisgreat <github@bigly.dog>
2025-07-29 00:36:18 -0400

    k

--- layers/posix/home/.config/helix/libexec/lang.py
+++ layers/posix/home/.config/helix/libexec/lang.py
@@ -6,24 +6,29 @@ from pathlib import Path
 from tomli import loads
 from tomli_w import dumps
 
-libexec = Path(__file__).resolve(strict=True).parent.parent
-online = libexec.parent.parent.parent.parent.parent / "var" / "helix.lang.toml"
 
-src = libexec / "languages.json"
-dst = libexec / "languages.toml"
+def _main() -> None:
+    libexec = Path(__file__).resolve(strict=True).parent.parent
+    online = libexec.parent.parent.parent.parent.parent / "var" / "helix.lang.toml"
 
-preset = loads(online.read_text())
-languages = {l["name"]: l.get("language-servers", ()) for l in preset["language"]}
+    src = libexec / "languages.json"
+    dst = libexec / "languages.toml"
 
-json = load_json(src.read_text())
-common, ls = json["$"], json["language"]
+    preset = loads(online.read_text())
+    languages = {l["name"]: l.get("language-servers", ()) for l in preset["language"]}
 
-acc = []
-for name, language in json["language"].items():
-    lsps = language.get("language-servers", [])
-    lsps.extend(languages.get(name, ()))
-    lsps.extend(common)
-    acc.append({"name": name, **language})
+    json = load_json(src.read_text())
+    common, ls = json["$"], json["language"]
 
-toml = dumps({"language": acc, "language-server": json["language-server"]})
-dst.write_text(toml)
+    acc = []
+    for name, language in json["language"].items():
+        lsps = language.get("language-servers", [])
+        lsps.extend(languages.get(name, ()))
+        lsps.extend(common)
+        acc.append({"name": name, **language})
+
+    toml = dumps({"language": acc, "language-server": json["language-server"]})
+    dst.write_text(toml)
+
+
+_main()
