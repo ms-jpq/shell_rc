4be6243e9b1e67f0f71818624c33b0beebe26324
dogisgreat <github@bigly.dog>
2025-07-28 20:45:07 -0400

    save hx

--- layers/posix/home/.config/tmux/libexec/whitelist.sh
+++ layers/posix/home/.config/tmux/libexec/whitelist.sh
@@ -7,7 +7,7 @@ if ! (($#)); then
 fi
 
 case "$1" in
-aerc | man | less | autossh | htop | btm | nvim | lazygit | cmatrix)
+aerc | man | less | autossh | htop | btm | nvim | hx | lazygit | cmatrix)
   exit
   ;;
 /bin/sh)
