c149f0b01a01f2bb7ad21e765c009d768b72b2f8
dogisgreat <github@bigly.dog>
2025-07-31 09:39:33 -0400

    cmt

--- layers/posix/home/.config/helix/libexec/install.sh
+++ layers/posix/home/.config/helix/libexec/install.sh
@@ -3,13 +3,12 @@
 set -o pipefail
 
 ROOT="$HOME/.cache/helix-rt"
+PROCS="${0%/*}/../procs.json"
 
 if ! [[ -v RECURSE ]]; then
-  :
-
   RECURSE=1 xargs -0 -r -- "$0"
 fi
 
-BIN=''
-LIB=''
-TMP=''
+export -- BIN="$ROOT/bin" LIB="$ROOT/LIB" TMP="$ROOT/tmp"
+
+cat -- "$PROCS"
