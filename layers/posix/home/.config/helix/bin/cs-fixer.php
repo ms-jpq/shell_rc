91291bb04bb2d7db3539fcb3e75fd80a2c15f479
dogisgreat <github@bigly.dog>
2025-07-31 09:08:05 -0400

    helix rip

---
+++ layers/posix/home/.config/helix/bin/cs-fixer.php
@@ -0,0 +1,44 @@
+#!/usr/bin/env -S -- php
+<?php
+
+declare(strict_types=1);
+
+$cwd = getcwd();
+assert($cwd);
+$tmp = tempnam($cwd, prefix: "");
+assert($tmp);
+
+try {
+    $data = file_get_contents("php://stdin");
+    assert($data);
+    assert(file_put_contents($tmp, data: $data));
+
+    $output = [];
+    $code = -1;
+    exec(
+        join(
+            " ",
+            array_map("escapeshellarg", [PHP_BINARY, "-l", $tmp]),
+        ),
+        $output,
+        $code
+    );
+    assert($code === 0, join(PHP_EOL, $output));
+
+    exec(
+        join(
+            " ",
+            array_map("escapeshellarg", ["php-cs-fixer", "--no-interaction", "fix", "--", $tmp])
+        ),
+        $output,
+        $code
+    );
+    assert($code === 0, join(PHP_EOL, $output));
+
+    $data = file_get_contents($tmp);
+    assert($data);
+
+    echo $data;
+} finally {
+    assert(unlink($tmp));
+}
