06a52800d2a336d64d066f4f5cd9cc7835e31cb3
dogisgreat <github@bigly.dog>
2025-08-15 06:14:13 -0700

    trigger

--- layers/posix/home/.config/helix/install/helix-gpt.sh
+++ layers/posix/home/.config/helix/install/helix-gpt.sh
@@ -16,6 +16,53 @@ if ! hash -- bun npm > /dev/null; then
 fi
 
 read -r -d '' -- PATCH <<- 'PATCH' || true
+diff --git i/src/config.ts w/src/config.ts
+index d264359..eb7fa13 100644
+--- i/src/config.ts
++++ w/src/config.ts
+@@ -128,7 +128,7 @@ if (
+
+ export default {
+   ...values,
+-  triggerCharacters: (values.triggerCharacters as string).split("||"),
++  triggerCharacters: [" ", ".", "'", '"', "`", "(", "{", "<", "&", "|", "!", "-", ">"],
+   debounce: parseInt(values.debounce as string),
+   fetchTimeout: parseInt(values.fetchTimeout as string),
+   actionTimeout: parseInt(values.actionTimeout as string),
+diff --git i/src/constants.ts w/src/constants.ts
+index ed82fd9..409ae5e 100644
+--- i/src/constants.ts
++++ w/src/constants.ts
+@@ -20,29 +20,4 @@ export const examples = [
+ ]
+
+ export const commands = [
+-  {
+-    key: "resolveDiagnostics",
+-    label: "Resolve diagnostics",
+-    query: "Resolve the diagnostics for this code."
+-  },
+-  {
+-    key: "generateDocs",
+-    label: "Generate documentation",
+-    query: "Add documentation to this code."
+-  },
+-  {
+-    key: "improveCode",
+-    label: "Improve code",
+-    query: "Improve this code."
+-  },
+-  {
+-    key: "refactorFromComment",
+-    label: "Refactor code from a comment",
+-    query: "Refactor this code based on the comment."
+-  },
+-  {
+-    key: "writeTest",
+-    label: "Write a unit test",
+-    query: "Write a unit test for this code. Do not include any imports.",
+-  }
+ ]
 diff --git i/src/events/completions.ts w/src/events/completions.ts
 index ffe96b7..b6469dc 100644
 --- i/src/events/completions.ts
