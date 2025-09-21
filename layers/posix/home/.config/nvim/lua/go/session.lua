345d7641dd90d745a73c22f02a8399f2b1639d24
dogisgreat <git@bigly.dog>
2025-09-20 23:26:28 -0400

    add session support

--- layers/posix/home/.config/nvim/lua/go/session.lua
+++ layers/posix/home/.config/nvim/lua/go/session.lua
@@ -1,2 +1,63 @@
+-- limit session restoration info
 vim.opt.sessionoptions:remove("blank", "buffers", "curdir")
 vim.opt.sessionoptions:append("skiprtp")
+
+local state = vim.fn.stdpath("state")
+
+local session_path = function()
+  local cwd = vim.fs.normalize(vim.fn.getcwd(), {expand_env = false})
+  local name = vim.re.gsub(cwd, "[/\\]", ".")
+  local path = vim.fs.joinpath(state, "sessions", name)
+  local norm = vim.fs.normalize(path, {expand_env = false})
+  local escaped = vim.fn.fnameescape(norm)
+  return {norm .. ".vim", escaped}
+end
+
+local mk_session = function()
+  local path, escaped = unpack(session_path())
+  local parent = vim.fs.dirname(path)
+  vim.fn.mkdir(parent, "p", 0700)
+  vim.cmd([[mksession! ]] .. escaped)
+end
+
+vim.api.nvim_create_autocmd(
+  {"SessionWritePost"},
+  {
+    callback = function()
+      vim.cmd [[doautocmd User CHADSave]]
+    end
+  }
+)
+
+vim.api.nvim_create_autocmd(
+  {"VimLeavePre", "VimSuspend", "FocusLost", "CursorHold"},
+  {callback = mk_session}
+)
+
+vim.api.nvim_create_autocmd(
+  {"VimEnter"},
+  {
+    callback = function()
+      local bufs = vim.api.nvim_list_bufs()
+      for _, buf in ipairs(bufs) do
+        if vim.api.nvim_buf_get_name(buf) ~= "" then
+          return
+        end
+      end
+
+      local path, escaped = unpack(session_path())
+      if vim.fn.filereadable(path) then
+        vim.cmd([[silent! source ]] .. escaped)
+      end
+    end
+  }
+)
+
+vim.api.nvim_create_user_command(
+  "KillSession",
+  function()
+    vim.cmd [[%bwipeout!]]
+    mk_session()
+  end,
+  {}
+)
---
+++ layers/posix/home/.config/nvim/lua/go/text_objects.lua
@@ -0,0 +1,1 @@
+require("go.text_objects.move")
