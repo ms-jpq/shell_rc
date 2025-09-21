88ba3c7d33a89de40ea138d4dd46421b5aa54a4e
dogisgreat <git@bigly.dog>
2025-09-21 00:53:20 -0400

    move many lines

--- layers/posix/home/.config/nvim/lua/go/text_objects.lua
+++ layers/posix/home/.config/nvim/lua/go/text_objects.lua
@@ -13,10 +13,19 @@ local fn_ref =
 return {
   set_opfunc = vim.fn[fn_ref.output],
   norm = [[<c-\><c-n>]],
+  operator_marks = function(buf, visual_type)
+    local mark1, mark2 = unpack(visual_type and {"[", "]"} or {"<", ">"})
+
+    local row1, col1 = unpack(vim.api.nvim_buf_get_mark(buf, mark1))
+    local row2, col2 = unpack(vim.api.nvim_buf_get_mark(buf, mark2))
+
+    return {row1 - 1, col1, row2 - 1, col2 + 1}
+  end,
   set_visual_selection = function(win, mode, r1, c1, r2, c2, reverse)
     local cmd = [[norm! ]] .. mode
     local lo = {r1 + 1, c1}
     local hi = {r2 + 1, math.max(0, c2 - 1)}
+
     if reverse then
       vim.api.nvim_win_set_cursor(win, hi)
       vim.cmd(cmd)
--- layers/posix/home/.config/nvim/lua/go/text_objects/move.lua
+++ layers/posix/home/.config/nvim/lua/go/text_objects/move.lua
@@ -1,4 +1,6 @@
-Go.op_move = function(up)
+local to = require("go.text_objects")
+
+Go.op_norm_move = function(up)
   local row = unpack(vim.api.nvim_win_get_cursor(0))
   local count = vim.api.nvim_buf_line_count(0)
 
@@ -13,9 +15,39 @@ Go.op_move = function(up)
   end
 end
 
-local cmd = function(hold)
-  return [[<cmd>lua Go.op_move(]] .. tostring(hold) .. [[)<cr>]]
+local cmd = function(name, hold)
+  return [[<cmd>lua Go.]] .. name .. [[(]] .. tostring(hold) .. [[)<cr>]]
 end
 
-vim.keymap.set("n", "<m-up>", cmd(true), {noremap = true})
-vim.keymap.set("n", "<m-down>", cmd(false), {noremap = true})
+vim.keymap.set("n", "<m-up>", cmd("op_norm_move", true), {noremap = true})
+vim.keymap.set("n", "<m-down>", cmd("op_norm_move", false), {noremap = true})
+
+Go.op_visual_move = function(up)
+  local row = unpack(vim.api.nvim_win_get_cursor(0))
+  local row1, col1, row2, col2 = unpack(to.operator_marks(0, nil))
+  local count = vim.api.nvim_buf_line_count(0)
+
+  if up then
+    if row <= 1 then
+      vim.cmd [[norm! gv]]
+    else
+      vim.cmd(row1 + 1 .. "," .. row2 + 1 .. "move " .. row1 - 1)
+      to.set_visual_selection(0, "v", row1 - 1, col1, row2 - 1, col2, true)
+    end
+  else
+    if row2 + 1 >= count then
+      vim.cmd [[norm! gv]]
+    else
+      vim.cmd(row1 + 1 .. "," .. row2 + 1 .. "move " .. row2 + 2)
+      to.set_visual_selection(0, "v", row1 + 1, col1, row2 + 1, col2, true)
+    end
+  end
+end
+
+vim.keymap.set("v", "<m-up>", to.norm .. cmd("op_visual_move", true), {noremap = true})
+vim.keymap.set(
+  "v",
+  "<m-down>",
+  to.norm .. cmd("op_visual_move", false),
+  {noremap = true}
+)
