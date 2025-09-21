99f341836e3f5195834dfcdcd3ae2d8cd932b9a4
dogisgreat <git@bigly.dog>
2025-09-21 05:43:52 -0400

    fmt

--- layers/posix/home/.config/nvim/lua/go/formatter.lua
+++ layers/posix/home/.config/nvim/lua/go/formatter.lua
@@ -1,20 +1,55 @@
 Go.run_fmt = function()
-  local name = vim.api.nvim_buf_get_name(0)
+  local buf = vim.api.nvim_get_current_buf()
+  local name = vim.api.nvim_buf_get_name(buf)
   local cwd = name ~= "" and vim.fs.dirname(name) or vim.fn.getcwd()
-  local timeout = 99
+  local timeout = 3000
 
-  local cmd = {"tee", "--"}
+  local cmd = {"sleep", "--", "2"}
 
-  local on_stderr = function()
+  local handle = nil
+  local on_exit = function(waited)
+    vim.uv.timer_stop(handle)
+
+    if waited.signal ~= 0 then
+      vim.notify(
+        [[☠️ pid $?=]] .. waited.pid .. [[signal $?=]] .. waited.signal,
+        vim.log.levels.ERROR,
+        {}
+      )
+      return
+    elseif waited.code ~= 0 then
+      vim.notify(
+        [[⚠️ pid $?=]] .. waited.pid .. [[code $?]] .. waited.code,
+        waited.stderr,
+        vim.log.levels.ERROR,
+        {}
+      )
+      return
+    end
+
+    if vim.api.nvim_get_current_buf() == buf then
+      local lines = {}
+      for line in string.gmatch(waited.stdout, [[^$]]) do
+        table.insert(lines, line)
+      end
+
+      vim.api.nvim_buf_set_lines(buf, 0, -1, true, lines)
+      vim.notify([[✅...]], vim.log.levels.INFO, {})
+    end
   end
-  local on_exit = function()
-  end
 
-  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
-  local opts = {cwd = cwd, stdin = lines, stderr = on_stderr, text = true}
-  local proc = vim.system(cmd, opts, on_exit)
+  local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, true)
+  local opts = {cwd = cwd, stdin = lines, text = true}
+  local proc = vim.system(cmd, opts, vim.schedule_wrap(on_exit))
 
-  proc:wait(timeout)
+  handle =
+    vim.defer_fn(
+    function()
+      proc:wait(0)
+    end,
+    timeout
+  )
+  vim.notify([[⏳...]], vim.log.levels.INFO, {})
 end
 
 vim.opt.formatexpr = "v:Go.run_fmt()"
