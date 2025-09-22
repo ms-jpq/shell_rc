a461a5f71ac830d06a1a1eda782de203e6639e0a
dogisgreat <git@bigly.dog>
2025-09-22 01:00:31 -0400

    nice

--- layers/posix/home/.config/nvim/lua/go/operators/replace.lua
+++ layers/posix/home/.config/nvim/lua/go/operators/replace.lua
@@ -1,13 +1,31 @@
-local lib = require("go")
 local to = require("go.text_objects")
 
 Go.op_replace = function(visual_type)
-  vim.print(visual_type)
   local row1, col1, row2, col2 = to.operator_marks(0, visual_type)
-  local replacement = vim.split(vim.fn.getreg(), "\n", true) 
+  local replacement = vim.split(vim.fn.getreg(), "\n", true)
 
   vim.api.nvim_buf_set_text(0, row1, col1, row2, col2, replacement)
 end
 
-vim.keymap.set("n", "gb", [[<cmd>set opfunc=v:lua.Go.op_replace<cr>g@]], {noremap = true})
-vim.keymap.set("v", "gb", to.norm .. [[<cmd>lua Go.op_replace(vim.NIL)<cr>]], {noremap = true})
+vim.keymap.set(
+  "n",
+  "gb",
+  [[<cmd>set opfunc=v:lua.Go.op_replace<cr>g@]],
+  {noremap = true}
+)
+vim.keymap.set(
+  "v",
+  "gb",
+  to.norm .. [[<cmd>lua Go.op_replace(vim.NIL)<cr>]],
+  {noremap = true}
+)
+
+local replace_line = function()
+  local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  local text = string.gsub(vim.fn.getreg(), [[^%s+|%s+$]], "")
+  local lines = vim.split(text, "\n", true)
+
+  vim.api.nvim_buf_set_lines(0, row - 1, row, true, lines)
+end
+
+vim.keymap.set("n", "gbb", replace_line, {noremap = true})
