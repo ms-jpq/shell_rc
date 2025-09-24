4a907132b67b976021c73bcda542444af56fd395
dogisgreat <git@bigly.dog>
2025-09-24 18:46:18 -0400

    rgb

--- layers/posix/home/.config/nvim/lua/go/pack/start/fzf.lua
+++ layers/posix/home/.config/nvim/lua/go/pack/start/fzf.lua
@@ -22,14 +22,32 @@ vim.keymap.set("n", [[<leader>f]], [[<cmd>Files<cr>]], {noremap = true})
 vim.keymap.set("n", [[<leader>g]], [[<cmd>GFiles?<cr>]], {noremap = true})
 vim.keymap.set("n", [[<leader>G]], [[<cmd>GFiles<cr>]], {noremap = true})
 
-vim.keymap.set("n", [[<leader>/]], [[<cmd>BL<cr>]], {noremap = true})
-vim.keymap.set("n", [[<leader>?]], [[<cmd>RG<cr>]], {noremap = true})
+vim.keymap.set("n", [[<leader>/]], [[<cmd>BL!<cr>]], {noremap = true})
+vim.keymap.set("n", [[<leader>?]], [[<cmd>RG!<cr>]], {noremap = true})
 
 vim.api.nvim_create_user_command(
   "RG",
   function(opts)
     local cmd = [[rg --fixed-strings --column --line-number --no-heading --color=always --smart-case --]]
-    vim.fn["fzf#vim#grep2"](cmd, opts.args, vim.fn["fzf#vim#with_preview"](), opts.bang)
+    local preview = vim.fn["fzf#vim#with_preview"]()
+    vim.fn["fzf#vim#grep2"](cmd, opts.args, preview, opts.bang)
+  end,
+  {
+    force = true,
+    bang = true,
+    nargs = "*"
+  }
+)
+
+vim.api.nvim_create_user_command(
+  "BL",
+  function(opts)
+    local name = vim.fn.shellescape(vim.api.nvim_buf_get_name(0))
+    local cmd =
+      [[rgb.sh ]] .. name .. [[ --fixed-strings --column --line-number --no-heading --color=always --smart-case --]]
+
+    local preview = {options = {[[--preview=bat --force-colorization --highlight-line {1} -- ]] .. name}}
+    vim.fn["fzf#vim#grep2"](cmd, opts.args, preview, opts.bang)
   end,
   {
     force = true,
