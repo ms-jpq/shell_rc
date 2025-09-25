cd088e108f278ba3333bb8071940adcc79aba83a
dogisgreat <git@bigly.dog>
2025-09-24 22:14:24 -0400

    k

--- layers/posix/home/.config/nvim/lua/go/terminal.lua
+++ layers/posix/home/.config/nvim/lua/go/terminal.lua
@@ -1,1 +1,24 @@
-local server = vim.fn.serverstart()
+local server = vim.env.NVIM_SERVERNAME or vim.fn.serverstart()
+
+vim.keymap.set(
+  "n",
+  [[<leader>u]],
+  function()
+    local cmd = {
+      "tmux",
+      "display-popup",
+      "-EE",
+      "-d",
+      "#{pane_current_path}",
+      "-w",
+      "95%",
+      "-h",
+      "95%",
+      "-e",
+      "NVIM_SERVERNAME=" .. server,
+      "--"
+    }
+    vim.system(cmd):wait(666)
+  end,
+  {noremap = true}
+)
