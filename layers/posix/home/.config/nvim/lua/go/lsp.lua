733092887b3763a90d7c73cf785c928c8beecde9
dogisgreat <git@bigly.dog>
2025-09-25 01:05:32 -0400

    ok

--- layers/posix/home/.config/nvim/lua/go/lsp.lua
+++ layers/posix/home/.config/nvim/lua/go/lsp.lua
@@ -31,7 +31,11 @@ vim.api.nvim_create_autocmd(
   "LspAttach",
   {
     callback = function(args)
-      local _ = assert(vim.lsp.get_client_by_id(args.data.client_id))
+      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+      if client:supports_method("textDocument/foldingRange") then
+        local win = vim.api.nvim_get_current_win()
+        vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+      end
     end
   }
 )
