02a75c0dde956d31af3f98fbc9133babd6b6a12c
dogisgreat <git@bigly.dog>
2025-09-27 13:19:54 -0400

    prune

--- layers/posix/home/.config/nvim/lua/go/session.lua
+++ layers/posix/home/.config/nvim/lua/go/session.lua
@@ -58,25 +58,27 @@ end
 
 vim.api.nvim_create_autocmd({"VimSuspend", "FocusLost", "CursorHold"}, {callback = mk_session})
 
-local prune_session = function()
-  local dead = {}
-  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    if vim.bo[buf].buflisted then
-      local name = vim.api.nvim_buf_get_name(buf)
-      if vim.fn.filereadable(name) == 0 then
-        table.insert(dead, buf)
+vim.api.nvim_create_autocmd(
+  {"QuitPre"},
+  {
+    callback = function()
+      local dead = {}
+      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.bo[buf].buflisted then
+          local name = vim.api.nvim_buf_get_name(buf)
+          if vim.fn.filereadable(name) == 0 then
+            table.insert(dead, buf)
+          end
+        end
       end
+
+      if #dead > 0 then
+        vim.cmd([[bwipeout! ]] .. table.concat(dead, " "))
+      end
+      mk_session()
     end
-  end
-
-  if #dead > 0 then
-    vim.cmd([[bwipeout! ]] .. table.concat(dead, " "))
-  end
-  mk_session()
-end
-
-vim.api.nvim_create_user_command("MakeSession", prune_session, {})
-vim.api.nvim_create_autocmd({"VimLeavePre"}, {callback = prune_session})
+  }
+)
 
 vim.api.nvim_create_autocmd(
   {"VimEnter"},
