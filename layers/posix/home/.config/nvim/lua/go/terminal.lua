b4f1796d8e232613b7d49b30c7adcbde2fd52438
dogisgreat <git@bigly.dog>
2025-09-27 09:38:04 -0400

    home

--- layers/posix/home/.config/nvim/lua/go/terminal.lua
+++ layers/posix/home/.config/nvim/lua/go/terminal.lua
@@ -26,11 +26,15 @@ local termstart = function(cmd, env)
   vim.api.nvim_win_set_buf(0, buf)
 end
 
+local lfcmd = function()
+  return {{"lf"}, {"-config", vim.fs.joinpath(vim.fn.stdpath("config"), "lfrc.2")}}
+end
+
 vim.keymap.set(
   "n",
   [[<c-t>]],
   function()
-    local cmd = {{"lf"}, {"-config", vim.fs.joinpath(vim.fn.stdpath("config"), "lfrc.2")}}
+    local cmd = lfcmd()
     local path = vim.api.nvim_buf_get_name(0)
     if vim.fn.filereadable(path) == 0 then
       path = vim.fn.getcwd()
