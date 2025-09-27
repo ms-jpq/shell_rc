b915791e703cf3ba17b447f0cee8f66f0e2525b2
dogisgreat <git@bigly.dog>
2025-09-26 23:10:15 -0400

    ok

--- zsh/darwin/iterm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env -S -- bash
-
-if [[ -f ~/.iterm2_shell_integration.zsh ]]; then
-  export -- ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=1
-  # shellcheck disable=1090
-  source -- ~/.iterm2_shell_integration.zsh
-fi
