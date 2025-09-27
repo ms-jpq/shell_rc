3a44faa5bdd847b0f7fe6c7801a90dabf8271198
dogisgreat <git@bigly.dog>
2025-09-27 00:05:39 -0400

    tmux no skin

--- layers/posix/home/.local/opt/initd/makelib/tmux.posix.mk
+++
@@ -1,9 +0,0 @@
-.PHONY: tmux
-
-ifneq (aarch64-linux, $(HOSTTYPE)-$(OS))
-tmux: $(SHARE)/tmux
-endif
-
-$(SHARE)/tmux:
-	'$(PY_EXE)' -m venv --upgrade -- '$@'
-	'$@/$(PY_BIN)/$(PY_EXE)' -m pip install --require-virtualenv --upgrade --requirement '$(call UNIX_2_NT,$(CONFIG)/tmux/requirements.txt)'
