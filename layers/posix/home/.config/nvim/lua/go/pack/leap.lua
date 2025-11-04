080ebc83595c2bdd2b7c60e136952956045c8e70
dogisgreat <git@bigly.dog>
2025-11-04 11:27:23 -0500

    leap

--- layers/posix/home/.config/nvim/lua/go/pack/leap.lua
+++ layers/posix/home/.config/nvim/lua/go/pack/leap.lua
@@ -1,6 +1,6 @@
 local leap = require("leap")
 
-leap.set_default_mappings()
+vim.keymap.set({"n", "x", "o"}, "s", [[<Plug>(leap)]])
 
 leap.opts.preview_filter = function(ch0, ch1, ch2)
   return not (ch1:match("%s") or ch0:match("%a") and ch1:match("%a") and ch2:match("%a"))
