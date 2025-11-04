0bd91d64bdc000550b93a3b9837337bc6c344e1d
dogisgreat <git@bigly.dog>
2025-11-04 11:45:58 -0500

    high

--- layers/posix/home/.config/nvim/lua/go/pack/leap.lua
+++ layers/posix/home/.config/nvim/lua/go/pack/leap.lua
@@ -2,6 +2,7 @@ local leap = require("leap")
 
 vim.keymap.set({"n", "x", "o"}, "s", [[<Plug>(leap)]])
 
+leap.opts.highlight_unlabeled_phase_one_targets = true
 leap.opts.preview_filter = function(ch0, ch1, ch2)
   return not (ch1:match("%s") or ch0:match("%a") and ch1:match("%a") and ch2:match("%a"))
 end
