ce1fb5a8b2cf30363ca24afca4c4d5172d369add
dogisgreat <git@bigly.dog>
2025-11-30 15:21:31 -0500

    no size

--- zsh/darwin/bin/rdp.sh
+++ zsh/darwin/bin/rdp.sh
@@ -3,22 +3,17 @@
 set -o pipefail
 
 OPTS='s:,u:,p:,h:'
-LONG_OPTS='size:,user:,pass:,host:'
+LONG_OPTS='user:,pass:,host:'
 GO="$(getopt --options="$OPTS" --longoptions="$LONG_OPTS" --name="$0" -- "$@")"
 eval -- set -- "$GO"
 
 ADMIN='administrator'
 
-SIZE=''
 UNAME="$ADMIN"
 PASS=''
 HOST=''
 while true; do
   case "$1" in
-  -s | --size)
-    SIZE="$2"
-    shift -- 2
-    ;;
   -u | --user)
     UNAME="$2"
     shift -- 2
@@ -46,16 +41,9 @@ if [[ -z $HOST ]]; then
   exit 2
 fi
 
-if [[ -z $SIZE ]]; then
-  set -x
-  system_profiler SPDisplaysDataType | sed -E -n 's#^ +Resolution: ([0-9]+) x ([0-9]+).*$#\1x\2#p'
-  exit 2
-fi
-
 ARGV=(
   sdl-freerdp
   /cert:ignore
-  "/size:$SIZE"
   "/u:$UNAME"
   "/p:$PASS"
   "/v:$HOST"
