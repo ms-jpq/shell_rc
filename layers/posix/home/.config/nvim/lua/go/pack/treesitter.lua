d318c8e442cbb4b484e25c1e081312bf924b173a
dogisgreat <git@bigly.dog>
2026-02-02 19:09:42 -0500

    mv treesitter

--- layers/posix/home/.config/nvim/lua/go/pack/treesitter.lua
+++ layers/posix/home/.config/nvim/lua/go/pack/treesitter.lua
@@ -1,81 +1,95 @@
 local lib = require("go")
 
-require("nvim-treesitter.config").setup {
-  auto_install = not lib.is_win,
-  highlight = {
-    enable = true,
-    disable = {"json", "yaml"}
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "+",
-      node_decremental = "_",
-      node_incremental = "+"
-    }
-  },
-  indent = {
-    enable = true
-  },
-  textobjects = {
-    lsp_interop = {
-      enable = true,
-      peek_definition_code = {
-        ["L"] = "@function.outer",
-        ["M"] = "@class.outer"
-      }
-    },
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ["ib"] = "@block.inner",
-        ["ab"] = "@block.outer",
-        ["iF"] = "@call.inner",
-        ["aF"] = "@call.outer",
-        ["iC"] = "@class.inner",
-        ["aC"] = "@class.outer",
-        ["ic"] = "@conditional.inner",
-        ["ac"] = "@conditional.outer",
-        ["if"] = "@function.inner",
-        ["af"] = "@function.outer",
-        ["iL"] = "@loop.inner",
-        ["aL"] = "@loop.outer",
-        ["ia"] = "@parameter.inner",
-        ["aa"] = "@parameter.outer",
-        ["is"] = "@statement.outer",
-        ["as"] = "@statement.outer"
-      }
-    },
-    move = {
-      enable = true,
-      set_jumps = true,
-      goto_next_start = {
-        ["]s"] = "@statement.outer",
-        ["]m"] = "@block.outer"
-      },
-      goto_next_end = {
-        ["]S"] = "@statement.outer",
-        ["]M"] = "@block.outer"
-      },
-      goto_previous_start = {
-        ["[s"] = "@statement.outer",
-        ["[m"] = "@block.outer"
-      },
-      goto_previous_end = {
-        ["[S"] = "@statement.outer",
-        ["[M"] = "@block.outer"
-      }
-    },
-    swap = {
-      enable = true,
-      swap_previous = {},
-      swap_next = {}
-    }
+require("nvim-treesitter").setup {}
+
+local parsers = {}
+for parser, _ in pairs(require("nvim-treesitter.parsers")) do
+  table.insert(parsers, parser)
+end
+
+vim.api.nvim_create_autocmd(
+  "FileType",
+  {
+    group = lib.group,
+    pattern = parsers,
+    callback = function()
+      vim.treesitter.start()
+    end
   }
-}
+)
 
 require("treesitter-context").setup {
   enable = true,
   multiline_threshold = 1
 }
+
+require("nvim-treesitter-textobjects").setup {
+  select = {
+    lookahead = true
+  }
+}
+
+local selector = require("nvim-treesitter-textobjects.select")
+for lhs, rhs in pairs {
+  ["ib"] = "@block.inner",
+  ["ab"] = "@block.outer",
+  ["iF"] = "@call.inner",
+  ["aF"] = "@call.outer",
+  ["iC"] = "@class.inner",
+  ["aC"] = "@class.outer",
+  ["ic"] = "@conditional.inner",
+  ["ac"] = "@conditional.outer",
+  ["if"] = "@function.inner",
+  ["af"] = "@function.outer",
+  ["iL"] = "@loop.inner",
+  ["aL"] = "@loop.outer",
+  ["ia"] = "@parameter.inner",
+  ["aa"] = "@parameter.outer",
+  ["is"] = "@statement.outer",
+  ["as"] = "@statement.outer"
+} do
+  vim.keymap.set(
+    {"x", "o"},
+    lhs,
+    function()
+      selector.select_textobject(rhs, "textobjects")
+    end
+  )
+end
+
+local mover = require("nvim-treesitter-textobjects.move")
+for lhs, rhs in pairs {
+  goto_next_start = {
+    ["]s"] = "@statement.outer",
+    ["]m"] = "@block.outer"
+  },
+  goto_next_end = {
+    ["]S"] = "@statement.outer",
+    ["]M"] = "@block.outer"
+  },
+  goto_previous_start = {
+    ["[s"] = "@statement.outer",
+    ["[m"] = "@block.outer"
+  },
+  goto_previous_end = {
+    ["[S"] = "@statement.outer",
+    ["[M"] = "@block.outer"
+  }
+} do
+  for l, r in pairs(rhs) do
+    vim.keymap.set(
+      {"n", "x", "o"},
+      l,
+      function()
+        mover[lhs](r, "textobjects")
+      end
+    )
+  end
+end
+
+-- lsp_interop = {
+--   peek_definition_code = {
+--     ["L"] = "@function.outer",
+--     ["M"] = "@class.outer"
+--   }
+-- },
--- layers/posix/home/.config/nvim/lua/go/treesitter.lua
+++ layers/posix/home/.config/nvim/lua/go/treesitter.lua
@@ -1,1 +1,2 @@
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
