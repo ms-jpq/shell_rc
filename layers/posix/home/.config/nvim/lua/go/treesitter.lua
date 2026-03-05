95b9f86d70e3a42fd01bb676d288b00732041349
dogisgreat <hao@stainlessapi.com>
2026-03-05 10:11:28 -0500

    ok

--- layers/posix/home/.config/nvim/lua/go/treesitter.lua
+++ layers/posix/home/.config/nvim/lua/go/treesitter.lua
@@ -1,2 +1,2 @@
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+vim.opt.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
