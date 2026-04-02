8cda337fccea342f4d6685c2934899238af7e6a6
dogisgreat <git@bigly.dog>
2026-04-02 14:47:14 -0400

    lmap

--- layers/posix/home/.config/nvim/libexec/language-map.jq
+++ layers/posix/home/.config/nvim/libexec/language-map.jq
@@ -2,6 +2,7 @@
 
 . as $stdin
 | $lmap[] as $mapping
-| $stdin | .language | [to_entries[] | select(.value.formatter)]
-| map({key: ($mapping[.key] // .key), value: (.value.formatter | .args |= . // [])})
+| $stdin | .language
+| [to_entries[] | select(.value.formatter) | .value as $v | ($mapping[.key] // [.key])[] | {key: ., value: $v}]
+| map({key: .key, value: (.value.formatter | .args |= . // [])})
 | from_entries
--- makelib/nvim.mk
+++ makelib/nvim.mk
@@ -9,9 +9,9 @@ clobber.nvim:
 	rm -vfr -- $(NVIM)/apriori/{lsp,fmt}.json
 
 nvim: $(NVIM)/apriori/lsp.json
-$(NVIM)/apriori/lsp.json: $(NVIM)/libexec/languages.sh $(NVIM)/apriori/lsp.yml | $(VENV)/$(PY_BIN)
-	'$<' < '$(NVIM)/apriori/lsp.yml' > '$@'
+$(NVIM)/apriori/lsp.json: $(NVIM)/libexec/languages.sh $(NVIM)/lsp.yml | $(VENV)/$(PY_BIN)
+	'$<' < '$(NVIM)/lsp.yml' > '$@'
 
 nvim: $(NVIM)/apriori/fmt.json
-$(NVIM)/apriori/fmt.json: $(NVIM)/libexec/language-map.jq $(NVIM)/apriori/language-map.json $(HELIX)/languages.json
-	'$<' --slurpfile lmap '$(NVIM)/apriori/language-map.json' < '$(HELIX)/languages.json' > '$@'
+$(NVIM)/apriori/fmt.json: $(NVIM)/libexec/language-map.jq $(NVIM)/language-map.json $(HELIX)/languages.json
+	'$<' --slurpfile lmap '$(NVIM)/language-map.json' < '$(HELIX)/languages.json' > '$@'
