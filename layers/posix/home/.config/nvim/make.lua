f8847a9cd03d309ef70a85eafb2ce061817c6140
dogisgreat <git@bigly.dog>
2026-04-02 09:19:02 -0400

    ok

--- layers/posix/home/.config/nvim/make.lua
+++ layers/posix/home/.config/nvim/make.lua
@@ -10,4 +10,6 @@ vim.opt.rtp:append {
 
 require("go.paths")
 require("go.pack.treesitter")
+
+vim.env.PATH = vim.env.PATH .. ":" .. vim.fs.joinpath(vim.env.HOME, ".local", "asdf", "shims")
 require("nvim-treesitter").install {"all"}:wait(5 * 60 * 1000)
