4812381a1d92679485c5372e47af249e7b8093ee
dogisgreat <git@bigly.dog>
2026-04-02 23:17:08 -0400

    esc

--- layers/posix/home/.config/nvim/lua/go/plugins.lua
+++ layers/posix/home/.config/nvim/lua/go/plugins.lua
@@ -2,10 +2,10 @@ vim.opt.loadplugins = false
 vim.g.no_plugin_maps = 1
 
 local man = unpack(vim.api.nvim_get_runtime_file("plugin/man.*", true))
-vim.cmd.source(man)
+vim.cmd.source(vim.fn.fnameescape(man))
 
 local parens = unpack(vim.api.nvim_get_runtime_file("plugin/matchparen.vim", true))
-vim.cmd.source(parens)
+vim.cmd.source(vim.fn.fnameescape(parens))
 
 vim.cmd.packadd [[matchit]]
 vim.cmd.packadd [[nvim.difftool]]
