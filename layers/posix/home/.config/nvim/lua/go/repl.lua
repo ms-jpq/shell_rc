10a032968f9f20c8213d9c048d90c81f953935ca
dogisgreat <git@bigly.dog>
2026-04-05 03:33:59 -0400

    -

--- layers/posix/home/.config/nvim/lua/go/repl.lua
+++ layers/posix/home/.config/nvim/lua/go/repl.lua
@@ -1,7 +1,7 @@
 local lib = require("go")
-local ns = vim.api.nvim_create_namespace()
 
 local tmux_buf = string.gsub("nvim-" .. vim.fn.tempname(), "/", "-")
+local ns = vim.api.nvim_create_namespace(tmux_buf)
 local send = function(buf, lo, hi, pane)
   local sep = lib.buf_linefeed(buf)
   local lines = vim.api.nvim_buf_get_lines(buf, lo, hi, true)
@@ -11,7 +11,7 @@ local send = function(buf, lo, hi, pane)
     pcall(
     function()
       for _, stdin in ipairs {text, sep} do
-        local proc1 = vim.system({"tmux", "load-buffer", "-b", tmux_buf, "--", "-"}, {stdin = text}):wait()
+        local proc1 = vim.system({"tmux", "load-buffer", "-b", tmux_buf, "--", "-"}, {stdin = stdin}):wait()
         assert(proc1.code == 0, vim.inspect(proc1))
         local proc2 = vim.system("tmux", "paste-buffer", "-r", "-p", "-t", pane, "-b", tmux_buf):wait()
         assert(proc2.code == 0, vim.inspect(proc2))
@@ -22,7 +22,7 @@ local send = function(buf, lo, hi, pane)
   if not ok then
     vim.notify(err, vim.log.levels.ERROR)
 
-    local proc3 = vim.system({"tmux", "delete-buffer", "-b", tmux_buf}, {stdin = text}):wait()
+    local proc3 = vim.system({"tmux", "delete-buffer", "-b", tmux_buf}):wait()
     assert(proc3.code == 0, vim.inspect(proc3))
   end
 end
