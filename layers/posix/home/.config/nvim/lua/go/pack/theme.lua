655ce332161ac50d16602fe3c5643ca6e6ee986d
dogisgreat <git@bigly.dog>
2026-04-05 01:36:48 -0400

    theme

--- layers/posix/home/.config/nvim/lua/go/pack/theme.lua
+++ layers/posix/home/.config/nvim/lua/go/pack/theme.lua
@@ -1,9 +1,13 @@
--- https://github.com/EdenEast/nightfox.nvim
-
 vim.opt.background = [[light]]
 
--- vim.cmd [[colorscheme dayfox]]
-vim.cmd.colorscheme([[iceberg]])
+do
+  -- https://github.com/EdenEast/nightfox.nvim
+  -- vim.cmd [[colorscheme dayfox]]
+end
 
--- theme
-vim.cmd.source(vim.fs.joinpath(vim.fn.stdpath("config"), "plugin", "theme.vim"))
+do
+  vim.cmd.colorscheme([[iceberg]])
+
+  -- theme
+  vim.cmd.source(vim.fs.joinpath(vim.fn.stdpath("config"), "plugin", "theme.vim"))
+end
