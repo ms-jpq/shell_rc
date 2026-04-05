ea18d92d6aaaeb82d6d704e444819d7c25abec86
dogisgreat <git@bigly.dog>
2026-04-05 19:52:50 -0400

    -

---
+++ layers/posix/home/.config/yazi/libexec/copy-relative.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+TOP="$(git --no-optional-locks rev-parse --path-format=absolute --show-toplevel || printf -- '%s' "$PWD")"
+realpath --no-symlinks --relative-to "$TOP" -- "$@" | sed -E -z -e 's/\n$//' | ~/.config/zsh/bin/c
