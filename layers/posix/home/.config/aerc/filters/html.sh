24635a462644183615dffdfc352db14ae7b3b671
dogisgreat <git@bigly.dog>
2026-04-04 23:32:53 -0400

    -

--- layers/posix/home/.config/aerc/filters/html.sh
+++ layers/posix/home/.config/aerc/filters/html.sh
@@ -2,14 +2,20 @@
 
 set -o pipefail
 
-COLS="$(stty size < /dev/tty | cut -d ' ' -f 2)"
+if [[ -v AERC_MIME_TYPE ]]; then
+  COLOUR=1
+  COLS="$(stty size < /dev/tty | cut -d ' ' -f 2)"
+else
+  COLOUR=0
+  COLS=168
+fi
 
 ARGV=(
   w3m -W
   -T text/html
   -graph
   -cols "$COLS"
-  -o color=1
+  -o color="$COLOUR"
   -o display_image=1
   -o display_link_number=1
   -o ignorecase_search=1
@@ -22,12 +28,23 @@ YELLOW=$'\e[0;33m'
 BOLD_YELLOW=$'\e[1;33m'
 CLS=$'\e[0m'
 
+if [[ -v AERC_MIME_TYPE ]]; then
+  FILTER=(
+    sed -E
+    -e "s#(\[[[:digit:]]+\])[[:space:]]?#$BOLD_RED\1 $CLS#g"
+    -e "s#(https?://[^ ]+)#$GREEN\1$CLS#g"
+    -e "s#(mailto:[^ ]+)#$YELLOW\1$CLS#g"
+    -e "s#(^|[[:space:]]+)([[:digit:]]{6,})([[:space:]]+|$)#$BOLD_YELLOW\2$CLS#g"
+  )
+else
+  FILTER=(cat --)
+fi
+
 SED=(
   sed -E
-  -e "s#(\[[[:digit:]]+\])[[:space:]]?#$BOLD_RED\1 $CLS#g"
-  -e "s#(https?://[^ ]+)#$GREEN\1$CLS#g"
-  -e "s#(mailto:[^ ]+)#$YELLOW\1$CLS#g"
-  -e "s#(^|[[:space:]]+)([[:digit:]]{6,})([[:space:]]+|$)#$BOLD_YELLOW\2$CLS#g"
+  -e $'s/\u2007/ /g'
+  -e $'s/\u034f/ /g'
+  -e '/^[[:space:]]+$/d'
 )
 
-"${ARGV[@]}" "$@" | "${SED[@]}"
+"${ARGV[@]}" "$@" | "${FILTER[@]}" | "${SED[@]}"
