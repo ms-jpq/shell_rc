69f7caf54bedaa2c3a7cc8ecea84d36009dc07a7
dogisgreat <git@bigly.dog>
2026-04-05 11:24:51 -0400

    -

--- layers/posix/home/.config/nvim/lua/go/status_line.lua
+++ layers/posix/home/.config/nvim/lua/go/status_line.lua
@@ -1,14 +1,26 @@
-local preview = "%w"
-local ql = "%q"
-local name = "%f"
-local modified = "%m"
-local diagnostic_status = "%{v:lua.vim.diagnostic.status()}"
-local ft = "%y"
-local tabs = "%{&tabstop .. (&expandtab ? 'S' : 'T')}"
-local linefeed = "%{&fileformat}"
-local pos = "%5l:%-3c"
-local scroll = "%3p%%"
+local lhs = (function()
+  local preview = "%w"
+  local ql = "%q"
+  local name = "%f"
+  local modified = "%m"
 
-local lhs = preview .. ql .. name .. modified
-local rhs = diagnostic_status .. " | " .. ft .. " " .. tabs .. " " .. linefeed .. " @ " .. pos .. "≡ " .. scroll
+  return preview .. ql .. name .. modified
+end)()
+
+local rhs =
+  (function()
+  local progress = "%{%v:lua.vim.ui.progress_status()%}"
+  local diagnostics = "%{%v:lua.vim.diagnostic.status()%}"
+
+  local busy = "%{&busy > 0 ? '◐ ' : ''}"
+  local ft = "%y"
+  local tabs = "%{&tabstop .. (&expandtab ? 'S' : 'T')}"
+  local linefeed = "%{&fileformat}"
+  local pos = "%5l:%-3c"
+  local scroll = "%3p%%"
+
+  return progress ..
+    " " .. busy .. diagnostics .. " | " .. ft .. " " .. tabs .. " " .. linefeed .. " @ " .. pos .. "≡ " .. scroll
+end)()
+
 vim.opt.statusline = lhs .. " %= " .. rhs
