ea6c8162ef05fea46a2b7c61e8ec6e258d282b54
dogisgreat <git@bigly.dog>
2026-04-05 11:07:02 -0400

    -

--- layers/posix/home/.config/nvim/lua/go/status_line.lua
+++ layers/posix/home/.config/nvim/lua/go/status_line.lua
@@ -2,7 +2,7 @@ local preview = "%w"
 local ql = "%q"
 local name = "%f"
 local modified = "%m"
-local lsp_status = "%{v:lua.vim.lsp.status()}"
+local diagnostic_status = "(%{v:lua.vim.diagnostic.status()})"
 local ft = "%y"
 local tabs = "%{&tabstop .. (&expandtab ? 'S' : 'T')}"
 local linefeed = "%{&fileformat}"
@@ -10,5 +10,5 @@ local pos = "%5l:%-3c"
 local scroll = "%3p%%"
 
 local lhs = preview .. ql .. name .. modified
-local rhs = lsp_status .. " | " .. ft .. " " .. tabs .. " " .. linefeed .. " @ " .. pos .. "≡ " .. scroll
+local rhs = diagnostic_status .. " | " .. ft .. " " .. tabs .. " " .. linefeed .. " @ " .. pos .. "≡ " .. scroll
 vim.opt.statusline = lhs .. " %= " .. rhs
