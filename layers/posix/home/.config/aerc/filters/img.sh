24f5af9f95592b1efcca20a85813bab3d7ef52b2
dogisgreat <git@bigly.dog>
2026-04-05 00:36:10 -0400

    -

---
+++ layers/posix/home/.config/aerc/filters/img.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+COLS="$(stty size < /dev/tty | cut -d ' ' -f 2)"
+
+exec -- catimg -w "$COLS" -
