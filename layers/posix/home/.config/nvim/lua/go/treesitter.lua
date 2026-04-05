2a000b4566e79e71f996b67090c8957496d6a66a
dogisgreat <git@bigly.dog>
2026-04-05 12:12:18 -0400

    -

--- layers/posix/home/.config/nvim/lua/go/treesitter.lua
+++ layers/posix/home/.config/nvim/lua/go/treesitter.lua
@@ -1,6 +1,38 @@
+local lib = require("go")
+
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
 
 vim.keymap.set("n", "<M-o>", "van", {remap = true})
 vim.keymap.set({"x", "o"}, "<M-o>", "an", {remap = true})
 vim.keymap.set({"x", "o"}, "<M-i>", "in", {remap = true})
+
+do
+  local ok, parsers = pcall(require, "nvim-treesitter.parsers")
+  if not ok then
+    parsers = {}
+  end
+
+  local filetypes = {}
+  for parser, _ in pairs(parsers) do
+    for _, ft in pairs(vim.treesitter.language.get_filetypes(parser)) do
+      table.insert(filetypes, ft)
+    end
+  end
+
+  vim.api.nvim_create_autocmd(
+    "FileType",
+    {
+      group = lib.group,
+      pattern = filetypes,
+      callback = function(args)
+        local _, _ =
+          pcall(
+          function()
+            vim.treesitter.start(args.buf)
+          end
+        )
+      end
+    }
+  )
+end
