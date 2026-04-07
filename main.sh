18f533f9804d7ae8b349c1afd6e09b8178f7d0b3
dogisgreat <git@bigly.dog>
2026-04-07 02:37:18 -0400

    -

--- main.sh
+++ main.sh
@@ -48,7 +48,7 @@ CONN=(
   ssh
   -o 'ClearAllForwardings=yes'
   -o 'ControlMaster=auto'
-  -o "ControlPath=./var/%C"
+  -o "ControlPath=$PWD/var/%C"
   -o 'ControlPersist=60'
   -p $((PORT))
   --
