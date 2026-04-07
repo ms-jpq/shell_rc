967b08c0317cfbdd88f5f07376b1f8662d7a3887
dogisgreat <git@bigly.dog>
2026-04-07 00:12:56 -0400

    -

---
+++ layers/posix/home/.config/helix/install/lemminx.java
@@ -0,0 +1,39 @@
+// ; exec java -ea -Dprogram.name="$0" "$0" "$@"
+
+import java.lang.ProcessBuilder.Redirect;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPathFactory;
+
+public class lemminx {
+  public static void main(String args[]) throws Exception {
+    final var lib = Path.of(System.getenv("LIB"));
+    final var dst = lib.resolve("org.eclipse.lemminx-uber.jar");
+    final var base =
+        URI.create(
+            "https://repo.eclipse.org/content/repositories/lemminx-releases/org/eclipse/lemminx/org.eclipse.lemminx/");
+
+    final var version =
+        XPathFactory.newInstance()
+            .newXPath()
+            .evaluate(
+                "//metadata/versioning/latest/text()",
+                DocumentBuilderFactory.newInstance()
+                    .newDocumentBuilder()
+                    .parse(base.resolve("maven-metadata.xml").toString()));
+
+    final var uri =
+        base.resolve(version).resolve("/org.eclipse.lemminx-" + version + "-uber.jar").toString();
+
+    final var p =
+        new ProcessBuilder("env", "--", "get.sh", uri).redirectError(Redirect.INHERIT).start();
+    assert p.waitFor() == 0;
+    final var jar = new String(p.getInputStream().readAllBytes());
+
+    Files.createDirectories(lib);
+    Files.copy(Path.of(jar), dst, StandardCopyOption.REPLACE_EXISTING);
+  }
+}
