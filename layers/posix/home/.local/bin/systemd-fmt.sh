a529fd285ce8d0093b5b7de3d9bf2059e2940258
dogisgreat <git@bigly.dog>
2026-04-07 02:35:09 -0400

    -

--- layers/posix/home/.local/bin/systemd-fmt.sh
+++ layers/posix/home/.local/bin/systemd-fmt.sh
@@ -54,11 +54,7 @@ fmt() {
       fi
 
       M=$((MAX - L))
-      P=" "
-      for ((i = 0; i < M; i++)); do
-        P="$P "
-      done
-
+      printf -v P -- '%*s' $((M + 1)) ""
       printf -- '%s\n' "${BASH_REMATCH[1]}${P}=${RHS}"
     elif [[ $LINE =~ ^[[:space:]]*([^[:space:]]*)[[:space:]]*$ ]]; then
       printf -- '%s\n' "${BASH_REMATCH[1]}"
