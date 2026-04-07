62488ac8b6b3e653a8cc20ac279f2fcc407408ae
dogisgreat <git@bigly.dog>
2026-04-06 23:53:55 -0400

    -

--- layers/posix/home/.config/helix/install/lemminx.sh
+++ layers/posix/home/.config/helix/install/lemminx.sh
@@ -2,21 +2,8 @@
 
 set -o pipefail
 
-BASE_URI='https://download.jboss.org/jbosstools/vscode/stable/lemminx-binary'
-
-EXT=''
-case "$OSTYPE" in
-darwin*)
-  BASENAME='lemminx-osx-x86_64.zip'
-  ;;
-linux*)
-  BASENAME='lemminx-linux.zip'
-  ;;
-*)
-  BASENAME='lemminx-win32.zip'
-  EXT='.exe'
-  ;;
-esac
+XML_SRC='https://repo.eclipse.org/content/repositories/lemminx-releases/org/eclipse/lemminx/org.eclipse.lemminx/maven-metadata.xml'
+BASE_URI='https://repo.eclipse.org/content/repositories/lemminx-releases/org/eclipse/lemminx/org.eclipse.lemminx'
 
 if ! command -v -- xmllint > /dev/null; then
   set -x
@@ -29,13 +16,10 @@ if [[ -v IPV4 ]]; then
 elif [[ -v IPV6 ]]; then
   CURL+=(-6)
 fi
-CURL+=(-- "$BASE_URI")
 
-XPATH=(xmllint --html --xpath '(//html/body/table/tr/td/a/text())[last()]' -)
-VERSION="$("${CURL[@]}" | "${XPATH[@]}")"
-URI="$BASE_URI/$VERSION$BASENAME"
+XPATH=(xmllint --xpath '//metadata/versioning/latest/text()' -)
+VERSION="$("${CURL[@]}" -- "$XML_SRC" | "${XPATH[@]}")"
+URI="$BASE_URI/$VERSION/org.eclipse.lemminx-$VERSION-uber.jar"
+
 # shellcheck disable=SC2154
-get.sh "$URI" | unpack.sh "$RUN"
-# shellcheck disable=SC2154
-mkdir -p -- "$BIN"
-mv -v -f -- "$RUN/lemminx"* "$BIN/lemminx$EXT"
+get.sh "$URI" | unpack.sh "$LIB"
