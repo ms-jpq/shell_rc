5fb9e1d7f3451a2923eb7ef5df0c68ab2b2b8e9f
dogisgreat <git@bigly.dog>
2026-04-07 22:20:28 -0400

    -

--- layers/posix/home/.config/tmux/libexec/pane-active.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
-
-set -o pipefail
-
-TMUX_PANE="${1:-"$TMUX_PANE"}"
-
-ACTIVE="$(tmux display-message -t "$TMUX_PANE" -p '#{pane_active}#{window_active}#{session_active}')"
-[[ $ACTIVE == '111' ]]
