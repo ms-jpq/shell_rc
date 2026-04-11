9bf208143add35709d87b690bbba3110607eb87c
dogisgreat <git@bigly.dog>
2026-04-11 11:16:52 -0400

    -
    -

---
+++ layers/posix/home/.config/helix/install/bin-link.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+SELF="$(realpath -- "$0")"
+BASE="${SELF%/*}"
+BASE="${BASE%/*}"
+
+SRC="$BASE/bin"
+# shellcheck disable=SC2154
+DST="$BIN"
+mkdir -p -- "$BIN"
+
+shopt -u failglob
+for F in "$SRC"/*; do
+  NAME="$(basename -- "$F")"
+  STEM="${NAME%.*}"
+  ln -v -snf -- "$F" "$DST/$STEM"
+done
