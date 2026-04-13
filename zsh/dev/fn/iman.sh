58e3cee5012d0db43f689c1929b2dd24a60b3d60
dogisgreat <git@bigly.dog>
2026-04-12 22:58:46 -0400

    -

--- zsh/dev/fn/iman.sh
+++ zsh/dev/fn/iman.sh
@@ -2,7 +2,7 @@
 
 iman() {
   local q="$*"
-  q="$(jq --exit-status --raw-input --raw-output <<< "$q")"
+  q="$(jq --exit-status --raw-input --raw-output '@uri' <<< "$q")"
   q="https://manpages.ubuntu.com/cgi-bin/search.py?q=$q"
   printf -- '%s\n' "$q"
   open -- "$q"
