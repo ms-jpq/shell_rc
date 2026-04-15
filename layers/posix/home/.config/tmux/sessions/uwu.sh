d9f136362b7ead1ad06cf56ec0852c2a8cd1e9c7
dogisgreat <git@bigly.dog>
2026-04-15 10:26:00 -0400

    -

---
+++ layers/posix/home/.config/tmux/sessions/uwu.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+tmux set-environment -g -h -- TMUX_NO_SAVE 1
+
+tmux new-window -c ~/Downloads
+tmux select-pane -m
+tmux set-buffer -- $'aerc \n'
+tmux paste-buffer -d -p
+tmux select-pane -t '{marked}'
+tmux select-pane -M
+
+tmux new-window -c ~/Downloads
+tmux select-pane -m
+tmux set-buffer -- $'bash ~/.local/opt/ai/bin/notificationd \n'
+tmux paste-buffer -d -p
+tmux select-pane -t '{marked}'
+tmux select-pane -M
+tmux select-window -t :-1
+
+tmux set-environment -g -h -u -- TMUX_NO_SAVE
