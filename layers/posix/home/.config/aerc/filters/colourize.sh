f255097b6316ba6f812b1372f741c1fcf19dc46b
dogisgreat <hao@stainlessapi.com>
2026-04-15 12:47:58 -0400

    -

---
+++ layers/posix/home/.config/aerc/filters/colourize.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+MAX=128
+
+C="$COLUMNS"
+if ((C > MAX)); then
+  C=$((MAX))
+fi
+
+TEXT="$(tee)"
+MAX_LINE_LEN="$(wc --max-line-length <<< "$TEXT")"
+
+colorize <<< "$TEXT" | if ((MAX_LINE_LEN > (COLUMNS / 2))); then
+  printf -- '%s' "$TEXT"
+else
+  pr --omit-header --omit-pagination --indent $((C / 2)) --width $((C))
+fi
