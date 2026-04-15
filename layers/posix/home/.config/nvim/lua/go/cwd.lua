e2ebbc15c0e4aa8201e7e374ee3c1434068dcc55
dogisgreat <git@bigly.dog>
2026-04-14 23:45:27 -0400

    -

--- layers/posix/home/.config/nvim/lua/go/cwd.lua
+++ layers/posix/home/.config/nvim/lua/go/cwd.lua
@@ -1,21 +1,21 @@
+local async = require "go.async"
+
 local cwd = vim.fn.getcwd()
 
-local cycle = 666
-local check_cwd = nil
-check_cwd = function()
-  local chk = function()
-    if vim.fn.getcwd() == "" then
-      vim.fn.mkdir(cwd, "p")
-      vim.cmd.cd(vim.fn.fnameescape(cwd))
+async.run(function()
+  while true do
+    async.sleep(666)
+
+    local ok, err = pcall(function()
+      if vim.fn.getcwd() == "" then
+        vim.fn.mkdir(cwd, "p")
+        vim.cmd.cd(vim.fn.fnameescape(cwd))
+      end
+    end)
+
+    if not ok then
+      vim.notify(err, vim.log.levels.ERROR)
+      return
     end
   end
-
-  local go, err = pcall(chk)
-  if not go then
-    vim.notify(err, vim.log.levels.ERROR)
-  else
-    vim.defer_fn(check_cwd, cycle)
-  end
-end
-
-vim.defer_fn(check_cwd, cycle)
+end)
