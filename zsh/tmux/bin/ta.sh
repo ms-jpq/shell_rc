04408140a1246cd7125352c8904b148d07e0dc3b
dogisgreat <git@bigly.dog>
2026-04-15 09:46:15 -0400

    -

--- zsh/tmux/bin/ta.sh
+++ zsh/tmux/bin/ta.sh
@@ -3,33 +3,28 @@
 set -o pipefail
 
 SESSION="$*"
+
 # shellcheck disable=2154
-SESSION_SCRIPT="$XDG_STATE_HOME/tmux/$SESSION.1.sh"
+PLANNED_SESSION="$XDG_CONFIG_HOME/tmux/sessions/$SESSION.sh"
 
-if (($#)); then
-  if [[ -f $SESSION_SCRIPT ]]; then
-    # shellcheck disable=SC1090
-    source -- "$SESSION_SCRIPT"
-  elif [[ -v TMUX ]]; then
-    tmux new-session -d -c "$HOME" -s "$SESSION"
-    tmux switch-client -t "$SESSION"
-  else
-    tmux new-session -A -c "$HOME" -s "$SESSION"
-  fi
-else
-  SESSION_SCRIPT="$(tmux list-sessions -F '#{session_name}')"
-  SESSION="$(fzf -0 <<< "$SESSION_SCRIPT")"
-  SESSION="${SESSION:-"owo"}"
+if [[ -s $PLANNED_SESSION ]]; then
+  SESSION_SCRIPT="$PLANNED_SESSION"
+elif (($#)); then
+  # shellcheck disable=2154
   SESSION_SCRIPT="$XDG_STATE_HOME/tmux/$SESSION.1.sh"
-
-  if [[ $? -ne 130 ]]; then
-    if [[ -v TMUX ]]; then
-      tmux switch -t "$SESSION"
-    elif [[ -f $SESSION_SCRIPT ]]; then
-      # shellcheck disable=SC1090
-      source -- "$SESSION_SCRIPT"
-    else
-      tmux new-session -A -c "$HOME" -s "$SESSION"
-    fi
+else
+  if ! SESSION="$(tmux list-sessions -F '#{session_name}' | fzf -0)"; then
+    exit
   fi
+  SESSION_SCRIPT="$XDG_STATE_HOME/tmux/$SESSION.1.sh"
+fi
+
+if [[ -f $SESSION_SCRIPT ]]; then
+  # shellcheck disable=SC1090
+  source -- "$SESSION_SCRIPT"
+elif [[ -v TMUX ]]; then
+  tmux new-session -d -c "$HOME" -s "$SESSION"
+  tmux switch-client -t "$SESSION"
+else
+  tmux new-session -A -c "$HOME" -s "$SESSION"
 fi
