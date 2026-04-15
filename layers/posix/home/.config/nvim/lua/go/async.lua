3850dddd9e40dc40f44c603a15c60e4db95d9979
dogisgreat <git@bigly.dog>
2026-04-14 21:46:34 -0400

    -

---
+++ layers/posix/home/.config/nvim/lua/go/async.lua
@@ -0,0 +1,37 @@
+return {
+  wrap = function(fn)
+    return function(...)
+      local thread = coroutine.running()
+      assert(thread, "wrap: must be called inside running coroutine")
+
+      local resolved = nil
+
+      local argv = { ... }
+      table.insert(argv, function(...)
+        if coroutine.status(thread) == "running" then
+          resolved = { ... }
+        else
+          local ok, ret = coroutine.resume(thread, ...)
+          if not ok then
+            error(ret)
+          end
+        end
+      end)
+      fn(unpack(argv))
+
+      if resolved then
+        return unpack(resolved)
+      end
+      return coroutine.yield()
+    end
+  end,
+
+  run = function(fn)
+    local thread = coroutine.create(fn)
+    local ok, ret = coroutine.resume(thread)
+    if not ok then
+      local tb = debug.traceback(thread, ret)
+      error(tb)
+    end
+  end,
+}
