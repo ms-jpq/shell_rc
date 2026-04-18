1b158b155d89aaeac7e9cc23391c1ad9fa8025b8
dogisgreat <git@bigly.dog>
2026-04-18 00:43:52 -0400

    -
    -

---
+++ layers/posix/home/.config/aerc/filters/pandoc.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+MAX=128
+
+C="$COLUMNS"
+if ((C > MAX)); then
+  C=$((MAX))
+fi
+
+PANDOC=(
+  pandoc
+  --reference-links
+  --reference-location block
+  --eol lf
+  --read html-native_divs-native_spans
+  --write markdown
+  --lua-filter "${0%/*}/../libexec/html-filter.lua"
+  --columns $((C - 4))
+)
+
+"${PANDOC[@]}" | sed -E -e 's#(data:image/[^;]+;).*$#\1#g'
