5b1f9ecb3ea7db8acc9065c1e0e55a744b743624
dogisgreat <git@bigly.dog>
2026-04-22 15:35:04 -0400

    -

--- layers/posix/home/.local/opt/sandbox/libexec/sb-exec.sh
+++ layers/posix/home/.local/opt/sandbox/libexec/sb-exec.sh
@@ -55,6 +55,7 @@ ARGV=(
 
 PROFILES=(
   '(import (string-append (param "PROFILES") "/0-cli.sb"))'
+  '(import-profile "0-system.sb")'
   '(import-profile "3-confs.sb")'
   '(import-profile "3-caches.sb")'
 )
