7b0ecfd1c790672cba57dad626711253217afb3c
dogisgreat <git@bigly.dog>
2026-04-22 13:16:17 -0400

    -

--- layers/posix/home/.local/opt/sandbox/libexec/bubble-wrap.sh
+++ layers/posix/home/.local/opt/sandbox/libexec/bubble-wrap.sh
@@ -15,12 +15,10 @@ FILES=()
 while true; do
   case "$1" in
   --auth)
-    # shellcheck disable=SC2034
     AUTH=1
     shift -- 1
     ;;
   --network)
-    # shellcheck disable=SC2034
     NETWORK=1
     shift -- 1
     ;;
@@ -47,4 +45,131 @@ while true; do
   esac
 done
 
-exec -- "$@"
+# shellcheck disable=SC2154
+RO_BIND=(
+  /bin
+  /etc
+  /lib
+  /lib64
+  /opt
+  /sbin
+  /usr
+
+  "$HOME/.bashrc"
+  "$HOME/.netrc"
+  "$HOME/.tool-versions"
+  "$HOME/.zshenv"
+
+  "$HOME/.local/bin"
+  "$HOME/.local/lbin"
+  "$HOME/.local/libexec"
+  "$HOME/.local/lprofile.d"
+
+  "$XDG_CONFIG_HOME/asdf"
+  "$XDG_CONFIG_HOME/bat"
+  "$XDG_CONFIG_HOME/bottom"
+  "$XDG_CONFIG_HOME/bundle"
+  "$XDG_CONFIG_HOME/cloc"
+  "$XDG_CONFIG_HOME/fzf"
+  "$XDG_CONFIG_HOME/gem"
+  "$XDG_CONFIG_HOME/git"
+  "$XDG_CONFIG_HOME/helix"
+  "$XDG_CONFIG_HOME/irb"
+  "$XDG_CONFIG_HOME/mpv"
+  "$XDG_CONFIG_HOME/mypy"
+  "$XDG_CONFIG_HOME/npm"
+  "$XDG_CONFIG_HOME/nvim"
+  "$XDG_CONFIG_HOME/posh"
+  "$XDG_CONFIG_HOME/powershell"
+  "$XDG_CONFIG_HOME/psql"
+  "$XDG_CONFIG_HOME/python"
+  "$XDG_CONFIG_HOME/readline"
+  "$XDG_CONFIG_HOME/rediscli"
+  "$XDG_CONFIG_HOME/ripgrep"
+  "$XDG_CONFIG_HOME/terraform"
+  "$XDG_CONFIG_HOME/tmux"
+  "$XDG_CONFIG_HOME/vim"
+  "$XDG_CONFIG_HOME/yazi"
+  "$XDG_CONFIG_HOME/zsh"
+)
+
+# shellcheck disable=SC2154
+RW_BIND=(
+  "$HOME/.local/asdf"
+  "$PWD"
+  "$XDG_CACHE_HOME"
+)
+
+TMPFS=(
+  "$XDG_CACHE_HOME/aerc"
+  "$XDG_CACHE_HOME/containers"
+  "$XDG_CACHE_HOME/Firefox"
+  "$XDG_CACHE_HOME/isync"
+  "$XDG_CACHE_HOME/maildir"
+  "$XDG_CACHE_HOME/Mozilla"
+  "$XDG_CACHE_HOME/nvim"
+  "$XDG_CACHE_HOME/tmux"
+)
+
+if [[ -v TMPDIR && $TMPDIR != /tmp ]]; then
+  RW_BIND+=("$TMPDIR")
+fi
+
+# shellcheck disable=SC2154
+if ((AUTH)); then
+  RO_BIND+=(
+    "$HOME/.ssh"
+    "$XDG_CONFIG_HOME/ssh"
+    "$XDG_DATA_HOME/ssh"
+  )
+  RW_BIND+=(
+    "$HOME/.gnupg"
+    "$XDG_RUNTIME_DIR/gnupg"
+  )
+  if [[ -v SSH_AUTH_SOCK ]]; then
+    RW_BIND+=("$SSH_AUTH_SOCK")
+  fi
+fi
+
+RW_BIND+=("${DIRS[@]}" "${FILES[@]}")
+
+for P in "${USER_PROFILES[@]}"; do
+  case "$P" in
+  *)
+    set -x
+    exit 2
+    ;;
+  esac
+done
+
+BWRAP=(
+  bwrap
+  --unshare-all
+  --die-with-parent
+  --new-session
+
+  --proc /proc
+  --dev /dev
+  --dev-bind /dev/tty /dev/tty
+
+  --tmpfs /tmp
+  --tmpfs "$HOME"
+)
+
+if ((NETWORK)); then
+  BWRAP+=(--share-net)
+fi
+
+for P in "${RO_BIND[@]}"; do
+  BWRAP+=(--ro-bind-try "$P" "$P")
+done
+
+for P in "${RW_BIND[@]}"; do
+  BWRAP+=(--bind-try "$P" "$P")
+done
+
+for P in "${TMPFS[@]}"; do
+  BWRAP+=(--tmpfs "$P")
+done
+
+exec -- "${BWRAP[@]}" -- "$@"
