9446cf81af605243dc18ebc372814e25413d8366
dogisgreat <git@bigly.dog>
2026-04-23 10:05:42 -0400

    -

--- layers/posix/home/.config/nvim/lsp.lua
+++ layers/posix/home/.config/nvim/lsp.lua
@@ -30,14 +30,17 @@ for _, row in pairs(lsp_on()) do
     end
   end
 
-  local command = merged.cmd[1]
-  acc[command] = {
-    extensionToLanguage = vim.tbl_extend("force", extensions, exts),
-    command = command,
-    args = vim.list_slice(merged.cmd, 2),
-    initializationOptions = merged.init_options,
-    settings = merged.settings,
-  }
+  local mapping = vim.tbl_extend("force", extensions, exts)
+  if next(mapping) then
+    local command = merged.cmd[1]
+    acc[command] = {
+      extensionToLanguage = mapping,
+      command = command,
+      args = vim.list_slice(merged.cmd, 2),
+      initializationOptions = merged.init_options,
+      settings = merged.settings,
+    }
+  end
 end
 
 local json = vim.json.encode(acc, { indent = [[  ]], sort_keys = true })
