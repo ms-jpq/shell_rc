e5591380342a89bbb6c835467f11663ab1cfa11a
dogisgreat <git@bigly.dog>
2026-04-29 17:38:02 -0400

    -

--- layers/posix/home/.config/nvim/lua/go/pack.lua
+++ layers/posix/home/.config/nvim/lua/go/pack.lua
@@ -1,5 +1,12 @@
+local lib = require "go"
 local base = vim.fs.joinpath(vim.uv.os_homedir(), ".cache", "helix-rt", "nvim")
 local packed = vim.fs.joinpath(base, "pack")
+local opt = vim.fs.joinpath(packed, "opt")
+
+do
+  vim.opt.rtp:append { vim.fs.joinpath(opt, "*") }
+  vim.opt.packpath:append { packed }
+end
 
 local safe_require = function(module)
   local ok, err = pcall(require, module)
@@ -12,7 +19,6 @@ end
 do
   safe_require "go.pack.coq-nvim"
 
-  vim.opt.packpath:append { packed }
   local ok, err = pcall(vim.cmd.packloadall)
   if not ok then
     vim.notify(err, vim.log.levels.ERROR)
@@ -24,14 +30,26 @@ do
   end
 
   safe_require "go.pack.coq-3p"
-  safe_require "go.pack.easyalign"
-  safe_require "go.pack.fzf"
-  safe_require "go.pack.gitsigns"
-  safe_require "go.pack.illuminate"
-  safe_require "go.pack.leap"
   safe_require "go.pack.theme"
   safe_require "go.pack.treesitter"
 
   local lsp_on = safe_require "go.pack.lsp"
   lsp_on()
 end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  group = lib.group,
+  once = true,
+  callback = function()
+    local globbed = vim.fn.globpath(opt, "*/plugin/*.{lua,vim}", true, true)
+    for _, file in pairs(globbed) do
+      vim.cmd.source(file)
+    end
+
+    safe_require "go.pack.easyalign"
+    safe_require "go.pack.fzf"
+    safe_require "go.pack.gitsigns"
+    safe_require "go.pack.illuminate"
+    safe_require "go.pack.leap"
+  end,
+})
