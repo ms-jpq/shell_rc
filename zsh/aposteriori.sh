7f52d20b737d593c0d57dbed82e56a4d33812e33
dogisgreat <git@bigly.dog>
2026-04-29 16:01:52 -0400

    -

--- zsh/aposteriori.sh
+++ zsh/aposteriori.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env -S -- bash
 
-for sh in ~/.local/lprofile.d/*.sh; do
-  # shellcheck disable=SC1090
-  source -- "$sh"
-done
+# shellcheck disable=SC1090
+source -- /dev/null ~/.local/lprofile.d/*.sh
 
-# shellcheck disable=SC1091
-if [[ -v BASH ]]; then
-  sh='bash'
-else
-  sh='zsh'
+_sh="${SHELL##*/}"
+
+# shellcheck disable=SC2154
+_posh_conf="$XDG_CONFIG_HOME/posh/config.yml"
+# shellcheck disable=SC2154
+_posh_cache="$XDG_CACHE_HOME/oh-my-posh/init.dog.$_sh"
+
+# shellcheck disable=SC2312
+if [[ ! -s $_posh_cache || $_posh_conf -nt $_posh_cache || $(command -v -- oh-my-posh) -nt $_posh_cache ]]; then
+  oh-my-posh init "$_sh" --config "$_posh_conf" > "$_posh_cache"
 fi
 
-# shellcheck disable=SC2154,SC2312
-eval -- "$(oh-my-posh init "$sh" --config "$XDG_CONFIG_HOME/posh/config.yml")"
-unset -- sh
+# shellcheck disable=SC2312
+source -- "$_posh_cache"
+
+unset -- _sh _posh_conf _posh_cache
