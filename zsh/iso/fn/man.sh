80f7ada11a34210f0b51c6c96a678721519a03b0
dogisgreat <git@bigly.dog>
2026-04-29 19:04:32 -0400

    -

--- zsh/iso/fn/man.sh
+++ zsh/iso/fn/man.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S -- bash
 
-if [[ -v TMUX ]]; then
+# shellcheck disable=SC2312
+if [[ -v TMUX ]] && tmux display-message -p -- '#{window_panes}' | grep -E --quiet -v --line-regexp -e '^1$'; then
   tmux new-window -a -c '#{pane_current_path}' -- man "$@"
 else
   command -- man "$@"
