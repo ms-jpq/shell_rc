73566ac7328cdecd09fe94dd8a45d4a1ff8eeebf
dogisgreat <git@bigly.dog>
2026-04-29 16:53:56 -0400

    -

--- layers/posix/home/.config/nvim/lua/go/pack/lsp.lua
+++ layers/posix/home/.config/nvim/lua/go/pack/lsp.lua
@@ -10,35 +10,40 @@ return function()
       conf = {}
     end
 
-    local keys = { "cmd", "filetypes", "init_options", "settings" }
     local overrides = { detached = false }
-    for _, k in pairs(keys) do
-      if conf[k] then
+    for _, k in pairs { "cmd", "filetypes", "init_options", "settings" } do
+      if conf[k] and conf[k] ~= vim.NIL then
         overrides[k] = conf[k]
       end
     end
 
-    if coq then
-      overrides = coq.lsp_ensure_capabilities(overrides)
-    end
-
     vim.lsp.config(name, overrides)
     local merged = vim.lsp.config[name]
-
     local argv = merged.cmd
+
     if type(argv) == "table" then
-      local bin = unpack(argv)
+      vim.api.nvim_create_autocmd("FileType", {
+        group = lib.group,
+        pattern = merged.filetypes or { "*" },
+        once = true,
+        callback = function()
+          local bin = unpack(argv)
+          if bin and vim.fn.executable(bin) == 1 then
+            if coq then
+              overrides = coq.lsp_ensure_capabilities(overrides)
+            end
 
-      if bin and vim.fn.executable(bin) == 1 then
-        overrides.cmd = function(dispatchers, config)
-          local workdir = (config or {}).root_dir or vim.fn.getcwd()
-          local cmd = vim.iter({ lib.sandbox(workdir, conf.sandbox or {}), argv }):flatten():totable()
+            overrides.cmd = function(dispatchers, config)
+              local workdir = (config or {}).root_dir or vim.fn.getcwd()
+              local cmd = vim.iter({ lib.sandbox(workdir, conf.sandbox or {}), argv }):flatten():totable()
 
-          return vim.lsp.rpc.start(cmd, dispatchers)
-        end
-        vim.lsp.config(name, overrides)
-        vim.lsp.enable(name)
-      end
+              return vim.lsp.rpc.start(cmd, dispatchers)
+            end
+            vim.lsp.config(name, overrides)
+            vim.lsp.enable(name)
+          end
+        end,
+      })
 
       table.insert(acc, { merged, conf.extensions or vim.empty_dict() })
     else
