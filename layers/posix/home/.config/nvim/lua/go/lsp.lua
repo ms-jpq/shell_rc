e144643ad64123e08b1a8df5f44b3d6340fd363b
dogisgreat <git@bigly.dog>
2026-04-30 00:42:04 -0400

    -

--- layers/posix/home/.config/nvim/lua/go/lsp.lua
+++ layers/posix/home/.config/nvim/lua/go/lsp.lua
@@ -15,6 +15,33 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
 })
 
 do
+  local kind_hl = {
+    Class = "@type",
+    Constant = "@constant",
+    Constructor = "@constructor",
+    Enum = "@type",
+    EnumMember = "@constant",
+    Event = "@type",
+    Field = "@variable.member",
+    File = "Directory",
+    Folder = "Directory",
+    Function = "@function",
+    Interface = "@type",
+    Keyword = "@keyword",
+    Method = "@function.method",
+    Module = "@module",
+    Operator = "@operator",
+    Property = "@property",
+    Reference = "@string.special",
+    Snippet = "@string.special",
+    Struct = "@type",
+    Text = "@string",
+    TypeParameter = "@type.qualifier",
+    Unit = "@constant",
+    Value = "@constant",
+    Variable = "@variable",
+  }
+
   vim.api.nvim_create_autocmd({ "LspAttach" }, {
     group = lib.group,
     callback = function(args)
@@ -23,7 +50,12 @@ do
       end
       vim.b.__attached__ = true
 
-      vim.lsp.completion.enable(true, args.data.client_id, args.buf)
+      vim.lsp.completion.enable(true, args.data.client_id, args.buf, {
+        convert = function(item)
+          local kind = vim.lsp.protocol.CompletionItemKind[item.kind]
+          return { kind_hlgroup = kind_hl[kind] }
+        end,
+      })
 
       vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
         group = lib.group,
