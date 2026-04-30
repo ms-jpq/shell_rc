f808254b246010a52529da26e5dba27defb57b43
dogisgreat <git@bigly.dog>
2026-04-30 00:23:33 -0400

    -

--- layers/posix/home/.config/nvim/lua/go/complete.lua
+++ layers/posix/home/.config/nvim/lua/go/complete.lua
@@ -15,27 +15,38 @@ do
   for _, key in pairs { "<esc>", "<c-c>", "<left>", "<right>", "<bs>", "<c-w>", "<c-u>" } do
     vim.keymap.set("i", key, function()
       return (vim.fn.pumvisible() == 1 and ce or "") .. key
-    end, { expr = true, noremap = true })
+    end, { noremap = true, expr = true })
+  end
+end
+
+do
+  local function has_text_before_cursor()
+    local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local line = vim.api.nvim_get_current_line()
+    return string.match(string.sub(line, 1, col), "%S") ~= nil
   end
 
-  vim.keymap.set(
-    "i",
-    "<cr>",
-    [[pumvisible() ? (complete_info(['selected']).selected == -1 ? '<c-e><cr>' : '<c-y>') : '<cr>']],
-    { noremap = true, expr = true }
-  )
+  vim.keymap.set("i", [[<cr>]], function()
+    if vim.fn.pumvisible() == 0 then
+      return [[<cr>]]
+    end
+    if vim.fn.complete_info({ "selected" }).selected == -1 then
+      return [[<c-e><cr>]]
+    end
+    return [[<c-y>]]
+  end, { noremap = true, expr = true })
 
-  vim.keymap.set(
-    "i",
-    "<tab>",
-    [[pumvisible() && !empty(trim(strpart(getline('.'), 0, col('.') - 1))) ? '<c-n>' : '<tab>']],
-    { noremap = true, expr = true }
-  )
+  vim.keymap.set("i", [[<tab>]], function()
+    if vim.fn.pumvisible() ~= 0 and has_text_before_cursor() then
+      return [[<c-n>]]
+    end
+    return [[<tab>]]
+  end, { noremap = true, expr = true })
 
-  vim.keymap.set(
-    "i",
-    "<s-tab>",
-    [[pumvisible() && !empty(trim(strpart(getline('.'), 0, col('.') - 1))) ? '<c-p>' : '<bs>']],
-    { noremap = true, expr = true }
-  )
+  vim.keymap.set("i", [[<s-tab>]], function()
+    if vim.fn.pumvisible() ~= 0 and has_text_before_cursor() then
+      return [[<c-p>]]
+    end
+    return [[<bs>]]
+  end, { noremap = true, expr = true })
 end
