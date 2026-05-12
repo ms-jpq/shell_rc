cf7a27b229454177246bc61de195fffdf450c6ea
dogisgreat <git@bigly.dog>
2026-05-11 22:24:56 -0400

    -

--- layers/posix/home/.local/opt/isync/imap_notify.py
+++ layers/posix/home/.local/opt/isync/imap_notify.py
@@ -1,18 +1,17 @@
-#!/usr/bin/env -S -- PYTHONSAFEPATH= python3
+#!/usr/bin/env -S -- PYTHONSAFEPATH= /opt/homebrew/bin/python3
 
 from argparse import ArgumentParser, Namespace
 from collections.abc import Generator, Iterator
 from concurrent.futures import ThreadPoolExecutor
 from contextlib import contextmanager, nullcontext, suppress
 from functools import cache, lru_cache, partial
-from imaplib import IMAP4, IMAP4_SSL, Commands
+from imaplib import IMAP4, IMAP4_SSL
 from itertools import islice
 from logging import INFO, LogRecord, basicConfig, captureWarnings, getLogger
 from logging.handlers import SysLogHandler
 from os import linesep
 from pathlib import Path
 from platform import system
-from selectors import EVENT_READ, DefaultSelector
 from socket import gaierror
 from string import Template
 from subprocess import CalledProcessError, check_output
@@ -21,16 +20,13 @@ from syslog import LOG_MAIL, openlog, syslog
 from threading import Lock
 from time import monotonic, sleep
 
-_MINUTE, _CYCLE, _PULSE = 60, 9, 2
+_MINUTE, _IDLE = 60, 25 * 60
 _FILE = Path(__file__).resolve()
 
 
 with nullcontext():
     captureWarnings(True)
     basicConfig(format="%(message)s", level=INFO)
-
-with nullcontext():
-    Commands["IDLE"] = ("SELECTED",)
 
 
 @cache
@@ -43,14 +39,15 @@ def _auth(authn: str, user: str, now: int) -> bytes:
     parent = _FILE.parent
     state = Path.home() / ".local" / "state" / parent.name
 
-    if authn == "oauth":
-        argv = (parent / "oauth.sh", "--", state / f"{user}.oauth.gpg")
-        password = check_output(argv, text=True, timeout=_MINUTE).rstrip()
-        return f"user={user}\1auth=Bearer {password}\1\1".encode()
-    elif authn == "plain":
-        return state.joinpath(f"{user}.password").read_bytes().strip()
-    else:
-        assert False
+    match authn:
+        case "oauth":
+            argv = (parent / "oauth.sh", "--", state / f"{user}.oauth.gpg")
+            password = check_output(argv, text=True, timeout=_MINUTE).rstrip()
+            return f"user={user}\1auth=Bearer {password}\1\1".encode()
+        case "plain":
+            return state.joinpath(f"{user}.password").read_bytes().strip()
+        case _:
+            assert False
 
 
 @contextmanager
@@ -59,26 +56,22 @@ def _imap(host: str, authn: str, user: str) -> Generator[IMAP4]:
     with _lock():
         auth = _auth(authn, user=user, now=now)
 
-    cooked = False
     m = IMAP4_SSL(host=host, timeout=_MINUTE * 1.1)
     try:
-        if authn == "oauth":
-            ok, _ = m.authenticate("XOAUTH2", lambda _: auth)
-            assert ok == "OK", ok
-        elif authn == "plain":
-            m.login(user, password=auth.decode())
+        match authn:
+            case "oauth":
+                ok, _ = m.authenticate("XOAUTH2", lambda _: auth)
+                assert ok == "OK", ok
+            case "plain":
+                m.login(user, password=auth.decode())
 
         yield m
-    except TimeoutError:
-        cooked = True
-        raise
     finally:
-        if not cooked:
-            with suppress(IMAP4.abort):
-                m.logout()
+        with suppress(IMAP4.error, OSError):
+            m.logout()
 
 
-def _boxes(m: IMAP4) -> Iterator[str]:
+def _mailboxes(m: IMAP4) -> Iterator[str]:
     ok, data = m.list()
     assert ok == "OK", ok
     sep = b' "/" '
@@ -89,43 +82,17 @@ def _boxes(m: IMAP4) -> Iterator[str]:
         yield dir.decode()
 
 
-# https://github.com/python/cpython/issues/55454
 def _waiting(host: str, authn: str, user: str, mailbox: str) -> Iterator[None]:
-    with DefaultSelector() as sel, _imap(host, authn=authn, user=user) as m:
-        sel.register(m.file, EVENT_READ)
-
+    with _imap(host, authn=authn, user=user) as m:
         ok, _ = m.select(mailbox, readonly=True)
         assert ok == "OK", ok
 
-        for _ in range(_CYCLE):
-            tag = m._command("IDLE")
-            cooked = False
-
-            try:
-                for _ in range(_PULSE):
-                    if sel.select(timeout=_MINUTE):
-                        line = m._get_line()
-                        assert isinstance(line, bytes)
-                        lo = line.lower()
-
-                        getLogger().info("%s", f"{mailbox} -> {line!r}")
-
-                        if lo.startswith(b"* bye"):
-                            return
-
-                        if not lo.startswith(b"+ idl"):
-                            yield None
-
-                        if line.endswith(b"EXISTS"):
-                            break
-
-            except BaseException:
-                cooked = True
-                raise
-            finally:
-                if not cooked:
-                    m.send(b"DONE\r\n")
-                    m._command_complete("IDLE", tag)
+        with m.idle(duration=_IDLE) as idler:
+            for typ, data in idler:
+                getLogger().info("%s", f"{mailbox} -> {typ} {data!r}")
+                if typ == "BYE":
+                    return
+                yield None
 
 
 def _trigger(channel: str) -> None:
@@ -207,19 +174,20 @@ def main() -> None:
             Path.home() / "Library" / "LaunchAgents" / f"imap.notify.{channel}.plist"
         )
 
-        if args.remove:
-            return launchd.unlink(missing_ok=True)
-        elif args.install:
-            return _install(channel, path=launchd)
+        match args.remove, args.install:
+            case True, _:
+                return launchd.unlink(missing_ok=True)
+            case _, True:
+                return _install(channel, path=launchd)
 
         idle = partial(_idle, args.daemon, channel, host, authn, user)
 
-        if not (boxes := args.boxes):
+        if not (mailbox_names := args.boxes):
             with _imap(host, authn=authn, user=user) as m:
-                boxes = tuple(_boxes(m))
+                mailbox_names = tuple(_mailboxes(m))
 
         with ThreadPoolExecutor() as ex:
-            tuple(ex.map(idle, boxes))
+            tuple(ex.map(idle, mailbox_names))
     except Exception as e:
         getLogger().exception("%s", e)
         raise
