8eb0833830783e59186ead0da3cdd604bd7cb21b
dogisgreat <git@bigly.dog>
2026-05-29 12:41:47 -0700

    checktime

--- layers/posix/home/.config/nvim/lua/go/checktime.lua
+++ layers/posix/home/.config/nvim/lua/go/checktime.lua
@@ -11,40 +11,45 @@ vim.opt.autowriteall = true
 -- noskip backup
 vim.opt.backupskip = ""
 
-vim.api.nvim_create_autocmd(
-  { "FocusGained", "VimResume", "WinEnter" },
-  { group = lib.group, command = "silent! checktime" }
-)
-
 vim.api.nvim_create_autocmd({ "VimLeavePre" }, { group = lib.group, once = true, command = [[silent! wall! ++p]] })
 
 do
-  local check_time = function(lo)
-    return function()
-      if lo then
-        if vim.api.nvim_buf_get_name(0) ~= "" then
-          local nr = vim.api.nvim_get_current_buf()
-          vim.cmd.checktime(nr)
-        end
-      else
-        vim.cmd [[silent! checktime]]
-      end
+  local cycle = 1888
+  local focused = true
 
+  vim.api.nvim_create_autocmd({ "FocusGained", "VimResume", "WinEnter" }, {
+    group = lib.group,
+    callback = function()
+      focused = true
+      vim.cmd "silent! checktime"
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ "FocusLost" }, {
+    group = lib.group,
+    callback = function()
+      focused = false
+      if vim.api.nvim_buf_get_name(0) ~= "" then
+        vim.cmd("silent! checktime " .. vim.api.nvim_get_current_buf())
+      end
       vim.cmd [[silent! wall! ++p]]
+    end,
+  })
+
+  local sweep = function()
+    for _, b in pairs(vim.api.nvim_list_bufs()) do
+      if vim.api.nvim_buf_is_loaded(b) and vim.bo[b].buftype == "" and vim.api.nvim_buf_get_name(b) ~= "" then
+        vim.cmd("silent! checktime " .. b)
+      end
     end
   end
 
-  vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, { group = lib.group, callback = check_time(false) })
-  vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, { group = lib.group, callback = check_time(true) })
-
-  local cycle = 1888
   async.run(function()
     while true do
       async.sleep(cycle)
 
-      local mode = vim.api.nvim_get_mode().mode
-      if not vim.startswith(mode, "i") then
-        check_time(true)()
+      if not vim.startswith(vim.api.nvim_get_mode().mode, "i") or not focused then
+        sweep()
       end
     end
   end)
