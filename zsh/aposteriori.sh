5414068649670ca26bd3640b94636bd56ffcd567
dogisgreat <git@bigly.dog>
2026-05-30 23:45:49 -0700

    -

--- zsh/aposteriori.sh
+++ zsh/aposteriori.sh
@@ -13,15 +13,8 @@ fi
 
 # shellcheck disable=SC2154
 _posh_conf="$XDG_CONFIG_HOME/posh/config.yml"
-# shellcheck disable=SC2154
-_posh_cache="$XDG_CACHE_HOME/oh-my-posh/init.dog.$_sh"
 
-# shellcheck disable=SC2312
-if [[ ! -s $_posh_cache || $_posh_conf -nt $_posh_cache || $(command -v -- oh-my-posh) -nt $_posh_cache ]]; then
-  oh-my-posh init "$_sh" --config "$_posh_conf" > "$_posh_cache"
-fi
+# shellcheck disable=SC1090,2312
+source -- <(oh-my-posh init "$_sh" --config "$_posh_conf")
 
-# shellcheck disable=SC1090
-source -- "$_posh_cache"
-
-unset -- _sh _posh_conf _posh_cache
+unset -- _sh _posh_conf
