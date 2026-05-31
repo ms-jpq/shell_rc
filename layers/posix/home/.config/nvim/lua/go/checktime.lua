e917c7db566ae57bd23673cd952fc3eadcb11824
dogisgreat <git@bigly.dog>
2026-05-31 02:20:42 -0700

    check time

--- layers/posix/home/.config/nvim/lua/go/checktime.lua
+++ layers/posix/home/.config/nvim/lua/go/checktime.lua
@@ -36,6 +36,13 @@ do
     end,
   })
 
+  vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+    group = lib.group,
+    callback = function()
+      vim.cmd [[silent! wall! ++p]]
+    end,
+  })
+
   local sweep = function()
     for _, b in pairs(vim.api.nvim_list_bufs()) do
       if vim.api.nvim_buf_is_loaded(b) and vim.bo[b].buftype == "" and vim.api.nvim_buf_get_name(b) ~= "" then
