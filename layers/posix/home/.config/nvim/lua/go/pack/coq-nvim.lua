2653308b93959deb6ad932a0cc96a78b9fdcaa2f
dogisgreat <git@bigly.dog>
2026-06-06 00:11:54 -0700

    okok

--- layers/posix/home/.config/nvim/lua/go/pack/coq-nvim.lua
+++ layers/posix/home/.config/nvim/lua/go/pack/coq-nvim.lua
@@ -1,9 +1,6 @@
 vim.g.coq_v2 = 1
 
 vim.g.coq_settings = {
-  display = {
-    mark_applied_notify = false,
-  },
   clients = {
     ["registers.lines"] = { "z" },
     ["snippets.user_path"] = "~/.cache/helix-rt/nvim/pack/opt/snips",
