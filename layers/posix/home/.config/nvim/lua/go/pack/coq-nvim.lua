0b4401a035e4e8d0e22a851335b31cd6a3f67b51
dogisgreat <git@bigly.dog>
2026-06-08 11:09:46 -0700

    ok

--- layers/posix/home/.config/nvim/lua/go/pack/coq-nvim.lua
+++ layers/posix/home/.config/nvim/lua/go/pack/coq-nvim.lua
@@ -5,7 +5,4 @@ vim.g.coq_settings = {
     ["registers.lines"] = { "z" },
     ["snippets.user_path"] = "~/.cache/helix-rt/nvim/pack/opt/snips",
   },
-  keymap = {
-    jump_to_mark = [[<c-y>]],
-  },
 }
