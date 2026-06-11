8119e07fc475f0e1f4df4cde81fa481dc4aff695
dogisgreat <git@bigly.dog>
2026-06-11 07:40:35 -0700

    buf

--- layers/posix/home/.config/nvim/lua/go/pack/gitsigns.lua
+++ layers/posix/home/.config/nvim/lua/go/pack/gitsigns.lua
@@ -5,23 +5,31 @@ local gs = require "gitsigns"
 gs.setup {
   current_line_blame = true,
   -- word_diff = true
-  on_attach = function()
-    vim.keymap.set({ "o", "x" }, "ih", "<cmd>Gitsigns select_hunk<cr>")
+  on_attach = function(buf)
+    local opt = { noremap = true, buffer = buf }
+
+    vim.keymap.set({ "o", "x" }, "ih", "<cmd>Gitsigns select_hunk<cr>", opt)
 
     vim.keymap.set({ "n" }, "[g", function()
       gs.nav_hunk "prev"
-    end)
+    end, opt)
     vim.keymap.set({ "n" }, "]g", function()
       gs.nav_hunk "next"
-    end)
+    end, opt)
 
-    vim.keymap.set({ "n" }, [[<leader>i]], gs.preview_hunk_inline)
-    vim.keymap.set({ "n" }, [[<leader>I]], gs.diffthis)
+    vim.keymap.set({ "n" }, [[<leader>i]], gs.preview_hunk_inline, opt)
+    vim.keymap.set({ "n" }, [[<leader>I]], gs.diffthis, opt)
 
-    vim.keymap.set({ "n" }, [[<leader>e]], gs.stage_hunk)
+    vim.keymap.set({ "n" }, [[<leader>e]], gs.stage_hunk, opt)
 
-    vim.keymap.set({ "n", "x" }, [[<leader>c]], gs.reset_hunk)
+    vim.keymap.set({ "n", "x" }, [[<leader>c]], gs.reset_hunk, opt)
 
-    vim.api.nvim_create_autocmd({ "FocusGained" }, { group = lib.group, command = [[Gitsigns refresh]] })
+    vim.api.nvim_create_autocmd({ "FocusGained" }, {
+      group = lib.group,
+      buffer = buf,
+      callback = function()
+        gs.refresh(buf)
+      end,
+    })
   end,
 }
