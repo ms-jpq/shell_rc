f3754b9fe59278084b9ce6ab804466c947441ffe
dogisgreat <git@bigly.dog>
2026-06-11 16:35:10 -0700

    k

--- zsh/iso/fn/title.sh
+++ zsh/iso/fn/title.sh
@@ -2,8 +2,10 @@
 
 if [[ -v TMUX ]]; then
   # OSC 2 -> set pane_title
+  # shellcheck disable=SC1003
   printf -- '\e]2;%s\e\\' "$*"
 else
   # OSC 0 -> set terminal icon
+  # shellcheck disable=SC1003
   printf -- '\e]0;%s\e\\' "$*"
 fi
