45732134213c017f4cf62db6576d325f18856ef6
dogisgreat <git@bigly.dog>
2026-06-13 11:27:01 -0700

    tp

---
+++ zsh/tmux/bin/tp.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+DST='tee'
+if [[ -t 1 ]]; then
+  DST=~/.config/zsh/bin/c
+fi
+
+tmux capture-pane -J -S - -E - -p | "$DST"
--- zsh/tmux/fn/tp.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env -S -- bash
-
-tp() {
-  # shellcheck disable=SC2312
-  tmux capture-pane -J -S - -E - -p | c
-}
