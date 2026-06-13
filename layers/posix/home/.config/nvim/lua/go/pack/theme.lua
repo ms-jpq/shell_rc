1d0f085ce671c8f919c691c43eae5f38e0c1d19f
dogisgreat <git@bigly.dog>
2026-06-13 08:24:19 -0700

    theme

--- layers/posix/home/.config/nvim/lua/go/pack/theme.lua
+++ layers/posix/home/.config/nvim/lua/go/pack/theme.lua
@@ -1,4 +1,13 @@
+local lib = require "go.lib"
+
 vim.opt.background = [[light]]
+
+vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+  group = lib.group,
+  callback = function()
+    vim.opt.background = [[light]]
+  end,
+})
 
 do
   -- https://github.com/EdenEast/nightfox.nvim
