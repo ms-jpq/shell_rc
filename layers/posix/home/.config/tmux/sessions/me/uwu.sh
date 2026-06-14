484b1c0ea6793e4d1ef9e904108b6ecc458f0f80
dogisgreat <git@bigly.dog>
2026-06-14 05:22:21 -0700

    k

--- layers/posix/home/.config/tmux/sessions/me/uwu.sh
+++ layers/posix/home/.config/tmux/sessions/me/uwu.sh
@@ -7,21 +7,21 @@ tmux set-environment -g -h -- "$ENV" 1
 
 tmux new-window -c ~/Downloads
 tmux select-pane -m
-tmux set-buffer -- $'aerc \n'
+tmux set-buffer -- $'aerc\n'
 tmux paste-buffer -d -p
 tmux select-pane -t '{marked}'
 tmux select-pane -M
 
 tmux new-window -c "$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents"
 tmux select-pane -m
-tmux set-buffer -- $'yazi \n'
+tmux set-buffer -- $'yazi\n'
 tmux paste-buffer -d -p
 tmux select-pane -t '{marked}'
 tmux select-pane -M
 
 tmux new-window -c ~/Downloads
 tmux select-pane -m
-tmux set-buffer -- $'~/.local/opt/ai/bin/notificationd \n'
+tmux set-buffer -- $'~/.local/opt/ai/bin/notificationd\n'
 tmux paste-buffer -d -p
 tmux select-pane -t '{marked}'
 tmux select-pane -M
