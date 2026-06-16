6c6227eeb6fb1173b71943cf9501fc09f6382f71
dogisgreat <git@bigly.dog>
2026-06-16 03:39:02 -0700

    empty commit

--- zsh/iso/fn/cdt.sh
+++ zsh/iso/fn/cdt.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env -S -- bash
 
 cdt() {
-  local tmp
+  local -- tmp
   tmp="$(mktemp --directory)"
   cd -- "$tmp" || return 1
+
   git init --quiet
+  git commit --quiet --allow-empty --allow-empty-message --no-edit --message ''
   if (($#)); then
     "$@"
   fi
