62a381cd8bd4bd2b99307dcdc1a673ec60a22442
dogisgreat <git@bigly.dog>
2026-06-16 20:04:23 -0700

    paths

--- zsh/darwin/path.sh
+++ zsh/darwin/path.sh
@@ -17,6 +17,7 @@ path=(
   '/opt/homebrew/opt/lsof/bin'
   '/opt/homebrew/opt/m4/bin'
   '/opt/homebrew/opt/make/libexec/gnubin'
+  '/opt/homebrew/opt/moreutils/bin'
   '/opt/homebrew/opt/mysql-client/bin'
   '/opt/homebrew/opt/ncurses/bin'
   '/opt/homebrew/opt/openssl/bin'
