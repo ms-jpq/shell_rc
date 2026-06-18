411c5741b2b4265286a65dd9e34d4ab7369a97aa
dogisgreat <git@bigly.dog>
2026-06-17 22:42:42 -0700

    -

---
+++ layers/posix/home/.config/ranger/commands.py
@@ -0,0 +1,28 @@
+from os.path import normpath
+from pathlib import Path
+
+from ranger.api.commands import Command  # type: ignore
+
+
+class touch(Command):
+    def tab(self, _):
+        return self._tab_directory_content()
+
+    def execute(self):
+        if not (name := self.rest(1)):
+            self.fm.notify("touch: missing filename", bad=True)
+            return
+
+        cwd = Path(self.fm.thisdir.path)
+        path = Path(normpath(cwd / Path(name).expanduser()))
+        directory = path.parent
+
+        directory.mkdir(parents=True, exist_ok=True)
+        path.touch()
+        while True:
+            self.fm.get_directory(str(directory)).load_content(schedule=False)
+            if directory in (cwd, directory.parent):
+                break
+            directory = directory.parent
+
+        self.fm.select_file(str(path))
