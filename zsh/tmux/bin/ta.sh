cdcbf9d9d302accdf1d7f30fbb17f00b12859e9d
dogisgreat <git@bigly.dog>
2026-06-17 22:26:22 -0700

    tmux choose

--- zsh/tmux/bin/ta.sh
+++ zsh/tmux/bin/ta.sh
@@ -17,6 +17,8 @@ elif [[ -n $SESSION ]]; then
   fi
 elif [[ -v TMUX ]]; then
   exec -- tmux choose-tree -G -Z -O name -s -NN
+elif SESSIONS="$(tmux list-sessions -F '#{session_name}' 2> /dev/null)" && [[ -n $SESSIONS ]]; then
+  exec -- tmux attach-session -t "=${SESSIONS%%$'\n'*}" \; choose-tree -G -Z -O name -s -NN
 else
   SESSION='me/owo'
   SESSION_SCRIPT="$XDG_STATE_HOME/tmux/$SESSION.sh"
