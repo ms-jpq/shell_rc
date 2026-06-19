611d9c555b17705dd0cca72ad31fcb0df6ef1e49
dogisgreat <git@bigly.dog>
2026-06-18 20:03:00 -0700

    k

--- zsh/iso/editor.sh
+++ zsh/iso/editor.sh
@@ -2,7 +2,20 @@
 
 export -- EDITOR="${EDITOR:-nvim}"
 export -- VISUAL="$EDITOR"
-export -- BROWSER="${BROWSER:-open}"
+
+case "$OSTYPE" in
+darwin*)
+  _open=open
+  ;;
+msys | cygwin)
+  _open=cygstart
+  ;;
+*)
+  _open=xdg-open
+  ;;
+esac
+export -- BROWSER="${BROWSER:-$_open}"
+unset -- _open
 
 # shellcheck disable=2154
 export -- INPUTRC="$XDG_CONFIG_HOME/readline/inputrc"
