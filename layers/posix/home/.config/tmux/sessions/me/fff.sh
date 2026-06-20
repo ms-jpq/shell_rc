48e500d8feba1833738dd3e53bcdf807db28fb2d
dogisgreat <git@bigly.dog>
2026-06-20 02:17:25 -0700

    ok

--- layers/posix/home/.config/tmux/sessions/me/fff.sh
+++ layers/posix/home/.config/tmux/sessions/me/fff.sh
@@ -16,5 +16,27 @@ tmux select-pane -t \{marked\}
 tmux select-pane -M
 tmux select-layout -- even-horizontal
 
+tmux new-window -c ~/dev.localized/shell_rc
+tmux set-buffer -- $'claude --continue || claude --name shell_rc\n'
+tmux paste-buffer -d -p
+tmux split-window -c ~/dev.localized/shell_rc
+tmux select-pane -m
+tmux set-buffer -- $'nvim\n'
+tmux paste-buffer -d -p
+tmux select-pane -t \{marked\}
+tmux select-pane -M
+tmux select-layout -- even-horizontal
+
+tmux new-window -c ~/dev.localized/ai
+tmux set-buffer -- $'claude --continue || claude --name ai\n'
+tmux paste-buffer -d -p
+tmux split-window -c ~/dev.localized/ai
+tmux select-pane -m
+tmux set-buffer -- $'nvim\n'
+tmux paste-buffer -d -p
+tmux select-pane -t \{marked\}
+tmux select-pane -M
+tmux select-layout -- even-horizontal
+
 # tmux select-window -t :-2
 tmux set-environment -g -h -u -- "$ENV"
