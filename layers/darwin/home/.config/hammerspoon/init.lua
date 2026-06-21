9d6ea4bd31241864bf1e86622ab7ebb3e9a3a60a
dogisgreat <git@bigly.dog>
2026-06-21 09:34:20 -0700

    init

--- layers/darwin/home/.config/hammerspoon/init.lua
+++ layers/darwin/home/.config/hammerspoon/init.lua
@@ -1,11 +1,11 @@
 require "hs.ipc"
 
+local st = function()
+  return false
+end
+
 local function run(arg0, args)
-  return hs.task
-    .new(arg0, nil, function()
-      return false
-    end, args)
-    :start()
+  return hs.task.new(arg0, nil, st, args):start()
 end
 
 do
@@ -62,17 +62,79 @@ end
 do
   local KITTEN = "/opt/homebrew/bin/kitten"
 
-  local slurp = function()
-    return ""
+  local tmpfile = function(text)
+    local tmp = string.format("%sedit-%d.md", os.getenv "TMPDIR" or "/tmp/", os.time())
+
+    local fw = io.open(tmp, "w")
+    if fw then
+      fw:write(text)
+      fw:close()
+    end
+
+    return tmp,
+      function()
+        local fr = io.open(tmp, "r")
+        local read = fr and fr:read "*a" or ""
+        if fr then
+          fr:close()
+        end
+        os.remove(tmp)
+        return read
+      end
   end
-  local spit = function(text) end
+
+  local function focused()
+    local app = hs.application.frontmostApplication()
+    if not app then
+      return nil, nil
+    end
+    local ax_app = hs.axuielement.applicationElement(app)
+    if not ax_app then
+      return app, nil
+    end
+    return app, ax_app:attributeValue "AXFocusedUIElement"
+  end
+
+  local function slurp(el)
+    return (el and el:attributeValue "AXValue") or ""
+  end
+
+  local function spit(el, text)
+    if el then
+      el:setAttributeValue("AXValue", text)
+    end
+  end
 
   local function edit_in_kitty()
-    local pre_text = slurp()
-    local tmp = ""
-    run(KITTEN, { "quick-access-terminal", "--", "nvim", "--", tmp }):waitUntilExit()
-    local post_text = ""
-    spit(post_text)
+    local app, el = focused()
+    if not (app and el) then
+      return
+    end
+
+    local pre_text = slurp(el)
+    local tmp, read = tmpfile(pre_text)
+
+    hs.task
+      .new(
+        KITTEN,
+        function()
+          local post_text = read()
+          app:activate()
+          spit(el, post_text)
+        end,
+        st,
+        {
+          "quick-access-terminal",
+          "--instance-group=edit",
+          "--",
+          "/opt/homebrew/bin/zsh",
+          "-ic",
+          [[nvim -- "$1"]],
+          "-",
+          tmp,
+        }
+      )
+      :start()
   end
 
   _G.editHotkey = hs.hotkey.bind({ "cmd", "shift" }, "e", edit_in_kitty)
