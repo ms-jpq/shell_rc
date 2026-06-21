920e1dbc644ed87fea1da350c62e3177ddfca3c0
dogisgreat <git@bigly.dog>
2026-06-21 09:08:56 -0700

    run

--- layers/darwin/home/.config/hammerspoon/init.lua
+++ layers/darwin/home/.config/hammerspoon/init.lua
@@ -1,19 +1,19 @@
 require "hs.ipc"
 
+local function run(arg0, args)
+  return hs.task
+    .new(arg0, nil, function()
+      return false
+    end, args)
+    :start()
+end
+
 do
   local VPN = "/Applications/OpenVPN Connect/OpenVPN Connect.app/Contents/MacOS/OpenVPN Connect"
   local INTERNAL = {
     ["home1"] = true,
     ["home2"] = true,
   }
-
-  local function run(args)
-    hs.task
-      .new(VPN, nil, function()
-        return false
-      end, args)
-      :start()
-  end
 
   local pending = nil
   local function on_wifi(_, event)
@@ -27,10 +27,10 @@ do
     pending = hs.timer.doAfter(2, function()
       local ssid = hs.wifi.currentNetwork()
       if INTERNAL[ssid] then
-        run { "--quit" }
+        run(VPN, { "--quit" })
         hs.alert.show("🔓 VPN off — " .. ssid)
       elseif ssid then
-        run { "--minimize" }
+        run(VPN, { "--minimize" })
         hs.alert.show("🔒 VPN on — " .. ssid)
       else
         hs.alert.show "🔒 Wifi off"
@@ -57,4 +57,23 @@ do
 
   _G.screenWatcher = hs.screen.watcher.new(main_screen):start()
   main_screen()
+end
+
+do
+  local KITTEN = "/opt/homebrew/bin/kitten"
+
+  local slurp = function()
+    return ""
+  end
+  local spit = function(text) end
+
+  local function edit_in_kitty()
+    local pre_text = slurp()
+    local tmp = ""
+    run(KITTEN, { "quick-access-terminal", "--", "nvim", "--", tmp }):waitUntilExit()
+    local post_text = ""
+    spit(post_text)
+  end
+
+  _G.editHotkey = hs.hotkey.bind({ "cmd", "shift" }, "e", edit_in_kitty)
 end
