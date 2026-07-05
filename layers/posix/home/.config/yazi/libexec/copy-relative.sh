a360d2e25c95c93a47de44003182bff19aeea523
dogisgreat <git@bigly.dog>
2026-07-05 08:31:54 -0700

    k

--- layers/posix/home/.config/yazi/libexec/copy-relative.sh
+++ layers/posix/home/.config/yazi/libexec/copy-relative.sh
@@ -2,14 +2,20 @@
 
 set -o pipefail
 
-NAME="$*"
+T=1
+if ! TOP="$(git --no-optional-locks rev-parse --path-format=absolute --show-toplevel)"; then
+  T=0
+  TOP='/'
+fi
 
-if TOP="$(git --no-optional-locks rev-parse --path-format=absolute --show-toplevel)"; then
-  realpath --no-symlinks --relative-to "$TOP" -- "$NAME"
-elif [[ $NAME == "$HOME"/* ]]; then
-  # shellcheck disable=SC2088
-  printf -- '%s' '~/'
-  realpath --no-symlinks --relative-base "$HOME" -- "$NAME"
-else
-  printf -- '%s' "$NAME"
-fi | sed -E -z -e 's/\n$//' | ~/.config/zsh/bin/c
+for NAME in "$@"; do
+  if ((T)); then
+    realpath --no-symlinks --relative-to "$TOP" -- "$NAME"
+  elif [[ $NAME == "$HOME"/* ]]; then
+    # shellcheck disable=SC2088
+    printf -- '%s' '~/'
+    realpath --no-symlinks --relative-base "$HOME" -- "$NAME"
+  else
+    printf -- '%s' "$NAME"
+  fi
+done | sed -E -z -e 's/\n$//' | ~/.config/zsh/bin/c
