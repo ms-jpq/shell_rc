04241de25f025c528429b667403ade937db636b2
dogisgreat <git@bigly.dog>
2026-07-11 22:49:21 -0700

    simp

--- layers/posix/home/.config/nvim/lua/go/session/scope.lua
+++ layers/posix/home/.config/nvim/lua/go/session/scope.lua
@@ -6,38 +6,32 @@ local is_file_name = function(name)
   return name ~= "" and not string.match(name, [[^%w[%w+.-]*://]])
 end
 
+local realpath = function(path)
+  return vim.uv.fs_realpath(path) or vim.fs.normalize(path, { expand_env = false })
+end
+
 local in_cwd = function(cwd, name)
   if not is_file_name(name) then
     return false
   end
 
   local joined = string.sub(name, 1, 1) == lib.os.sep and name or vim.fs.joinpath(cwd, name)
-  local norm = vim.fs.normalize(joined, { expand_env = false })
-  return vim.fs.relpath(cwd, norm) ~= nil
+  return vim.fs.relpath(cwd, realpath(joined)) ~= nil
 end
 
-M.prune_buffers = function(cwd)
-  vim
-    .iter(vim.api.nvim_list_bufs())
-    :filter(function(buf)
-      if vim.bo[buf].modified then
-        return false
-      end
-      local name = vim.api.nvim_buf_get_name(buf)
-      return is_file_name(name) and not in_cwd(cwd, name)
-    end)
-    :each(function(buf)
-      vim.api.nvim_buf_delete(buf, { force = true })
-    end)
+local external_buffer = function(cwd, buf)
+  local name = vim.api.nvim_buf_get_name(buf)
+  return is_file_name(name) and not vim.bo[buf].modified and not in_cwd(cwd, name)
 end
 
 M.prune_session = function(cwd)
+  cwd = realpath(cwd)
+
   for _, win in pairs(vim.api.nvim_list_wins()) do
     if vim.api.nvim_win_is_valid(win) then
       local buf = vim.api.nvim_win_get_buf(win)
-      local name = vim.api.nvim_buf_get_name(buf)
 
-      if not in_cwd(cwd, name) and not vim.bo[buf].modified then
+      if external_buffer(cwd, buf) then
         local tab = vim.api.nvim_win_get_tabpage(win)
 
         if #vim.api.nvim_tabpage_list_wins(tab) > 1 then
@@ -53,7 +47,11 @@ M.prune_session = function(cwd)
     end
   end
 
-  M.prune_buffers(cwd)
+  for _, buf in pairs(vim.api.nvim_list_bufs()) do
+    if external_buffer(cwd, buf) then
+      vim.api.nvim_buf_delete(buf, { force = true })
+    end
+  end
 end
 
 return M
