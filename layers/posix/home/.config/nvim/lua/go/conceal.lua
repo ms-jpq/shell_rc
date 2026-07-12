61ea69f77215ac3f27d20841b339fa5a286726fe
dogisgreat <git@bigly.dog>
2026-07-12 12:10:57 -0700

    syntax?

--- layers/posix/home/.config/nvim/lua/go/conceal.lua
+++ layers/posix/home/.config/nvim/lua/go/conceal.lua
@@ -1,4 +1,3 @@
-local async = require "go.async"
 local lib = require "go.lib"
 
 vim.opt.conceallevel = 2
@@ -14,41 +13,60 @@ do
 end
 
 do
+  local prefix = "__go_conceal__"
   local syn = vim.fs.joinpath(lib.cfg, "after", "syntax")
   local tax = vim.fs.joinpath(syn, "_.vim")
 
-  local apply = function(buf)
-    local ft = vim.bo[buf].filetype
-    if ft == "" then
-      return false
-    end
-
-    local path = vim.fs.joinpath(syn, ft .. ".vim")
-
-    vim.api.nvim_buf_call(buf, function()
-      vim.cmd.source(tax)
-      if vim.fn.filereadable(path) == 1 then
-        vim.cmd.source(path)
-      end
-    end)
-
-    return true
+  local syntax_name = function(ft)
+    return prefix .. ft
   end
 
-  vim.api.nvim_create_autocmd({ "FileType", "FileChangedShellPost" }, {
+  local syntax_ft = function(syntax)
+    return string.match(syntax, "^" .. prefix .. "(.+)$")
+  end
+
+  local enable = function(buf)
+    local ft = vim.bo[buf].filetype
+    if ft ~= "" and vim.bo[buf].syntax ~= syntax_name(ft) then
+      vim.bo[buf].syntax = syntax_name(ft)
+    end
+  end
+
+  vim.api.nvim_create_autocmd({ "VimEnter" }, {
     group = lib.group,
-    callback = async(function(args)
-      local buf = args.buf
-      if vim.b[buf].__conceal__ and args.event ~= "FileChangedShellPost" then
-        return
-      end
+    once = true,
+    callback = function()
+      vim.cmd.syntax "manual"
 
-      async.scheduled()
-      if not vim.api.nvim_buf_is_valid(buf) then
-        return
-      end
+      vim.api.nvim_create_autocmd({ "Syntax" }, {
+        group = lib.group,
+        callback = function(args)
+          local ft = syntax_ft(args.match)
+          if not ft then
+            return
+          end
 
-      vim.b[buf].__conceal__ = apply(buf)
-    end),
+          local path = vim.fs.joinpath(syn, ft .. ".vim")
+
+          vim.cmd.source(tax)
+          if vim.fn.filereadable(path) == 1 then
+            vim.cmd.source(path)
+          end
+        end,
+      })
+
+      vim.api.nvim_create_autocmd({ "BufReadPost", "FileChangedShellPost", "FileType" }, {
+        group = lib.group,
+        callback = function(args)
+          enable(args.buf)
+        end,
+      })
+
+      for _, buf in pairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_loaded(buf) then
+          enable(buf)
+        end
+      end
+    end,
   })
 end
