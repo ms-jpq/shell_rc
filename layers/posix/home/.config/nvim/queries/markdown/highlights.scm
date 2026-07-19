76c73f4bee88582d9987e68a7cf8ff85396f7eea
dogisgreat <git@bigly.dog>
2026-07-19 00:18:28 -0700

    sweet

--- layers/posix/home/.config/nvim/queries/markdown/highlights.scm
+++ layers/posix/home/.config/nvim/queries/markdown/highlights.scm
@@ -1,4 +1,7 @@
 ;; extends
 
 ((block_quote) @comment
-  (#lua-match? @comment "^> |"))
+  (#lua-match? @comment "^>%s*|"))
+
+((block_quote) @keyword.directive
+  (#lua-match? @keyword.directive "^>%s*|%s*>>>"))
