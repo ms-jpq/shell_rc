33e5134e5278b1f303852859d7be7cddf1a148d5
dogisgreat <git@bigly.dog>
2026-07-25 06:58:43 -0700

    tables

--- layers/posix/home/.config/nvim/queries/markdown/highlights.scm
+++ layers/posix/home/.config/nvim/queries/markdown/highlights.scm
@@ -2,3 +2,21 @@
 
 ((block_quote) @comment
   (#lua-match? @comment "^>%s*|"))
+
+(pipe_table_header "|" @markup.table.separator
+  (#set! conceal "│"))
+
+(pipe_table_row "|" @markup.table.separator
+  (#set! conceal "│"))
+
+(pipe_table_delimiter_row "|" @markup.table.separator
+  (#set! conceal "│"))
+
+(pipe_table_delimiter_cell "-" @markup.table.separator
+  (#set! conceal "─"))
+
+((pipe_table_align_left) @markup.table.separator
+  (#set! conceal "╶"))
+
+((pipe_table_align_right) @markup.table.separator
+  (#set! conceal "╴"))
