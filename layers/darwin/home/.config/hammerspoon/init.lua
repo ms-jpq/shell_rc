30d9ff18d008d3ba686302a7cca9b795b3f1ad1b
dogisgreat <git@bigly.dog>
2026-08-03 12:27:37 -0700

    screen

--- layers/darwin/home/.config/hammerspoon/init.lua
+++ layers/darwin/home/.config/hammerspoon/init.lua
@@ -4,7 +4,6 @@ require("togo.cursor").init()
 require("togo.desktop").init()
 require("togo.kitty").init()
 require("togo.screen-brightness").init()
-require("togo.screen-size").init()
 require("togo.vpn").init()
 
 hs.alert.show ">>> hs <<<"
