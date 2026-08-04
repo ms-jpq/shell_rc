60a84951ee95b136054aa7d421cfa4da395d6be7
dogisgreat <git@bigly.dog>
2026-08-03 23:58:53 -0700

    f

--- layers/posix/home/.config/nvim/lua/go/pack/treesitter.lua
+++ layers/posix/home/.config/nvim/lua/go/pack/treesitter.lua
@@ -3,7 +3,6 @@ require("nvim-treesitter").setup {}
 vim.opt.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
 
 require("treesitter-context").setup {
-  enable = true,
   multiline_threshold = 1,
 }
 
