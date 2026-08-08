03d394bd81d1f3d915c18767a7c5ce7bc0bc4ae1
dogisgreat <git@bigly.dog>
2026-08-08 11:18:12 -0700

    adsf

--- layers/posix/home/.config/nvim/pack/local/start/goto/libexec/repl.sh
+++ layers/posix/home/.config/nvim/pack/local/start/goto/libexec/repl.sh
@@ -2,12 +2,12 @@
 
 set -o pipefail
 
-: "${REPL_FILE_NAME:?}"
-: "${REPL_LINE_COL:?}"
-: "${REPL_LINE_COUNT:?}"
-: "${REPL_LINE_ROW:?}"
-: "${REPL_PRETTY_NAME:?}"
-: "${REPL_TARGET:?}"
+REPL_FILE_NAME="${REPL_FILE_NAME:-}"
+REPL_LINE_COL="${REPL_LINE_COL:-}"
+REPL_LINE_COUNT="${REPL_LINE_COUNT:-}"
+REPL_LINE_ROW="${REPL_LINE_ROW:-}"
+REPL_PRETTY_NAME="${REPL_PRETTY_NAME:-}"
+REPL_TARGET="${REPL_TARGET:-}"
 
 SOCKET="${__TMUX_ROOT_SOCKET__:-${TMUX%%,*}}"
 CURRENT_PANE="${__TMUX_ROOT_PANE__:-${TMUX_PANE:-}}"
@@ -17,28 +17,44 @@ if [[ -z $SOCKET || -z $CURRENT_PANE ]]; then
 fi
 
 case "$REPL_TARGET" in
-"")
+'')
   TM=(tmux -S "$SOCKET")
   CURRENT_WINDOW="$("${TM[@]}" display-message -t "$CURRENT_PANE" -p -F '#{window_id}')"
 
   FORMAT=$'#{pane_id}\t#{window_id}\t#{window_active}\t#{session_name} -> #{window_index} -> #{pane_index}\t#{?#{pane_path},#{pane_path},#{pane_current_path}}'
 
   read -r -d '' AWK << 'AWK' || true
-$1 != pane {
-  active = $3 == "1" ? 0 : 1
-  same_window = $2 == window ? 0 : 1
-  glyph = same_window == 0 ? "\342\226\243" : active == 0 ? "\342\233\266" : ""
-  label = $4 " " $5
-  if (glyph != "") {
-    label = label " " glyph
+BEGIN {
+  ACTIVE = 0
+  GLYPH = ""
+  LABEL = ""
+  SAME_WINDOW = 0
+}
+
+$1 != PANE {
+  ACTIVE = $3 == "1" ? 0 : 1
+  SAME_WINDOW = $2 == WINDOW ? 0 : 1
+  GLYPH = SAME_WINDOW == 0 ? SAME_WINDOW_GLYPH : ACTIVE == 0 ? ACTIVE_GLYPH : ""
+  LABEL = $4 " " $5
+  if (GLYPH != "") {
+    LABEL = LABEL " " GLYPH
   }
-  printf "%d\t%d\t%09d\t%s\t%s\n", active, same_window, NR, $1, label
+  printf "%d\t%d\t%09d\t%s\t%s\n", ACTIVE, SAME_WINDOW, NR, $1, LABEL
 }
 AWK
+  PARSE=(
+    awk -F '\t'
+    -v ACTIVE_GLYPH='⛶'
+    -v SAME_WINDOW_GLYPH='▣'
+    -v PANE="$CURRENT_PANE"
+    -v WINDOW="$CURRENT_WINDOW"
+    "$AWK"
+  )
 
-  "${TM[@]}" list-panes -a -F "$FORMAT" | awk -F '\t' -v pane="$CURRENT_PANE" -v window="$CURRENT_WINDOW" "$AWK" | LC_ALL=C.UTF-8 sort -t $'\t' -k1,1n -k2,2n -k3,3n | awk -F '\t' '{ printf "%s\t%s%c", $4, $5, 0 }'
+  "${TM[@]}" list-panes -a -F "$FORMAT" | "${PARSE[@]}" | LC_ALL=C.UTF-8 sort -t $'\t' -k1,1n -k2,2n -k3,3n | awk -F '\t' '{ printf "%s\t%s%c", $4, $5, 0 }'
   ;;
 *)
+  PANE="${REPL_TARGET%%$'\t'*}"
 
   read -r -d '' AWK << 'AWK' || true
 BEGIN {
@@ -59,12 +75,12 @@ NR > HI {
 }
 
 {
-  MARKER = NR == ROW ? "->" : "  "
+  MARKER = NR == ROW ? "→" : " "
   printf "%s %*d | %s\n", MARKER, WIDTH, NR, $0
 }
 AWK
 
-  GEN=(
+  PRINT_CONTEXT=(
     awk
     -v COL="$REPL_LINE_COL"
     -v COUNT="$REPL_LINE_COUNT"
@@ -75,7 +91,7 @@ AWK
     "$AWK"
   )
 
-  "${GEN[@]}" < "$REPL_FILE_NAME" | ~/.config/tmux/libexec/send-text.sh "$PANE"
+  "${PRINT_CONTEXT[@]}" < "$REPL_FILE_NAME" | ~/.config/tmux/libexec/send-text.sh "$PANE"
 
   printf '%s' "⮕  [$PANE]"
   ;;
