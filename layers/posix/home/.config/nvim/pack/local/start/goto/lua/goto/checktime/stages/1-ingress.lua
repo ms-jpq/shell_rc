80f2b9802e92f0a70e2bbe1e39f61f47b60c90fd
dogisgreat <git@bigly.dog>
2026-08-12 01:57:36 -0700

    k

--- layers/posix/home/.config/nvim/pack/local/start/goto/lua/goto/checktime/stages/1-ingress.lua
+++ layers/posix/home/.config/nvim/pack/local/start/goto/lua/goto/checktime/stages/1-ingress.lua
@@ -124,6 +124,7 @@ M.start = function(spec)
       state.dispatch { kind = reducer.ACTIONS.CHANGE, buf = action.buf, change = reducer.CHANGES.REMOTE }
     elseif action.kind == reader.OBSERVATIONS.BASE then
       ---@cast action ChecktimeReaderBase
+      local_change(action.buf)
       local checkpoint = session.take_checkpoint(action.buf)
       if
         checkpoint
@@ -135,7 +136,6 @@ M.start = function(spec)
         vim.bo[action.buf].modified = true
         state.dispatch { kind = reducer.ACTIONS.CHANGE, buf = action.buf, change = reducer.CHANGES.REMOTE }
       else
-        local_change(action.buf)
         state.dispatch { kind = reducer.ACTIONS.BASE, buf = action.buf, base = action.base }
         if action.observed and action.base.text ~= action.observed then
           state.dispatch { kind = reducer.ACTIONS.CHANGE, buf = action.buf, change = reducer.CHANGES.REMOTE }
