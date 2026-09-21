4a895103387146cd2b168541bd6604e1e6873e63
dogisgreat <git@bigly.dog>
2026-09-21 00:30:33 -0700

    f

--- zsh/darwin/fn/brewup.sh
+++ zsh/darwin/fn/brewup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S -- bash
 
 brew update
-brew upgrade
+brew upgrade --yes
 # shellcheck disable=SC2312
 brew list --versions | awk -- '$2 ~ /^HEAD/ { print $1 }' | xargs -r -- brew upgrade --fetch-HEAD --
 brew cleanup
